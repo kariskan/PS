27f4e064e86ffd7a91a14706f2a093a67c05a9e0
kariskan <73534426+kariskan@users.noreply.github.com>
2022-03-03 00:02:05 +0900

    [Gold III] Title: 소수의 연속합, Time: 60 ms, Memory: 33272 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -1,36 +1,42 @@
-#include <stdio.h>
-#include <math.h>
-int num[300000] = { 0, };
-int a[4000001] = { 0, };
+#include <iostream>
+using namespace std;
+int n;
+int prime[4000001];
+int idx;
+int a[4000001];
+void p() {
+    for (int i = 2; i <= n; i++) {
+        if (prime[i]) continue;
+        prime[idx++] = i;
+        for (int j = i * 2; j <= n; j += i) {
+            prime[j] = 1;
+        }
+    }
+}
+
 int main() {
-	for (int i = 2; i <= sqrt(4000000); i++) {
-		if (a[i] != 0)continue;
-		for (int j = i * 2; j <= 4000000; j += i) {
-			a[j] = 1;
-		}
-	}
-	int j = 0;
-	for (int i = 2; i <= 4000000; i++)
-		if (a[i] == 0) {
-			num[j] = i;
-			j++;
-		}
-	int sum = 0, ans = 0;
-	int n;
-	scanf("%d", &n);
-	for (int i = 0; i <= j; i++) {
-		sum = 0;
-		if (num[i] > n)break;
-		for (int k = i; k <= j; k++) {
-			sum += num[k];
-			if (sum == n) {
-				ans++;
-				break;
-			}
-			else if (sum > n) {
-				break;
-			}
-		}
-	}
-	printf("%d", ans);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> n;
+    p();
+    int p1 = 0, p2 = 0;
+    int ans = 0, sum = prime[0];
+    while (p1 <= p2 && p2 < idx) {
+        if (sum == n) {
+            ans++;
+            sum += prime[++p2];
+        }
+        else if (sum > n) {
+            sum -= prime[p1++];
+            if (p1 > p2) {
+                p2 = p1;
+                sum = prime[p1];
+            }
+        }
+        else {
+            sum += prime[++p2];
+        }
+    }
+    cout << ans;
 }
