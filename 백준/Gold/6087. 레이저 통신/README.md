7bd152e91c68ea43c839e7e7e11db314c137bc03
kariskan <73534426+kariskan@users.noreply.github.com>
2022-03-07 00:08:28 +0900

    [Gold IV] Title: 레이저 통신, Time: 0 ms, Memory: 2072 KB -BaekjoonHub

---
+++ 백준/Gold/6087. 레이저 통신/README.md
@@ -0,0 +1,12 @@
+# [Gold IV] 레이저 통신 - 6087 
+
+[문제 링크](https://www.acmicpc.net/problem/6087) 
+
+### 성능 요약
+
+메모리: 2072 KB, 시간: 0 ms
+
+### 분류
+
+0-1 너비 우선 탐색(0_1_bfs), 너비 우선 탐색(bfs), 다익스트라(dijkstra), 그래프 이론(graphs), 그래프 탐색(graph_traversal)
+
---
+++ 백준/Gold/6087. 레이저 통신/레이저 통신.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <queue>
+#include <tuple>
+using namespace std;
+int n, m;
+char board[101][101];
+int visit[101][101];
+int x[4] = { 0,0,1,-1 };
+int y[4] = { 1,-1,0,0 };
+pair<int, int> c1 = { -1,0 }, c2;
+int main() {
+	cin >> m >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> board[i][j];
+			if (board[i][j] == 'C') {
+				if (c1.first == -1) {
+					c1.first = i;
+					c1.second = j;
+				}
+				else {
+					c2.first = i;
+					c2.second = j;
+				}
+			}
+		}
+	}
+	queue<pair<int, int>> q; //i, j, dir
+	q.push({ c1.first,c1.second });
+	visit[c1.first][c1.second] = 1;
+	int ans = 100000000;
+	while (!q.empty()) {
+		int a, b;
+		a = q.front().first;
+		b = q.front().second;
+		q.pop();
+		if (a == c2.first && b == c2.second) {
+			ans = min(ans, visit[a][b]);
+			continue;
+		}
+		for (int i = 0; i < 4; i++) {
+			int nx = a + x[i];
+			int ny = b + y[i];
+			while (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] != '*') {
+				if (!visit[nx][ny])visit[nx][ny] = visit[a][b] + 1;
+				else {
+					if (visit[nx][ny] < visit[a][b] + 1) {
+						break;
+					}
+					visit[nx][ny] = visit[a][b] + 1;
+				}
+				q.push({ nx,ny });
+				nx += x[i];
+				ny += y[i];
+			}
+		}
+	}
+	cout << ans - 2;
+}
