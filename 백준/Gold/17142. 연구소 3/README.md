d5dc1d0a7affd69a64cc36304b90054400318305
kariskan <73534426+kariskan@users.noreply.github.com>
2022-03-08 22:25:09 +0900

    [Gold IV] Title: 연구소 3, Time: 12 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Gold/17142. 연구소 3/README.md
@@ -0,0 +1,12 @@
+# [Gold IV] 연구소 3 - 17142 
+
+[문제 링크](https://www.acmicpc.net/problem/17142) 
+
+### 성능 요약
+
+메모리: 2036 KB, 시간: 12 ms
+
+### 분류
+
+너비 우선 탐색(bfs), 브루트포스 알고리즘(bruteforcing), 그래프 이론(graphs), 그래프 탐색(graph_traversal)
+
---
+++ 백준/Gold/17142. 연구소 3/연구소 3.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+queue<pair<int, int>> q;
+int board[50][50];
+vector<pair<int, int>> virus;
+vector<int> t;
+int x[4] = { 0,0,1,-1 };
+int y[4] = { 1,-1,0,0 };
+int n, m;
+
+int main() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> board[i][j];
+			if (board[i][j] == 2) {
+				virus.push_back({ i,j });
+			}
+		}
+	}
+	t.resize(virus.size() - m);
+	for (int i = 0; i < m; i++) {
+		t.push_back(1);
+	}
+	int ans = -1;
+	do {
+		int visit[50][50] = { 0, };
+		int res = 1;
+		for (int i = 0; i < virus.size(); i++) {
+			if (t[i] == 1) {
+				q.push({ virus[i].first,virus[i].second });
+				visit[virus[i].first][virus[i].second] = 1;
+			}
+		}
+		while (!q.empty()) {
+			int a = q.front().first;
+			int b = q.front().second;
+			q.pop();
+			for (int i = 0; i < 4; i++) {
+				int nx = a + x[i];
+				int ny = b + y[i];
+				if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visit[nx][ny] && board[nx][ny] != 1) {
+					visit[nx][ny] = visit[a][b] + 1;
+					if(board[nx][ny]!=2) res = visit[nx][ny];
+					q.push({ nx,ny });
+				}
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (visit[i][j] == 0 && board[i][j] != 1 && board[i][j] != 2) {
+					res = -1;
+					break;
+				}
+			}
+			if (res == -1)break;
+		}
+		if (ans == -1) {
+			if (res != -1)ans = res - 1;
+		}
+		else {
+			if (res != -1)ans = min(ans, res - 1);
+		}
+	} while (next_permutation(t.begin(), t.end()));
+	cout << ans;
+}
