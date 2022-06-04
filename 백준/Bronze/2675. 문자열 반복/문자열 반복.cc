0b7c9d22139c51e028572757cac0f9edf8d0cc7b
kariskan <73534426+kariskan@users.noreply.github.com>
2022-06-05 00:17:39 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int a; string s;
+		cin >> a >> s;
+		for (int j = 0; j < s.length(); j++) {
+			for (int k = 0; k < a; k++) {
+				cout << s[j];
+			}
+		}
+		cout << "\n";
+	}
+}
