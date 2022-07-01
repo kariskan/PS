bae99e06e6dffdd45cbbc5ddb6bea38ae032e3d1
kariskan <73534426+kariskan@users.noreply.github.com>
2022-07-02 00:55:52 +0900

    [Gold II] Title: 낚시왕, Time: 136 ms, Memory: 2264 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.cc
+++ 백준/Gold/17143. 낚시왕/낚시왕.cc
@@ -1,40 +1,30 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
-#include <algorithm>
 using namespace std;
 
 typedef struct shark {
-	int i;
-	int j;
 	int speed;
 	int dir;
 	int size;
-	bool deleted;
 };
 
-bool compare(shark& a, shark& b) {
-	return a.i < b.i;
-}
-
 int R, C, M;
-vector<shark> v;
+shark map[102][102], temp[102][102];
 int x[5] = { 0,-1,1,0,0 };
 int y[5] = { 0,0,0,1,-1 };
-int map[101][101], temp[101][101];
-
 void move() {
-	for (int i = 0; i < v.size(); i++) {
-		shark t = v[i];
-		if (t.deleted)continue;
-		int dir = t.dir;
-		int siz = t.size;
-		int a = t.i;
-		int b = t.j;
-		int cnt = t.speed;
-		int speed = cnt;
-
-		if (dir == 1 || dir == 2) {
+	for (int i = 1; i <= R; i++) {
+		for (int j = 1; j <= C; j++) {
+			if (map[i][j].size) {
+				int cnt = map[i][j].speed;
+				int speed = cnt;
+				int dir = map[i][j].dir;
+				int siz = map[i][j].size;
+				int a = i;
+				int b = j;
+				map[i][j] = { 0,0,0 };
+				if (dir == 1 || dir == 2) {
 			int rot = (R - 1) * 2;
 			if (cnt >= rot)cnt %= rot;
 			for (int j = 0; j < cnt; j++) {
@@ -66,23 +56,19 @@ void move() {
 				}
 			}
 		}
-
-		if (temp[a][b]) {
-			if (v[temp[a][b] - 1].size < siz) {
-				v[temp[a][b] - 1].deleted = true;
-				temp[a][b] = i + 1;
-				v[i] = { a,b,speed,dir,siz,false };
+				if (temp[a][b].size) {
+					if (temp[a][b].size < siz) {
+						temp[a][b].speed = speed;
+						temp[a][b].dir = dir;
+						temp[a][b].size = siz;
+					}
+				}
+				else {
+					temp[a][b] = { speed,dir,siz };
+				}
 			}
-			else {
-				v[i].deleted = true;
-			}
-		}
-		else {
-			temp[a][b] = i + 1;
-			v[i] = { a,b,speed,dir,siz,false };
 		}
 	}
-	memset(map, 0, sizeof(map));
 	for (int i = 1; i <= R; i++) {
 		for (int j = 1; j <= C; j++) {
 			map[i][j] = temp[i][j];
@@ -98,24 +84,18 @@ int main() {
 	for (int i = 0; i < M; i++) {
 		int r, c, s, d, z;
 		cin >> r >> c >> s >> d >> z;
-		v.push_back({ r,c,s,d,z,false });
-	}
-	sort(v.begin(), v.end(), compare);
-	for (int i = 0; i < v.size(); i++) {
-		map[v[i].i][v[i].j] = i + 1;
+		map[r][c] = { s,d,z };
 	}
 	int ans = 0;
-	for (int i = 1; i <= C; i++) {
-		for (int j = 0; j < v.size(); j++) {
-			if (v[j].j == i && !v[j].deleted) {
-				ans += v[j].size;
-				v[j].deleted = true;
-				map[v[j].i][v[j].j] = 0;
+	for (int j = 1; j <= C; j++) {
+		for (int i = 1; i <= R; i++) {
+			if (map[i][j].size) {
+				ans += map[i][j].size;
+				map[i][j] = { 0,0,0 };
 				break;
 			}
 		}
 		move();
-		sort(v.begin(), v.end(), compare);
 	}
 	cout << ans;
 }
