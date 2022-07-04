d9d01d355960698fd4247e4c3f2106a46266127a
kariskan <73534426+kariskan@users.noreply.github.com>
2022-07-05 02:53:24 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 4.04 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    int ans;
+    for (int i = 0; i < seoul.size(); i++) {
+        if(seoul[i] == "Kim") {
+            ans = i;
+            break;
+        }
+    }
+    return "김서방은 " + to_string(ans) + "에 있다";
+}
