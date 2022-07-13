739f362d186c68545b77af00c080836308497fe4
kariskan <73534426+kariskan@users.noreply.github.com>
2022-07-13 22:45:38 +0900

    [Gold I] Title: 외판원 순회, Time: 20 ms, Memory: 6116 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.cc
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.cc
@@ -1,32 +1,33 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int n;
-int map[16][16];
-int dp[16][1<<16];
-int inf = 987654321;
-int go(int node, int bit) {
-	if(dp[node][bit]) return dp[node][bit];
-	if (bit == (1 << n) - 1) {
-		if (map[node][0] == 0) {
-			return inf;
-		}
-		return map[node][0];
-	}
-	int res = inf;
-	for (int i = 0; i < n; i++) {
-		if (!(bit & (1 << i)) && map[node][i]) {
-			res = min(res, go(i, bit | (1 << i)) + map[node][i]);
-		}
-	}
-	return dp[node][bit] = res;
+int dp[1<<16][16];
+int board[16][16];
+int dfs(int visited, int now) {
+	if(dp[visited][now]) return dp[visited][now];
+    if(visited == (1 << n) - 1) {
+    	if(board[now][0] == 0) {
+        	return 987654321;
+        }
+        return board[now][0];
+    }
+    int ret = 987654321;
+    
+    for(int i = 0; i < n; i++) {
+    	if(!(visited & (1 << i)) && board[now][i]) {
+        	ret = min(ret, dfs(visited | (1 << i), i) + board[now][i]);
+        }
+    }
+    
+    return dp[visited][now] = ret;
 }
 int main() {
 	cin >> n;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> map[i][j];
-		}
-	}
-
-	cout << go(0, 1);
+    for(int i = 0; i < n; i++) {
+    	for(int j = 0; j < n; j++) {
+        	cin >> board[i][j];
+        }
+    }
+    cout << dfs(1, 0);
 }
