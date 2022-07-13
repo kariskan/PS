d9b0d2c06fe37c1d5c6a481cef5c5471c32621b2
kariskan <73534426+kariskan@users.noreply.github.com>
2022-07-13 17:19:26 +0900

    [Gold I] Title: 최솟값 찾기, Time: 1468 ms, Memory: 63120 KB -BaekjoonHub

--- 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.cc
+++ 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.cc
@@ -1,10 +1,30 @@
+//#include <iostream>
+//#include <queue>
+//using namespace std;
+//
+//int n, l;
+//int a[5000001];
+//priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+//int main() {
+//	ios_base::sync_with_stdio(0);
+//	cin.tie(0);
+//	cout.tie(0);
+//	cin >> n >> l;
+//	for (int i = 1; i <= n; i++) {
+//		cin >> a[i];
+//		q.push({ a[i], i });
+//		while (q.top().second < i - l + 1)q.pop();
+//		cout << q.top().first << ' ';
+//	}
+//}
+
 #include <iostream>
-#include <queue>
+#include <deque>
 using namespace std;
 
 int n, l;
 int a[5000001];
-priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+deque<pair<int, int>> dq;
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
@@ -12,8 +32,9 @@ int main() {
 	cin >> n >> l;
 	for (int i = 1; i <= n; i++) {
 		cin >> a[i];
-		q.push({a[i], i});
-		while (q.top().second < i - l + 1)q.pop();
-		cout << q.top().first << ' ';
+		while (!dq.empty() && dq.back().first > a[i]) dq.pop_back();
+		while (!dq.empty() && dq.front().second < i - l + 1)dq.pop_front();
+		dq.push_back({ a[i], i });
+		cout << dq.front().first << ' ';
 	}
 }
