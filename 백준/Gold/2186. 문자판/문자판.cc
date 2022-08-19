003223bfaebccc948e7e403ccba89e86e8d4eb4a
kariskan <73534426+kariskan@users.noreply.github.com>
2022-08-19 16:09:58 +0900

    [Gold III] Title: 문자판, Time: 1776 ms, Memory: 5200 KB -BaekjoonHub

---
+++ 백준/Gold/2186. 문자판/문자판.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int n, m, c, dp[100][100][81];
+char map[100][100];
+int x[4] = { 0,0,1,-1 };
+int y[4] = { 1,-1,0,0 };
+string t;
+
+int go(int i, int j, int l, string s) {
+	
+	if (s == t)return 1;
+	if (s.length() > t.length())return 0;
+	if (dp[i][j][l] != -1)return dp[i][j][l];
+
+	int ret = 0;
+
+	for (int k = 0; k < 4; k++) {
+		for (int cnt = 1; cnt <= c; cnt++) {
+			int nx = i + x[k] * cnt;
+			int ny = j + y[k] * cnt;
+			string temp = s + map[nx][ny];
+			if (nx >= 0 && nx < n && ny >= 0 && ny < m && map[nx][ny] == t[s.length()]) {
+				ret += go(nx, ny, l + 1, temp);
+			}
+		}
+	}
+
+	return dp[i][j][l] = ret;
+}
+
+int main() {
+
+	cin >> n >> m >> c;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> map[i][j];
+		}
+	}
+
+	memset(dp, -1, sizeof(dp));
+
+	cin >> t;
+	int ans = 0;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (t[0] == map[i][j]) {
+				string temp(1, map[i][j]);
+				ans += go(i, j, 1, temp);
+			}
+		}
+	}
+
+	cout << ans;
+	return 0;
+}
