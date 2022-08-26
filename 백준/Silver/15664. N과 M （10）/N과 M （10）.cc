64913e5537b663f70da299f2599fcd0a67ff3da0
kariskan <73534426+kariskan@users.noreply.github.com>
2022-08-26 18:43:16 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int n, m, a[8];
+vector<vector<int>> ans;
+void go(vector<int> v, int idx) {
+	if (v.size() == m) {
+		ans.push_back(v);
+		return;
+	}
+
+	for (int i = idx; i < n; i++) {
+		v.push_back(a[i]);
+		go(v, i + 1);
+		v.pop_back();
+	}
+	
+}
+
+int main() {
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	sort(a, a + n);
+	go({}, 0);
+
+	sort(ans.begin(), ans.end());
+	ans.erase(unique(ans.begin(), ans.end()), ans.end());
+
+	for (auto& i : ans) {
+		for (auto& j : i) {
+			cout << j << ' ';
+		}
+		cout << '\n';
+	}
+}
