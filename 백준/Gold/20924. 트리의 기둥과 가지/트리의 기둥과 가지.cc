00b3facd9c8955ad932e42e21dea9b68c3a68997
kariskan <73534426+kariskan@users.noreply.github.com>
2022-09-05 17:05:01 +0900

    [Gold IV] Title: 트리의 기둥과 가지, Time: 300 ms, Memory: 25292 KB -BaekjoonHub

---
+++ 백준/Gold/20924. 트리의 기둥과 가지/트리의 기둥과 가지.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, r, g;
+vector<vector<pair<int, int>>> v;
+
+void findG(int node, int pre) {
+	if ((node == r && v[node].size() > 1 ) || v[node].size() > 2) {
+		g = node;
+		return;
+	}
+
+	g = node;
+
+	for (auto& i : v[node]) {
+		if (i.first != pre) {
+			findG(i.first, node);
+			if (g != 0)return;
+		}
+	}
+}
+
+int getgi(int node, int pre) {
+	if (node == g)return 0;
+
+	for (auto& i : v[node]) {
+		if (i.first != pre) {
+			return getgi(i.first, node) + i.second;
+		}
+	}
+}
+
+int getga(int node, int pre, bool ok) {
+
+	if (node == g) {
+		ok = true;
+	}
+
+	int ret = 0;
+
+	for (auto& i : v[node]) {
+		if (i.first != pre) {
+			if (ok) {
+				ret = max(ret, getga(i.first, node, ok) + i.second);
+			}
+			else ret = max(ret, getga(i.first, node, ok));
+		}
+	}
+
+	return ret;
+}
+
+int main() {
+	cin >> n >> r;
+
+	v.resize(n + 1);
+
+	for (int i = 0; i < n - 1; i++) {
+		int a, b, c; cin >> a >> b >> c;
+		v[a].push_back({ b,c });
+		v[b].push_back({ a,c });
+	}
+
+	if (v[r].size() > 1) g = r;
+	else findG(r, 0);
+	cout << getgi(r, 0) << ' ';
+	cout << getga(r, 0, false);
+}
