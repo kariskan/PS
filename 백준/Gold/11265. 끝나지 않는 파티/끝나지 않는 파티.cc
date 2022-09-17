03f7981261c720b47896eac42f8fa3966a17576c
kariskan <73534426+kariskan@users.noreply.github.com>
2022-09-17 17:04:14 +0900

    [Gold V] Title: 끝나지 않는 파티, Time: 48 ms, Memory: 3000 KB -BaekjoonHub

---
+++ 백준/Gold/11265. 끝나지 않는 파티/끝나지 않는 파티.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int n, m, map[501][501];
+
+int main() {
+    
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> map[i][j];
+		}
+	}
+
+	for (int k = 1; k <= n; k++) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (map[i][k] && map[k][j]) {
+					if (map[i][j] == 0 || map[i][j] > map[i][k] + map[k][j]) map[i][j] = map[i][k] + map[k][j];
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < m; i++) {
+		int a, b, c; cin >> a >> b >> c;
+		if (map[a][b] <= c) {
+			cout << "Enjoy other party\n";
+		}
+		else cout << "Stay here\n";
+	}
+}
