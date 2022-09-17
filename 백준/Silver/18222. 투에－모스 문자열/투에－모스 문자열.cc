74f442a3c7d2df351348fe811c9bce265b086928
kariskan <73534426+kariskan@users.noreply.github.com>
2022-09-17 15:49:22 +0900

    [Silver II] Title: 투에-모스 문자열, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Silver/18222. 투에－모스 문자열/투에－모스 문자열.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+long long k;
+
+void go(long long n, int cnt) {
+	if (n == 1) {
+		if (cnt % 2 == 0)cout << 0;
+		else cout << 1;
+		return;
+	}
+	else if (n == 2) {
+		if (cnt % 2 == 0)cout << 1;
+		else cout << 0;
+		return;
+	}
+	for (long long i = 60; i >= 1; i--) {
+		if ((long long)pow(2, i) < n) {
+			go(n - (long long)pow(2, i), 1 - cnt);
+			break;
+		}
+	}
+}
+
+int main() {
+	cin >> k;
+
+	go(k, 0);
+}
