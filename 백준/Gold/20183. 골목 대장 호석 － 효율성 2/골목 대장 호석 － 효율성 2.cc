2020d3adf07ca6e6a4dbbfbed4fb8c40929b0c6c
kariskan <73534426+kariskan@users.noreply.github.com>
2023-01-11 16:29:17 +0900

    [Gold II] Title: 골목 대장 호석 - 효율성 2, Time: 1484 ms, Memory: 33956 KB -BaekjoonHub

---
+++ 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.cc
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <climits>
+#include <cstring>
+using namespace std;
+
+long long n, m, a, b, c;
+long long disC[100001];
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> n >> m >> a >> b >> c;
+	vector<vector<pair<int, long long>>> v(n + 1);
+	long long start = 1, end = c;
+
+	for (int i = 0; i < m; i++)
+	{
+		int n1, n2;
+		long long cost;
+		cin >> n1 >> n2 >> cost;
+		v[n1].push_back({n2, cost});
+		v[n2].push_back({n1, cost});
+	}
+
+	long long ans = -1;
+
+	while (start <= end)
+	{
+		long long mid = (start + end) / 2;
+		for (int i = 1; i <= n; i++)
+		{
+			disC[i] = 1e15;
+		}
+
+		priority_queue<pair<int, int>> pq;
+		pq.push({0, a});
+		disC[a] = 0;
+
+		while (!pq.empty())
+		{
+			int node = pq.top().second;
+			long long cost = -pq.top().first;
+			pq.pop();
+
+			if (disC[node] < cost)
+				continue;
+
+			for (auto &i : v[node])
+			{
+				int nextNode = i.first;
+				long long nextCost = i.second + cost;
+				if (i.second > mid || nextCost > c || (disC[nextNode] <= nextCost))
+				{
+					continue;
+				}
+				pq.push({-nextCost, nextNode});
+				disC[nextNode] = nextCost;
+			}
+		}
+
+		if (disC[b] != 1e15)
+		{
+			ans = mid;
+			end = mid - 1;
+		}
+		else
+		{
+			start = mid + 1;
+		}
+	}
+	cout << ans;
+}
