bf4808ac942213cfd1900694166618f72b0c38d1
kariskan <73534426+kariskan@users.noreply.github.com>
2023-02-10 13:46:13 +0900

    [Gold V] Title: 용액 합성하기, Time: 60 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/14921. 용액 합성하기/용액 합성하기.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int a[100001];
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> a[i];
+	}
+
+	int left = 1, right = n;
+	int ans = 200000000;
+	while (left < right)
+	{
+		int s = a[right] + a[left];
+		if (abs(ans) > abs(s))
+		{
+			ans = s;
+		}
+
+		if (s < 0)
+		{
+			left++;
+		}
+		else
+		{
+			right--;
+		}
+	}
+
+	cout << ans;
+}
