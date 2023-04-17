cbd688582fc8af34ac6b8264e2f16f81d485ae5e
kariskan <73534426+kariskan@users.noreply.github.com>
2023-04-17 15:21:19 +0900

    [Silver I] Title: 최대공약수, Time: 280 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1850. 최대공약수/최대공약수.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+long long gcd(long long a, long long b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+int main() {
+    long long a, b;
+    cin >> a >> b;
+    long long g = gcd(a, b);
+
+    while (g--) {
+        cout << 1;
+    }
+}
