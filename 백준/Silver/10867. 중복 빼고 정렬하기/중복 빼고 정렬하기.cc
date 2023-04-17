102127d064d34abc3ffe60f4982ccb879cfc4ea8
kariskan <73534426+kariskan@users.noreply.github.com>
2023-04-17 15:23:10 +0900

    [Silver V] Title: 중복 빼고 정렬하기, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.cc
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    v.erase(unique(v.begin(), v.end()), v.end());
+    for (auto& i : v) {
+        cout << i << ' ';
+    }
+}
