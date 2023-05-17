b3fb0e75d9f8ebc233c285d5c75d9453e4e7cd72
kariskan <73534426+kariskan@users.noreply.github.com>
2023-05-17 10:23:20 +0900

    [Silver III] Title: 동전 게임, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10837. 동전 게임/동전 게임.cc
+++ 백준/Silver/10837. 동전 게임/동전 게임.cc
@@ -7,16 +7,18 @@ int main() {
     for (int i = 0; i < m; i++) {
         int a, b;
         cin >> a >> b;
+        int diff = abs(a - b);
+        int remain = n - max(a, b);
         if (a == b) {
             cout << "1\n";
         } else if (a < b) {
-            if (n - max(a, b) >= abs(b - a) - 1) {
+            if (diff - remain <= 1) {
                 cout << "1\n";
             } else {
                 cout << "0\n";
             }
         } else {
-            if (n - max(a, b) >= abs(b - a) - 2) {
+            if (diff - remain <= 2) {
                 cout << "1\n";
             } else {
                 cout << "0\n";
