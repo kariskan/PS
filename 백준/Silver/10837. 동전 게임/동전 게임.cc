fdb5bced0b4bb52ea4857ee81f76255d2b8e35cc
kariskan <73534426+kariskan@users.noreply.github.com>
2023-05-17 10:25:45 +0900

    [Silver III] Title: 동전 게임, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10837. 동전 게임/동전 게임.cc
+++ 백준/Silver/10837. 동전 게임/동전 게임.cc
@@ -1,28 +1,58 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int main() {
-    int n, m;
-    cin >> n >> m;
-    for (int i = 0; i < m; i++) {
-        int a, b;
-        cin >> a >> b;
-        int diff = abs(a - b);
-        int remain = n - max(a, b);
-        if (a == b) {
-            cout << "1\n";
-        } else if (a < b) {
-            if (diff - remain <= 1) {
-                cout << "1\n";
-            } else {
-                cout << "0\n";
-            }
-        } else {
-            if (diff - remain <= 2) {
-                cout << "1\n";
-            } else {
-                cout << "0\n";
-            }
-        }
-    }
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int K;
+	cin >> K;
+
+	int C;
+	cin >> C;
+
+	for (int c = 0; c < C; c++)
+	{
+		int M, N;
+		cin >> M >> N;
+
+		// 영희와 동수 점수가 같다면 100% 가능
+		if (M - N == 0)
+		{
+			cout << 1 << "\n";
+
+			continue;
+		}
+
+		int remainGames = K - max(M, N);
+		int diff = abs(M - N);
+
+		// 영희 점수가 더 높을 경우
+		if (M > N)
+		{
+			if (diff - remainGames <= 2)
+			{
+				cout << 1 << "\n";
+
+				continue;
+			}
+
+			cout << 0 << "\n";
+
+			continue;
+		}
+
+		// 동수 점수가 더 높을 경우
+		if (diff - remainGames <= 1)
+		{
+			cout << 1 << "\n";
+
+			continue;
+		}
+
+		cout << 0 << "\n";
+	}
+
+	return 0;
 }
