175188e203bc3ed258c71eba0645e93f0404bfa1
kariskan <73534426+kariskan@users.noreply.github.com>
2023-07-07 11:53:00 +0900

    [Gold IV] Title: 수 분해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1437. 수 분해/수 분해.cc
+++ 백준/Gold/1437. 수 분해/수 분해.cc
@@ -1,21 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int dp[1000001];
-
-int go(int n) {
-    if (dp[n]) {
-        return dp[n];
-    }
-    if (n <= 4) {
-        return n;
-    }
-    
-    return dp[n] = (go(n - 3) * 3) % 10007;
-}
-
 int main() {
+	
     int n;
     cin >> n;
-    cout << go(n);
+    long long ans = 1;
+    while (n >= 5) {
+    	ans = (ans * 3) % 10007;
+        n -= 3;
+    }
+    
+    ans = (ans * n) % 10007;
+    cout << ans;
 }
