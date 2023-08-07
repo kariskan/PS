fbd215e133dc855c2f67982bb7d7c6eb2d909912
kariskan <73534426+kariskan@users.noreply.github.com>
2023-08-07 10:27:13 +0900

    [Silver I] Title: 회전 초밥, Time: 708 ms, Memory: 2136 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.cc
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.cc
@@ -1,43 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int a[30000], v[3001];
+int a[30000];
 
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	int n, d, k, c;
-	cin >> n >> d >> k >> c;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n, d, k, c;
+    // 접시 수, 초밥 가지 수, 연속해서 먹는 초밥 수, 쿠폰 번호
+    cin >> n >> d >> k >> c;
 
-	for (int i = 0; i < n; i++)
-	{
-		cin >> a[i];
-	}
-	int ans = 0;
-	for (int i = 0; i < k; i++)
-	{
-		v[a[i]]++;
-	}
-	for (int i = 1; i < n; i++)
-	{
-		int left = i, right = (i + k - 1) % n;
-		v[a[left - 1]]--;
-		v[a[right]]++;
-		int cnt = 0;
-		for (int j = 1; j <= d; j++)
-		{
-			if (v[j])
-			{
-				cnt++;
-			}
-		}
-		if (!v[c])
-		{
-			cnt++;
-		}
-		ans = max(ans, cnt);
-	}
-	cout << ans;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i]; // 초밥
+    }
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        int t = k, idx = i;
+        int temp[3001] = {0};
+        int tt = 0;
+        while (t--) {
+            if (!temp[a[idx]]) {
+                tt++;
+            }
+            temp[a[idx]]++;
+            idx = (idx + 1) % n;
+        }
+
+        ans = max(ans, temp[c] ? tt : tt + 1);
+    }
+    cout << ans;
 }
