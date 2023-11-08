3bca893ed47676434401da7df295c3a7b3c32cdc
kariskan <73534426+kariskan@users.noreply.github.com>
2023-11-08 11:29:16 +0900

    [Silver I] Title: 단축키 지정, Time: 0 ms, Memory: 2084 KB -BaekjoonHub

--- 백준/Silver/1283. 단축키 지정/README.md
+++ 백준/Silver/1283. 단축키 지정/README.md
@@ -1,14 +1,18 @@
-# [Silver II] 단축키 지정 - 1283 
+# [Silver I] 단축키 지정 - 1283 
 
 [문제 링크](https://www.acmicpc.net/problem/1283) 
 
 ### 성능 요약
 
-메모리: 2032 KB, 시간: 0 ms
+메모리: 2084 KB, 시간: 0 ms
 
 ### 분류
 
-구현(implementation), 문자열(string)
+구현, 문자열
+
+### 제출 일자
+
+2023년 11월 7일 10:36:28
 
 ### 문제 설명
 
--- 백준/Silver/1283. 단축키 지정/단축키 지정.cc
+++ 백준/Silver/1283. 단축키 지정/단축키 지정.cc
@@ -1,99 +1,69 @@
-#include <set>
 #include <iostream>
+#include <set>
+#include <sstream>
 #include <vector>
 using namespace std;
 
-set<char> se;
-
-vector<string> parsed(string s)
-{
-    string res = "";
-    vector<string> ret;
-    for (int i = 0; i < s.length(); i++)
-    {
-        if (s[i] == ' ')
-        {
-            ret.push_back(res);
-            res = "";
-            continue;
-        }
-        res += s[i];
+char toAnotherCase(char c) {
+    if (c >= 'A' && c <= 'Z') {
+        return c - 'A' + 'a';
     }
-    if (!res.empty())
-        ret.push_back(res);
-    return ret;
+    return c - 'a' + 'A';
 }
 
-bool go(char c)
-{
-    int a;
-    if (c >= 'a' && c <= 'z')
-    {
-        a = c - 'a';
-    }
-    else
-    {
-        a = c - 'A';
-    }
-
-    return se.find(a + 'a') == se.end() && se.find(a + 'A') == se.end();
-}
-
-int main()
-{
+int main() {
     int n;
     cin >> n;
-    cin.get();
 
-    for (int i = 0; i < n; i++)
-    {
+    set<char> se;
+
+    getchar();
+    for (int i = 0; i < n; i++) {
         string s;
         getline(cin, s);
-        vector<string> v = parsed(s);
-        int ok = 0;
-        for (int j = 0; j < v.size(); j++)
-        {
-            if (go(v[j][0]))
-            {
-                for(int k=0;k<j;k++){
-                    cout<<v[k]<<' ';
-                }
-                se.insert(v[j][0]);
-                cout << "[" << v[j][0] << "]";
-                for (int k = 1; k < v[j].size(); k++)
-                {
-                    cout << v[j][k];
-                }
-                cout << ' ';
-                for (int k = j + 1; k < v.size(); k++)
-                {
-                    cout << v[k] << ' ';
-                }
-                ok = 1;
+
+        vector<string> split;
+
+        stringstream ss(s);
+        string temp;
+        while (getline(ss, temp, ' ')) {
+            split.push_back(temp);
+        }
+
+        int idx = -1, len = 0;
+        for (auto &i : split) {
+            if (se.find(i[0]) == se.end() &&
+                se.find(toAnotherCase(i[0])) == se.end()) {
+                se.insert(i[0]);
+                idx = len;
                 break;
             }
+            len += i.length() + 1;
         }
-        if (!ok)
-        {
-            for (int j = 0; j < s.length(); j++)
-            {
-                if (s[j] == ' ')
-                {
-                    cout << ' ';
+
+        if (idx == -1) {
+            for (int i = 0; i < s.length(); i++) {
+                if (s[i] == ' ') {
                     continue;
                 }
-                if (go(s[j]))
-                {
-                    se.insert(s[j]);
-                    cout << "[" << s[j] << "]";
-                    for(int k=j+1;k<s.length();k++){
-                        cout<<s[k];
-                    }
+                if (se.find(s[i]) == se.end() &&
+                    se.find(toAnotherCase(s[i])) == se.end()) {
+                    se.insert(s[i]);
+                    idx = i;
                     break;
                 }
-                cout << s[j];
             }
         }
-        cout<<'\n';
+
+        for (int i = 0; i < s.length(); i++) {
+            if (idx == i) {
+                cout << "[" << s[i] << "]";
+            }
+            else {
+                cout << s[i];
+            }
+        }
+
+        cout << '\n';
     }
 }
