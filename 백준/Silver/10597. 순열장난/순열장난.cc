07c6d433cf46b76836d2f370780c898f700899e9
kariskan <73534426+kariskan@users.noreply.github.com>
2023-11-13 10:30:03 +0900

    [Silver I] Title: 순열장난, Time: 16 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10597. 순열장난/순열장난.cc
+++ 백준/Silver/10597. 순열장난/순열장난.cc
@@ -1,58 +1,55 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
-int vis[55], lenv[55], alen;
+
+int check[100], maxNum;
 string s;
-bool go(int idx, int maxNum, string ans)
-{
-	if (idx >= s.length())
-	{
-		for (int i = 1; i <= maxNum; i++)
-		{
-			if (!vis[i])
-			{
-				return false;
-			}
-		}
-		cout << ans.substr(1);
-		return true;
-	}
-	string t = "";
-	for (int i = idx; i < s.length(); i++)
-	{
-		t += s[i];
-		int numT = stoi(t);
-		if (alen < numT)
-		{
-			return false;
-		}
-		if (!vis[numT])
-		{
-			vis[numT] = 1;
-			if (go(i + 1, max(maxNum, numT), ans + " " + t))
-			{
-				return true;
-			}
-			vis[numT] = 0;
-		}
-	}
-	return false;
+
+bool ok() {
+    for (int i = 1; i <= maxNum; i++) {
+        if (!check[i]) {
+            return false;
+        }
+    }
+    return true;
 }
-int main()
-{
-	int len = 0;
-	for (int i = 1; i <= 50; i++)
-	{
-		len += to_string(i).length();
-		lenv[i] = len;
-	}
-	cin >> s;
-	for (int i = 1; i <= 50; i++)
-	{
-		if (lenv[i] == s.length())
-		{
-			alen = i;
-		}
-	}
-	go(0, 0, "");
+
+void go(string seq, int idx) {
+    if (s[idx] == '0') {
+        return;
+    }
+    if (idx >= s.length()) {
+        if (ok()) {
+            cout << seq;
+            exit(0);
+        }
+    }
+
+    string now = "";
+    for (int i = idx; i < s.length(); i++) {
+        now += s[i];
+        int n = stoi(now);
+        if (n > maxNum) {
+            break;
+        }
+        if (n <= maxNum && n > 0 && !check[n]) {
+            check[n] = 1;
+            go(seq + now + " ", i + 1);
+            check[n] = 0;
+        }
+    }
+}
+
+int main() {
+    cin >> s;
+    string t = "";
+    for (int i = 1;; i++) {
+        t += to_string(i);
+        if (t.length() == s.length()) {
+            maxNum = i;
+            break;
+        }
+    }
+    go("", 0);
 }
