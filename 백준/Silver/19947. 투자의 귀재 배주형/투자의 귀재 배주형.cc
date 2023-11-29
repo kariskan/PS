035ba39b1ab2381efb938ba35a27885bb2ae1090
kariskan <73534426+kariskan@users.noreply.github.com>
2023-11-29 10:24:30 +0900

    [Silver V] Title: 투자의 귀재 배주형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.cc
+++ 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.cc
@@ -1,26 +1,23 @@
 #include <iostream>
+
 using namespace std;
+
 int dp[11];
-int main()
-{
-    int n, m;
-    cin >> n >> m;
-    dp[0] = n;
-    for (int i = 1; i <= 10; i++)
-    {
-        if (i - 1 >= 0)
-        {
-            dp[i] = max(dp[i], (int)(dp[i - 1] * 1.05));
+
+int main() {
+    int h, y;
+    cin >> h >> y;
+
+    dp[0] = h;
+
+    for (int i = 1; i <= y; i++) {
+        dp[i] = dp[i - 1] * 1.05;
+        if (i >= 3) {
+            dp[i] = max(dp[i], (int) (dp[i - 3] * 1.2));
         }
-        if (i - 3 >= 0)
-        {
-            dp[i] = max(dp[i], (int)(dp[i - 3] * 1.20));
-        }
-        if (i - 5 >= 0)
-        {
-            dp[i] = max(dp[i], (int)(dp[i - 5] * 1.35));
+        if (i >= 5) {
+            dp[i] = max(dp[i], (int) (dp[i - 5] * 1.35));
         }
     }
-    
-    cout << dp[m];
+    cout << dp[y];
 }
