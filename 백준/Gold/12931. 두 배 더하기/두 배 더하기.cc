b9ee76bca085589d28676b4f6ccd4cb34b85e1e0
kariskan <73534426+kariskan@users.noreply.github.com>
2023-12-01 10:11:28 +0900

    [Gold V] Title: 두 배 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/12931. 두 배 더하기/두 배 더하기.cc
+++ 백준/Gold/12931. 두 배 더하기/두 배 더하기.cc
@@ -1,51 +1,42 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 using namespace std;
 
-int n;
-int a[50], b[50];
-vector <vector<int>> seq(1001);
-bool compare(int a1, int a2) {
-	return a1 > a2;
-}
-void run(int num) {
-	int temp = num;
-	int idx = 0;
-	while (temp) {
-		if (temp % 2 == 0) {
-			seq[num].push_back(2);
-			temp /= 2;
-		}
-		else {
-			seq[num].push_back(1);
-			temp--;
+int a[50], n;
+
+bool check() {
+	for (int i = 0; i < n; i++) {
+		if (a[i] != 0) {
+			return false;
 		}
 	}
+	return true;
 }
+
 int main() {
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		cin >> b[i];
+		cin >> a[i];
 	}
-	for (int i = 1; i <= 1000; i++) {
-		run(i);
-	}
-	sort(b, b + n, compare);
-	int plusSum = 0, maxDouble = 0;
-	for (int i = 0; i < n; i++) {
-		if (b[i]) {
-			int tempDouble = 0;
-			for (int k : seq[b[i]]) {
-				if (k == 1) {
-					plusSum++;
-				}
-				else {
-					tempDouble++;
-				}
+
+	int ans = 0;
+	while (!check()) {
+		for (int i = 0; i < n; i++) {
+			if (a[i] % 2 == 1) {
+				a[i]--;
+				ans++;
 			}
-			maxDouble = max(maxDouble, tempDouble);
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (a[i]) {
+				ans++;
+				for (int j = i; j < n; j++) {
+					a[j] /= 2;
+				}
+				break;
+			}
 		}
 	}
-	cout << plusSum + maxDouble;
+
+	cout << ans;
 }
