87b673d4be06155ab8996dc47523dcdfb73674ad
kariskan <73534426+kariskan@users.noreply.github.com>
2023-12-14 10:11:52 +0900

    [Silver I] Title: 부분수열의 합, Time: 32 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.cc
@@ -1,31 +1,26 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
+
 using namespace std;
 
-int visit[2000001];
-int a[21];
+int n, a[20], s[2000002];
 
 int main() {
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-	}
-	vector<int> v;
-	for (int i = 1; i < (1 << n); i++) {
-		int sum = 0;
-		for (int j = 0; j < n; j++) {
-			if (i & (1 << j)) {
-				sum += a[j];
-			}
-		}
-		visit[sum] = 1;
-	}
-	for (int i = 1; i <= 2000000; i++) {
-		if (!visit[i]) {
-			cout << i;
-			return 0;
-		}
-	}
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    s[0] = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 2000001 - a[i]; j >= 0; j--) {
+            if (s[j]) {
+                s[j + a[i]] = 1;
+            }
+        }
+    }
+    for (int i = 1; i <= 2000001; i++) {
+        if (!s[i]) {
+            cout << i;
+            return 0;
+        }
+    }
 }
