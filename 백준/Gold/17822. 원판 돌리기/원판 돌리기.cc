f31b1a1b5125f33a9cd471f3bc6fcd93540030bd
kariskan <73534426+kariskan@users.noreply.github.com>
2023-12-20 11:48:00 +0900

    [Gold II] Title: 원판 돌리기, Time: 4 ms, Memory: 2428 KB -BaekjoonHub

--- 백준/Gold/17822. 원판 돌리기/원판 돌리기.cc
+++ 백준/Gold/17822. 원판 돌리기/원판 돌리기.cc
@@ -1,122 +1,100 @@
 #include <iostream>
-#include <queue>
+#include <algorithm>
+#include <vector>
+
 using namespace std;
 
-int n, m, t, x, d, k;
-int circle[51][51];  // i번째에 오른쪽 기준(i, 0)으로, 시계 방향으로 작성
+int n, m, t, map[50][50];
 
-void rotateClockwise(int idx, int cnt) {  // 1, 3, 5, 4 -> 4, 1, 3, 5
-    for (int i = 0; i < cnt; i++) {
-        int last = circle[idx][m];
-        for (int j = m; j >= 2; j--) {
-            circle[idx][j] = circle[idx][j - 1];
-        }
-        circle[idx][1] = last;
-    }
-}
-
-void rotateCounterClockwise(int idx, int cnt) {
-    for (int i = 0; i < cnt; i++) {
-        int first = circle[idx][1];
-        for (int j = 1; j < m; j++) {
-            circle[idx][j] = circle[idx][j + 1];
-        }
-        circle[idx][m] = first;
-    }
-}
-
-double getAvg() {
-    int res = 0, cnt = 0;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            if (circle[i][j] != 0) {
-                cnt++;
-                res += circle[i][j];
-            }
+void rotate(int x, int d, int k) {
+    int newMap[50];
+    for (int i = 0; i < m; i++) {
+        if (d == 0) {
+            newMap[i] = map[x][(i - k + m) % m];
+        } else {
+            newMap[i] = map[x][(i + k) % m];
         }
     }
-    return res / (cnt * 1.0);
-}
-
-void eraseAdjecant() {
-    queue<pair<int, int>> erase;
-
-    for (int c = 1; c <= n; c++) {
-        for (int i = 1; i <= m; i++) {
-            for (int j = c - 1; j >= 1; j--) {  // 반지름이 작은 원 체크
-                if (circle[c][i] != circle[j][i] || circle[c][i] == 0) {
-                    break;
-                }
-                erase.push({j, i});
-            }
-            for (int j = c + 1; j <= n; j++) {  // 반지름이 큰 원 체크
-                if (circle[c][i] != circle[j][i] || circle[c][i] == 0) {
-                    break;
-                }
-                erase.push({j, i});
-            }
-            if (i != m && circle[c][i] == circle[c][i + 1] && circle[c][i] != 0) {
-                erase.push({c, i});
-                erase.push({c, i + 1});
-            }
-        }
-        if (circle[c][1] == circle[c][m] && circle[c][1] != 0) {
-            erase.push({c, 1});
-            erase.push({c, m});
-        }
-    }
-    if (!erase.empty()) {
-        while (!erase.empty()) {
-            int x = erase.front().first;
-            int y = erase.front().second;
-            erase.pop();
-
-            circle[x][y] = 0;
-        }
-    } else {
-        double avg = getAvg();
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (circle[i][j] == 0) {
-                    continue;
-                }
-                if (avg < circle[i][j]) {
-                    circle[i][j]--;
-                } else if (avg > circle[i][j]) {
-                    circle[i][j]++;
-                }
-            }
-        }
+    for (int i = 0; i < m; i++) {
+        map[x][i] = newMap[i];
     }
 }
 
 int main() {
     cin >> n >> m >> t;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            cin >> circle[i][j];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> map[i][j];
         }
     }
-
-    for (int i = 0; i < t; i++) {
+    while (t--) {
+        int x, d, k;
         cin >> x >> d >> k;
-        for (int j = x; j <= n; j += x) {
-            if (d == 0) {  // 시계
-                rotateClockwise(j, k);
-            } else {
-                rotateCounterClockwise(j, k);
+        for (int i = x - 1; i < n; i += x) {
+            rotate(i, d, k);
+        }
+        vector<pair<int, int>> v;
+        int sum = 0, cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 0) {
+                    continue;
+                }
+                sum += map[i][j];
+                cnt++;
+                if (map[i][j] == map[i][(j - 1 + m) % m]) {
+                    v.push_back({i, j});
+                    v.push_back({i, (j - 1 + m) % m});
+                }
+                if (map[i][j] == map[i][(j + 1) % m]) {
+                    v.push_back({i, j});
+                    v.push_back({i, (j + 1) % m});
+                }
+                if (i < n - 1 && map[i][j] == map[i + 1][j]) {
+                    v.push_back({i, j});
+                    v.push_back({i + 1, j});
+                }
+                if (i > 0 && map[i][j] == map[i - 1][j]) {
+                    v.push_back({i, j});
+                    v.push_back({i - 1, j});
+                }
             }
         }
-        eraseAdjecant();
-    }
-
-    int ans = 0;
-
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            ans += circle[i][j];
+        sort(v.begin(), v.end());
+        v.erase(unique(v.begin(), v.end()), v.end());
+        if (!v.empty()) {
+            while (!v.empty()) {
+                map[v.back().first][v.back().second] = 0;
+                v.pop_back();
+            }
+        } else {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (map[i][j] == 0) {
+                        continue;
+                    }
+                    if (sum % cnt == 0) {
+                        if (sum / cnt > map[i][j]) {
+                            map[i][j]++;
+                        } else if (sum / cnt < map[i][j]) {
+                            map[i][j]--;
+                        }
+                    } else {
+                        if (sum / cnt >= map[i][j]) {
+                            map[i][j]++;
+                        } else {
+                            map[i][j]--;
+                        }
+                    }
+                }
+            }
         }
     }
-
-    cout << ans;
+    int s = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            s += map[i][j];
+        }
+    }
+    cout << s;
 }
