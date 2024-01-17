0a8ed8c70634df4478cc23a37bc23a5d210a580d
kariskan <73534426+kariskan@users.noreply.github.com>
2024-01-17 12:38:29 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int solution(int left, int right) {
+    int answer = 0;
+    for(int i=left;i<=right;i++){
+        int cnt = 0;
+        if ((int)sqrt(i)*(int)sqrt(i)==i){
+            answer-=i;
+        }else{
+            answer+=i;
+        }
+    }
+    return answer;
+}
