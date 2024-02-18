93c758c5ff4ba172bb36624903722ed78be15ce8
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-18 14:54:57 +0900

    [level 2] Title: 요격 시스템, Time: 0.01 ms, Memory: 3.74 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/README.md
+++ 프로그래머스/2/181188. 요격 시스템/README.md
@@ -1,10 +1,10 @@
 # [level 2] 요격 시스템 - 181188 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181188) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181188?language=cpp) 
 
 ### 성능 요약
 
-메모리: 3.67 MB, 시간: 0.01 ms
+메모리: 3.74 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 17:55:39
+2024년 2월 0일 14:54:55
 
 ### 문제 설명
 
--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
@@ -1,25 +1,22 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
-
-bool compare(vector<int>& v1, vector<int>& v2) {
-    return v1[1] < v2[1];
+bool compare(vector<int>& v1, vector<int>& v2){
+    return v1[1]<v2[1];
 }
-
 int solution(vector<vector<int>> targets) {
     int answer = 0;
-    
-    sort(targets.begin(), targets.end(), compare);
-    int pre = -1;
+    sort(targets.begin(),targets.end(),compare);
+	priority_queue<int> q;
     for(int i=0;i<targets.size();i++){
-        int start=targets[i][0];
-        int end=targets[i][1];
-        if(start>=pre){
-            answer++;
-            pre=end;
+        int idx=i+1;
+        while(idx<targets.size()&&targets[idx][0]<targets[i][1]){
+            idx++;
         }
+        answer++;
+        i=idx-1;
     }
-    
     return answer;
 }
