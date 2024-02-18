e5463550aa373483f0252921717dc360e02c8dc0
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-18 15:24:13 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 6.99 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/README.md
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 원 사이의 정수 쌍 - 181187 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181187#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181187?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 6.42 ms
+메모리: 4.21 MB, 시간: 6.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 18:59:11
+2024년 2월 0일 15:24:11
 
 ### 문제 설명
 
--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.cpp
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.cpp
@@ -4,17 +4,16 @@
 using namespace std;
 
 long long solution(int r1, int r2) {
-    long long r11 = r1;
-    long long r22 = r2;
-    long long answer = (r22-r11+1)*4;
-    for(long long y=1;y<r22;y++){
-        long long x1 = sqrt(r11*r11-y*y);
-        long long x2 = sqrt(r22*r22-y*y);
-        if(y<r1&&fmod(sqrt(r11*r11-y*y),1)==0){
+    long long R1=r1;
+    long long R2=r2;
+    long long answer = (R2-R1+1)*4;
+    for(long long i=1;i<R2;i++){
+        long long coor1=sqrt(R1*R1-i*i);
+        long long coor2=sqrt(R2*R2-i*i);
+        answer+=(coor2-coor1)*4;
+        if(i<R1&&coor1*coor1==R1*R1-i*i){
             answer+=4;
         }
-        answer += (x2-x1)*4;
     }
-    
     return answer;
 }
