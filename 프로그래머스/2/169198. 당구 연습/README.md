e3a0f6c37d1963331e216158d65a5776c1c2c8b8
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-18 16:23:46 +0900

    [level 2] Title: 당구 연습, Time: 0.19 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/169198. 당구 연습/README.md
+++ 프로그래머스/2/169198. 당구 연습/README.md
@@ -1,10 +1,10 @@
 # [level 2] 당구 연습 - 169198 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/169198) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/169198?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.33 ms
+메모리: 4.13 MB, 시간: 0.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 6일 15:8:10
+2024년 2월 0일 16:23:44
 
 ### 문제 설명
 
--- 프로그래머스/2/169198. 당구 연습/당구 연습.cpp
+++ 프로그래머스/2/169198. 당구 연습/당구 연습.cpp
@@ -1,32 +1,27 @@
 #include <string>
 #include <vector>
-
+#include <climits>
 using namespace std;
-
-int getDis(int x, int y, int x2, int y2){
-    return (x-x2)*(x-x2)+(y-y2)*(y-y2);
-}
 
 vector<int> solution(int m, int n, int startX, int startY, vector<vector<int>> balls) {
     vector<int> answer;
-    
     for(int i=0;i<balls.size();i++){
-        int x=balls[i][0],y=balls[i][1];
-        int ans=1000000000;
-        if(!(startY==y&&x>startX)){
-            ans=min(ans,getDis(startX,startY,m*2-x,y));
+        int x=balls[i][0];
+        int y=balls[i][1];
+        int p=INT_MAX;
+        if(y!=startY||x>startX){
+            p=min(p,(x+startX)*(x+startX)+(y-startY)*(y-startY));
         }
-        if(!(startY==y&&x<startX)){
-            ans=min(ans,getDis(startX,startY,-x,y));
+        if(y!=startY||x<startX){
+            p=min(p,(2*m-startX-x)*(2*m-startX-x)+(y-startY)*(y-startY));
         }
-        if(!(startX==x&&y<startY)){
-            ans=min(ans,getDis(startX,startY,x,-y));
+        if(x!=startX||y>startY){
+            p=min(p,(x-startX)*(x-startX)+(y+startY)*(y+startY));
         }
-        if(!(startX==x&&y>startY)){
-            ans=min(ans,getDis(startX,startY,x,2*n-y));
+        if(x!=startX||y<startY){
+            p=min(p,(x-startX)*(x-startX)+(2*n-startY-y)*(2*n-startY-y));
         }
-        answer.push_back(ans);
+        answer.push_back(p);
     }
-    
     return answer;
 }
