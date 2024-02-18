1b76d1c5ed73a4fc860cf867d67bcf5c43091cef
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-18 16:10:16 +0900

    [level 2] Title: 리코쳇 로봇, Time: 0.06 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/README.md
+++ 프로그래머스/2/169199. 리코쳇 로봇/README.md
@@ -1,10 +1,10 @@
 # [level 2] 리코쳇 로봇 - 169199 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/169199) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/169199?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.2 MB, 시간: 0.08 ms
+메모리: 4.17 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 6일 14:20:39
+2024년 2월 0일 16:10:14
 
 ### 문제 설명
 
--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.cpp
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.cpp
@@ -2,47 +2,43 @@
 #include <vector>
 #include <queue>
 using namespace std;
-int dx[4]={0,1,0,-1};
-int dy[4]={1,0,-1,0};
-int vis[4][100][100];
+int dx[4]={0,0,1,-1};
+int dy[4]={1,-1,0,0};
 int solution(vector<string> board) {
+    int answer = -1;
+    queue<pair<int,int>> q;
+    int vis[100][100]={0,};
     int n=board.size(),m=board[0].size();
-    int sx,sy;
-    queue<pair<int,pair<int, int>>>q;
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             if(board[i][j]=='R'){
-                sx=i;
-                sy=j;
-                for(int k=0;k<4;k++){
-                    q.push({k,{i,j}});
-                    vis[k][i][j]=1;
-                }
+                vis[i][j]=1;
+                q.push({i,j});
             }
         }
     }
     while(!q.empty()){
-        int dir=q.front().first;
-        int x=q.front().second.first;
-        int y=q.front().second.second;
+        int x=q.front().first;
+        int y=q.front().second;
         q.pop();
         if(board[x][y]=='G'){
-            return vis[dir][x][y]-1;
+            return vis[x][y]-1;
         }
+        
         for(int i=0;i<4;i++){
-            int nx=x;
-            int ny=y;
+            int nx=x+dx[i];
+            int ny=y+dy[i];
             while(nx>=0&&nx<n&&ny>=0&&ny<m&&board[nx][ny]!='D'){
                 nx+=dx[i];
                 ny+=dy[i];
             }
             nx-=dx[i];
             ny-=dy[i];
-            if(!vis[i][nx][ny]){
-                vis[i][nx][ny]=vis[dir][x][y]+1;
-                q.push({i,{nx,ny}});
+            if(!vis[nx][ny]){
+                vis[nx][ny]=vis[x][y]+1;
+                q.push({nx,ny});
             }
         }
     }
-    return -1;
+    return answer;
 }
