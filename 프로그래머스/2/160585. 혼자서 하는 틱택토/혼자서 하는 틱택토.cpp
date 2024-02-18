3ce409e4f5dd31516e5188c582f375f0aaaba564
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-18 16:40:53 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.cpp
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.cpp
@@ -2,66 +2,39 @@
 #include <vector>
 
 using namespace std;
-vector<string> board;
-int cnt;
 int answer;
-bool isGameEnd(vector<string> now){
+bool isGameEnd(vector<string> board, char c){
     for(int i=0;i<3;i++){
-        if(now[i][0]!='.'&&now[i][0]==now[i][1]&&now[i][1]==now[i][2]){
+        if(board[i][0]==board[i][1]&&board[i][1]==board[i][2]&&board[i][0]==c){
             return true;
         }
-        if(now[0][i]!='.'&&now[0][i]==now[1][i]&&now[1][i]==now[2][i]){
+        if(board[0][i]==board[1][i]&&board[1][i]==board[2][i]&&board[2][i]==c){
             return true;
         }
     }
-    if(now[0][0]!='.'&&now[0][0]==now[1][1]&&now[1][1]==now[2][2]){
+    if(board[0][0]==board[1][1]&&board[1][1]==board[2][2]&&board[0][0]==c){
         return true;
     }
-    if(now[0][2]!='.'&&now[0][2]==now[1][1]&&now[1][1]==now[2][0]){
+    if(board[0][2]==board[1][1]&&board[1][1]==board[2][0]&&board[0][2]==c){
         return true;
     }
     return false;
 }
-void go(vector<string> now, int flag, int c){
-    if(c==cnt||isGameEnd(now)){
-        bool f=true;
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                if(now[i][j]!=board[i][j]){
-                    f=false;
-                    break;
-                }
-            }
-        }
-        if(f){
-            answer=1;
-        }
-        return;
-    }
+int solution(vector<string> board) {
+    int oCnt=0,xCnt=0;
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            if(now[i][j]=='.'){
-                if(flag==0){
-                    now[i][j]='O';
-                }else{
-                    now[i][j]='X';
-                }
-                go(now,1-flag,c+1);
-                now[i][j]='.';
+            if(board[i][j]=='O'){
+                oCnt++;
+            }
+            if(board[i][j]=='X'){
+                xCnt++;
             }
         }
     }
-}
-
-int solution(vector<string> board) {
-    ::board=board;
-    for(int i=0;i<board.size();i++){
-        for(int j=0;j<board.size();j++){
-            if(board[i][j]!='.'){
-                cnt++;
-            }
-        }
+    if((isGameEnd(board,'O')&&oCnt<=xCnt)||oCnt<xCnt||(isGameEnd(board,'O')&&isGameEnd(board,'X'))||oCnt>xCnt+1||(isGameEnd(board,'X')&&oCnt!=xCnt)){
+        return 0;
     }
-    go({"...","...","..."},0,0);
-    return answer;
+    
+    return 1;
 }
