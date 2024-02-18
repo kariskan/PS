c5fcdaccb46a86a7bc887d65f6eaf1105c2cc4f6
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-18 17:17:22 +0900

    [level 2] Title: 미로 탈출, Time: 0.05 ms, Memory: 3.71 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/README.md
+++ 프로그래머스/2/159993. 미로 탈출/README.md
@@ -1,10 +1,10 @@
 # [level 2] 미로 탈출 - 159993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.03 ms
+메모리: 3.71 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 6일 18:13:58
+2024년 2월 0일 17:17:20
 
 ### 문제 설명
 
--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.cpp
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.cpp
@@ -1,61 +1,47 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <cstring>
 using namespace std;
+
 int dx[4]={0,0,1,-1};
 int dy[4]={1,-1,0,0};
-int vis[100][100];
-int n,m;
-vector<string> maps;
-
-int bfs(int sx, int sy, int ex, int ey){
-    memset(vis,0,sizeof(vis));
-    queue<pair<int,int>> q;
-    q.push({sx,sy});
-    vis[sx][sy]=1;
+int solution(vector<string> maps) {
+    queue<pair<pair<int,int>,int>> q;
+    int vis[100][100][2]={0,};
+    int n=maps.size(),m=maps[0].size();
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(maps[i][j]=='S'){
+                vis[i][j][0]=1;
+                q.push({{i,j},0});
+            }
+        }
+    }
     while(!q.empty()){
-        int x=q.front().first;
-        int y=q.front().second;
+        int x=q.front().first.first;
+        int y=q.front().first.second;
+        int flag=q.front().second;
         q.pop();
-        if(x==ex&&y==ey){
-            return vis[x][y]-1;
+        if(maps[x][y]=='E'&&flag==1){
+            return vis[x][y][flag]-1;
         }
         for(int i=0;i<4;i++){
             int nx=x+dx[i];
             int ny=y+dy[i];
-            if(nx>=0&&nx<n&&ny>=0&&ny<m&&!vis[nx][ny]&&maps[nx][ny]!='X'){
-                vis[nx][ny]=vis[x][y]+1;
-                q.push({nx,ny});
+            int nf=flag;
+            if(nx>=0&&nx<n&&ny>=0&&ny<m){
+                if(maps[nx][ny]=='L'){
+                    nf++;
+                }
+                if(nf>=2){
+                    continue;
+                }
+                if(maps[nx][ny]!='X'&&!vis[nx][ny][nf]){
+                    vis[nx][ny][nf]=vis[x][y][flag]+1;
+                    q.push({{nx,ny},nf});
+                }
             }
         }
     }
     return -1;
-}
-int solution(vector<string> maps) {
-    ::maps=maps;
-    int sx,sy,ex,ey,lx,ly;
-    n=maps.size(),m=maps[0].size();
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(maps[i][j]=='S'){
-                sx=i;
-                sy=j;
-            }
-            if(maps[i][j]=='L'){
-                lx=i;
-                ly=j;
-            }
-            if(maps[i][j]=='E'){
-                ex=i;
-                ey=j;
-            }
-        }
-    }
-    int first=bfs(sx,sy,lx,ly);
-    int second=bfs(lx,ly,ex,ey);
-    if(first==-1||second==-1){
-        return -1;
-    }
-    return first+second;
 }
