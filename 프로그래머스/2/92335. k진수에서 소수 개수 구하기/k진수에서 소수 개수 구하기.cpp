c493a34d8b6b714b368f228805808e50ca348720
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-19 15:06:05 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
@@ -2,43 +2,41 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
-bool prime(long long n){
-    if(n==0||n==1){
-        return false;
-    }
-    for(long long i=2;i*i<=n;i++){
-        if(n%i==0){
-            return false;
-        }
-    }
-    return true;
-}
-
-string parse(int n, int k){
-    string res = "";
+string get(int n,int k){
+    string res="";
     while(n){
-        res+=(n%k)+'0';
+        res+=(n%k+'0');
         n/=k;
     }
     reverse(res.begin(),res.end());
     return res;
 }
-
+bool isPrime(long long a){
+    if(a==0||a==1){
+        return false;
+    }
+    for(long long i=2;i*i<=a;i++){
+        if(a%i==0){
+            return false;
+        }
+    }
+    return true;
+}
 int solution(int n, int k) {
     int answer = 0;
-    string a = parse(n,k);
-    long long sum=0;
-    for(int i=0;i<a.length();i++){
-        if(sum!=0&&a[i]=='0'){
-            if(prime(sum)){
+    string g=get(n,k);
+    string temp="";
+    for(int i=0;i<g.length();i++){
+        if(g[i]=='0'){
+            if(temp!=""&&isPrime(stoll(temp))){
                 answer++;
             }
-            sum=0;
+            temp="";
+            continue;
         }
-        sum=sum*10+(a[i]-'0');
+        temp+=g[i];
     }
-    if(sum!=0&&prime(sum)){
+    if(temp!=""&&isPrime(stoll(temp))){
         answer++;
     }
     return answer;
