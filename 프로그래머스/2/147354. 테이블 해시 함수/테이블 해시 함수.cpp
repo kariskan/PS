0d90161323b120eb7a0db4f92b09575b5e093be2
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-19 10:19:20 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
@@ -2,24 +2,24 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+
 int col;
-bool compare(vector<int>& v1, vector<int>& v2){
-    if(v1[col-1]==v2[col-1]){
+bool compare(vector<int>& v1,vector<int>& v2){
+    if(v1[col]==v2[col]){
         return v1[0]>v2[0];
     }
-    return v1[col-1]<v2[col-1];
+    return v1[col]<v2[col];
 }
 
 int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
-    int answer = 0;
-    ::col=col;
+    ::col=col-1;
     sort(data.begin(),data.end(),compare);
+    int answer=0;
     for(int i=row_begin-1;i<=row_end-1;i++){
-        int s = 0;
+        int s=0;
         for(int j=0;j<data[i].size();j++){
             s+=data[i][j]%(i+1);
         }
-        printf("%d\n", s);
         answer^=s;
     }
     return answer;
