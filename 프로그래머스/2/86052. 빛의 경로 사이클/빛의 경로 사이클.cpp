178be0c011e7497839a1369a88d8d98188b4dc5d
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-20 12:04:16 +0900

    [level 2] Title: 빛의 경로 사이클, Time: 124.82 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/2/86052. 빛의 경로 사이클/빛의 경로 사이클.cpp
+++ 프로그래머스/2/86052. 빛의 경로 사이클/빛의 경로 사이클.cpp
@@ -2,14 +2,13 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-int vis[4][500][500];
+vector<string> grid;
+vector<int> answer;
 int dx[4]={0,1,0,-1};
 int dy[4]={1,0,-1,0};
-int n,m;
-vector<int> answer;
-vector<string> grid;
+int vis[500][500][4];
 
-int charToDir(char c, int dir){
+int getDir(int dir,char c){
     if(c=='S'){
         return dir;
     }
@@ -19,36 +18,37 @@ int charToDir(char c, int dir){
     return (dir+1)%4;
 }
 
-void go(int cnt,int dir,int x,int y){
-    int nextDir=charToDir(grid[x][y],dir);
-    int nx=x+dx[nextDir];
-    if(nx>=n){
+void go(int i,int j,int dir,int cnt){
+    
+    vis[i][j][dir]=1;
+    int nd=getDir(dir,grid[i][j]);
+    int nx=i+dx[nd];
+    if(nx<0){
+        nx=grid.size()-1;
+    }else if(nx>=grid.size()){
         nx=0;
-    }else if(nx<0){
-        nx=n-1;
     }
-    int ny=y+dy[nextDir];
-    if(ny>=m){
+    int ny=j+dy[nd];
+    if(ny<0){
+        ny=grid[0].size()-1;
+    }else if(ny>=grid[0].size()){
         ny=0;
-    }else if(ny<0){
-        ny=m-1;
     }
     
-    if(vis[nextDir][nx][ny]){
-        answer.push_back(cnt);
+    if(vis[nx][ny][nd]){
+        answer.push_back(cnt+1);
     }else{
-        vis[nextDir][nx][ny]=1;
-        go(cnt+1,nextDir,nx,ny);
+        go(nx,ny,nd,cnt+1);
     }
 }
+
 vector<int> solution(vector<string> grid) {
     ::grid=grid;
-    n=grid.size(),m=grid[0].size();
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
+    for(int i=0;i<grid.size();i++){
+        for(int j=0;j<grid[i].length();j++){
             for(int k=0;k<4;k++){
-                if(!vis[k][i][j]){
-                    go(0,k,i,j);
+                if(!vis[i][j][k]){
+                    go(i,j,k,0);
                 }
             }
         }
