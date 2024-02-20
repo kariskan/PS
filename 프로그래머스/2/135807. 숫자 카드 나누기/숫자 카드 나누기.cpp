691f0891b746a4ac69d1012f6ff35e4eab5c20df
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-20 10:11:02 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.cpp
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.cpp
@@ -1,8 +1,9 @@
 #include <string>
 #include <vector>
+
 using namespace std;
 
-int gcd(int a, int b){
+int gcd(int a,int b){
     if(b==0){
         return a;
     }
@@ -10,25 +11,30 @@ int gcd(int a, int b){
 }
 
 int solution(vector<int> arrayA, vector<int> arrayB) {
+    int answer = 0;
     int a=arrayA[0],b=arrayB[0];
     for(int i=1;i<arrayA.size();i++){
         a=gcd(a,arrayA[i]);
         b=gcd(b,arrayB[i]);
     }
-    bool flagA=true,flagB=true;
+    printf("%d %d\n", a,b);
+    int flag=1;
     for(int i=0;i<arrayA.size();i++){
-        if(arrayB[i]%a==0){
-            flagA=false;
-        }
         if(arrayA[i]%b==0){
-            flagB=false;
+            flag=0;
         }
     }
-    if(!flagA){
-        a=0;
+    if(flag){
+        answer=max(answer,b);
     }
-    if(!flagB){
-        b=0;
+    flag=1;
+    for(int i=0;i<arrayB.size();i++){
+        if(arrayB[i]%a==0){
+            flag=0;
+        }
     }
-    return max(a,b);
+    if(flag){
+        answer=max(answer,a);
+    }
+    return answer;
 }
