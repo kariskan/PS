43221b24c11884150ba597a5384f31f13ebad272
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-20 11:28:10 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.03 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -2,34 +2,27 @@
 #include <vector>
 
 using namespace std;
-int child[101];
 vector<vector<int>> v;
-
-int getChildCnt(int n, int pre){
+int answer = 100,n;
+int go(int now,int pre){
     int res=0;
-    for(auto&i:v[n]){
-        if(i != pre){
-            res+=getChildCnt(i, n);
+    for(auto&i:v[now]){
+        if(i==pre){
+            continue;
         }
+        int child=go(i,now);
+        answer=min(answer,abs(2*child-n));
+        res+=child;
     }
-    return child[n]=res+1;
+    return res+1;
 }
-
 int solution(int n, vector<vector<int>> wires) {
-    int answer = 1000000000;
     v.resize(n+1);
+    ::n=n;
     for(int i=0;i<wires.size();i++){
         v[wires[i][0]].push_back(wires[i][1]);
         v[wires[i][1]].push_back(wires[i][0]);
     }
-    getChildCnt(1, 0);
-    for(int i=0;i<wires.size();i++){
-        int c1=child[wires[i][0]];
-        int c2=child[wires[i][1]];
-        if(c1<c2){
-            swap(c1,c2);
-        }
-        answer=min(answer,abs(c2*2-n));
-    }
+    go(1,0);
     return answer;
 }
