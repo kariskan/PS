73a0fbdfa159f72a4cfa093fd46c20b1b0d4de45
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-22 17:31:24 +0900

    [level 2] Title: [3차] 압축, Time: 0.34 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.cpp
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.cpp
@@ -2,25 +2,30 @@
 #include <vector>
 #include <map>
 using namespace std;
-map<string,int> ma={{"",0}};
+map<string,int> ma;
 vector<int> solution(string msg) {
     vector<int> answer;
-    for(int i=1;i<=26;i++){
-        string s="";
-        s+=i-1+'A';
-        ma[s]=i;
+    for(int i=0;i<26;i++){
+        string s(1,i+'A');
+        ma[s]=i+1;
     }
-    int lastCnt=26;
+    int last=26;
     for(int i=0;i<msg.length();i++){
-        string s="";
         int idx=i;
-        while(i<msg.length()&&ma.find(s)!=ma.end()){
-            s+=msg[idx];
+        string s(1,msg[i]);
+        while(ma.find(s)!=ma.end()){
             idx++;
+            if(idx==msg.length()){
+                break;
+            }
+            s+=msg[idx];
         }
-        idx--;
-        answer.push_back(ma[s.substr(0,s.length()-1)]);
-        ma[s]=++lastCnt;
+        if(ma.find(s)==ma.end()){
+            ma[s]=++last;
+            answer.push_back(ma[s.substr(0,s.length()-1)]);
+        }else{
+            answer.push_back(ma[s]);
+        }
         i=idx-1;
     }
     return answer;
