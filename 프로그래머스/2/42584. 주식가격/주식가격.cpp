71cba5225d54a474a1ea9830daf25e54df6d2e9c
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-22 15:53:46 +0900

    [level 2] Title: 주식가격, Time: 15.68 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,21 +1,23 @@
 #include <string>
 #include <vector>
-#include <stack>
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
-    vector<int> answer(prices.size());
-    stack<pair<int,int>> st;
-    for(int i=0;i<prices.size();i++){
-        while(!st.empty()&&st.top().first>prices[i]){
-            answer[st.top().second]=i-st.top().second;
-            st.pop();
+    vector<int> answer;
+    int size=prices.size();
+
+    for(int i=0; i<size; i++){
+        int time=0;
+        for(int j=i+1; j<size; j++){
+            time++;
+            if(prices[j]<prices[i]||j==size-1){ //마지막에서 두번째 값은 만약 마지막 값이 더 크다면 push할 수가 없기때문에 조건에 추가
+                answer.push_back(time);
+                break;
+            }
         }
-        st.push({prices[i],i});
     }
-    while(!st.empty()){
-        answer[st.top().second]=prices.size()-1-st.top().second;
-        st.pop();
-    }
+
+    answer.push_back(0); //마지막은 시간이 없음
+
     return answer;
 }
