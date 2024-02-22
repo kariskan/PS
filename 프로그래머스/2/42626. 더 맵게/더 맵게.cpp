3dfb5d30c7280a1a8233152d450c9e51d65844cb
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-22 15:53:30 +0900

    [level 2] Title: 더 맵게, Time: 223.04 ms, Memory: 41.1 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -5,17 +5,17 @@ using namespace std;
 
 int solution(vector<int> scoville, int K) {
     int answer = 0;
-    priority_queue<int, vector<int>, greater<int>> q;
+    priority_queue<int> q;
     for(int i=0;i<scoville.size();i++){
-        q.push(scoville[i]);
+        q.push(-scoville[i]);
     }
-    while(q.size()>=2&&q.top()<K){
-        int first=q.top();
+    while(q.size()>=2&&-q.top()<K){
+        int f=-q.top();
         q.pop();
-        int second=q.top();
+        int s=-q.top();
         q.pop();
-        q.push(first+second*2);
+        q.push(-(f+s*2));
         answer++;
     }
-    return q.top()<K?-1:answer;
+    return q.empty()||-q.top()<K?-1:answer;
 }
