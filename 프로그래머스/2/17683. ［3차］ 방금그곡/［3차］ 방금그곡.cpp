267f10bfc85fd1351a02568250c239296867f358
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-22 17:47:48 +0900

    [level 2] Title: [3차] 방금그곡, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.cpp
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.cpp
@@ -1,24 +1,13 @@
 #include <string>
 #include <vector>
 #include <sstream>
-#include <map>
 using namespace std;
-map<string,string> ma={
-    {"C","0"},
-    {"C#","1"},
-    {"D","2"},
-    {"D#","3"},
-    {"E","4"},
-    {"F","5"},
-    {"F#","6"},
-    {"G","7"},
-    {"G#","8"},
-    {"A","9"},
-    {"A#","A"},
-    {"B","B"},
-};
-vector<string> parse(string music){
-    stringstream ss(music);
+
+int time(string s){
+    return stoi(s.substr(0,2))*60+stoi(s.substr(3));
+}
+vector<string> parse(string s){
+    stringstream ss(s);
     vector<string> v;
     string temp;
     while(getline(ss,temp,',')){
@@ -26,47 +15,48 @@ vector<string> parse(string music){
     }
     return v;
 }
-string parse2(string music){
+string parse2(string s){
     string res="";
-    for(int i=0;i<music.length();i++){
-        if(i<music.length()-1&&music[i+1]=='#'){
-            res+=ma[music.substr(i,2)];
-            i++;
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='#'){
+            res.pop_back();
+            res+=s[i-1]-'A'+'H';
         }else{
-            res+=ma[music.substr(i,1)];
+            res+=s[i];
         }
     }
     return res;
 }
-
 string solution(string m, vector<string> musicinfos) {
     string answer = "";
-    int maxLen=0;
+    int maxPlayTime=0;
     m=parse2(m);
     for(string music:musicinfos){
         vector<string> v=parse(music);
-        int start=stoi(v[0].substr(0,2))*60+stoi(v[0].substr(3));
-        int end=stoi(v[1].substr(0,2))*60+stoi(v[1].substr(3));
+        int start=time(v[0]);
+        int end=time(v[1]);
         string title=v[2];
         string info=parse2(v[3]);
         
-        string s="";
-        
-        for(int i=0;i<(end-start+1)/info.length();i++){
-            s+=info;
+        string total="";
+        for(int i=0;i<(end-start)/info.length();i++){
+            total+=info;
         }
-        s+=info.substr(0,(end-start+1)%info.length());
-        // printf("s: %s, m: %s\n", s.c_str(), parse2(m).c_str());
-        for(int i=0;i<=(int)s.length()-(int)m.length();i++){
-            // printf("%s\n", s.substr(i,(int)m.length()).c_str());
-            if(s.substr(i,(int)m.length())==m){
-                if(maxLen<end-start+1){
-                    maxLen=end-start+1;
+        if(total.length()<end-start){
+            total+=info.substr(0,end-start-total.length());
+        }
+        for(int i=0;i<=(int)total.length()-(int)m.length();i++){
+            if(total.substr(i,m.length())==m){
+                if(maxPlayTime<end-start){
+                    maxPlayTime=end-start;
                     answer=title;
-                    break;
                 }
             }
         }
     }
-    return answer==""?"(None)":answer;
+    
+    if(answer==""){
+        return "(None)";
+    }
+    return answer;
 }
