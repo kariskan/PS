3b04e3e89cc26f9f7781274ee420fcc30f0efe21
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-22 15:53:20 +0900

    [level 2] Title: 괄호 변환, Time: 0.05 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.cpp
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.cpp
@@ -1,60 +1,55 @@
 #include <string>
 #include <vector>
-#include <stack>
 #include <algorithm>
+#include <stack>
 using namespace std;
-
-bool isValid(string s){
+pair<string,string> balance(string p){
+    int lc=0,rc=0;
+    for(int i=0;i<p.length();i++){
+        if(p[i]==')'){
+            rc++;
+        }else{
+            lc++;
+        }
+        if(lc==rc){
+            return {p.substr(0,lc+rc),p.substr(lc+rc)};
+        }
+    }
+}
+bool valid(string p){
     stack<char> st;
-    for(int i=0;i<s.length();i++){
-        st.push(s[i]);
-        if(s[i]==')'){
-            st.pop();
-            if(st.size()<1||st.top()!='('){
+    for(int i=0;i<p.length();i++){
+        if(p[i]==')'){
+            if(st.empty()||st.top()!='('){
                 return false;
             }
             st.pop();
+            continue;
         }
+        st.push(p[i]);
     }
     return st.empty();
 }
-
-string go(string s){
-    if(s.empty()){
-        return "";
+string go(string p){
+    if(p.empty()){
+        return p;
     }
-    int leftCnt=0,rightCnt=0;
-    string u="",v="";
-    for(int i=0;i<s.length();i++){
-        u+=s[i];
-        if(s[i]=='('){
-            leftCnt++;
+    pair<string,string> b=balance(p);
+    if(valid(b.first)){
+        return b.first+go(b.second);
+    }
+    string temp="("+go(b.second)+")";
+    b.first.pop_back();
+    b.first.erase(b.first.begin());
+    for(int i=0;i<b.first.length();i++){
+        if(b.first[i]==')'){
+            b.first[i]='(';
         }else{
-            rightCnt++;
-        }
-        if(leftCnt==rightCnt){
-            if(i<s.length()-1){
-                v=s.substr(i+1);
-            }
-            break;
+            b.first[i]=')';
         }
     }
-    if(isValid(u)){
-        return u+go(v);
-    }
-    string t="("+go(v)+")";
-    u.erase(u.begin());
-    u.pop_back();
-    for(int i=0;i<u.length();i++){
-        if(u[i]=='('){
-            u[i]=')';
-        }else{
-            u[i]='(';
-        }
-    }
-    return t+u;
+    return temp+b.first;
 }
-
 string solution(string p) {
     return go(p);
 }
