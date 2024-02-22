b9600365a0032b42e20e078940b9a371fc576fa0
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-22 17:01:54 +0900

    [level 2] Title: [3차] n진수 게임, Time: 0.15 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.cpp
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.cpp
@@ -2,39 +2,33 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-string parse(int n,int m){
-    if(n==0){
+string binary(int i,int n){
+    if(i==0){
         return "0";
     }
     string res="";
-    while(n){
-        if(m<10||n%m<10){
-            res+=n%m+'0';
+    while(i){
+        if(i%n>=10){
+            res+=(i%n-10+'A');
         }else{
-            res+=(n%m-10)+'A';
+            res+=(i%n+'0');
         }
-        n/=m;
+        i/=n;
     }
     reverse(res.begin(),res.end());
     return res;
 }
 string solution(int n, int t, int m, int p) {
     string answer = "";
-    string s="";
-    for(int i=0;;i++){
-        s+=parse(i,n);
-        if(s.length()>=m*t){
-            break;
-        }
+    int len=p+(t-1)*m;
+    string temp="";
+    for(int i=0;temp.length()<len;i++){
+        temp+=binary(i,n);
     }
-    printf("%s\n",s.c_str());
-    for(int i=0;i<s.length();i++){
-        if((i+1)%m==p%m){
-            answer+=s[i];
-        }
-        if(answer.length()==t){
-            break;
-        }
+    int idx=p-1;
+    for(int i=0;i<t;i++){
+        answer+=temp[idx];
+        idx+=m;
     }
     return answer;
 }
