0a6a9b56c90cafddb12372151d48c966a1b95e89
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-22 15:54:07 +0900

    [level 2] Title: H-Index, Time: 0.07 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -1,22 +1,17 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
 int solution(vector<int> citations) {
     int answer = 0;
-    for(int i=0;i<=citations.size();i++){
-        int hup=0,hdown=0;
+    sort(citations.begin(),citations.end());
+    for(int i=0;i<=10000;i++){
         for(int j=0;j<citations.size();j++){
-            if(citations[j]>=i){
-                hup++;
+            if(citations[j]>=i&&citations.size()-j>=i){
+                answer=i;
+                break;
             }
-            if(citations[j]<=i){
-                hdown++;
-            }
-        }
-        if(hup>=i&&hdown<=i){
-            answer=max(answer,i);
         }
     }
     return answer;
