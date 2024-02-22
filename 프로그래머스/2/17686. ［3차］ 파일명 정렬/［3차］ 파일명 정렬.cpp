49eaa98a6534206390c0d472c00fb77e090a21bc
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-22 17:12:08 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 3.18 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.cpp
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.cpp
@@ -2,67 +2,50 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
-string tolower(string s){
-    string res="";
+string toLower(string s){
     for(int i=0;i<s.length();i++){
         if(s[i]>='A'&&s[i]<='Z'){
-            res+=s[i]-'A'+'a';
-        }else{
-            res+=s[i];
+            s[i]=s[i]-'A'+'a';
         }
     }
-    return res;
+    return s;
 }
-
-vector<string> parse(string file){
-    vector<string> v;
-    string s="";
-    int idx=0;
-    for(int i=0;i<file.length();i++){
-        if(file[i]>='0'&&file[i]<='9'){
-            idx=i;
-            v.push_back(s);
-            break;
+bool compare(pair<vector<string>,int>&v1,pair<vector<string>,int>&v2){
+    if(toLower(v1.first[0])==toLower(v2.first[0])){
+        if(stoi(v1.first[1])==stoi(v2.first[1])){
+            return v1.second<v2.second;
         }
-        s+=file[i];
+        return stoi(v1.first[1])<stoi(v2.first[1]);
     }
-    s="";
-    for(int i=idx;i<file.length();i++){
-        if(file[i]<'0'||file[i]>'9'){
-            break;
-        }
-        s+=file[i];
-    }
-    v.push_back(s);
-    idx+=s.length();
-    if(idx<file.length()){
-        v.push_back(file.substr(idx));
-    }
-    return v;
+    return toLower(v1.first[0])<toLower(v2.first[0]);
 }
-
-bool compare(pair<string,int>& s1, pair<string,int>& s2){
-    vector<string> v1=parse(s1.first);
-    vector<string> v2=parse(s2.first);
-    if(tolower(v1[0])==tolower(v2[0])){
-        if(stoi(v1[1])==stoi(v2[1])){
-            return s1.second<s2.second;
-        }
-        return stoi(v1[1])<stoi(v2[1]);
-    }
-    return tolower(v1[0])<tolower(v2[0]);
-}
-
 vector<string> solution(vector<string> files) {
     vector<string> answer;
-    vector<pair<string,int>> v;
+    vector<pair<vector<string>,int>> v;
     for(int i=0;i<files.size();i++){
-        v.push_back({files[i],i});
+        vector<string> t;
+        int idx1=0;
+        while(idx1<files[i].length()&&!(files[i][idx1]>='0'&&files[i][idx1]<='9')){
+            idx1++;
+        }
+        int idx2=idx1;
+        while(idx2<files[i].length()&&files[i][idx2]>='0'&&files[i][idx2]<='9'){
+            idx2++;
+        }
+        t.push_back(files[i].substr(0,idx1));
+        t.push_back(files[i].substr(idx1,idx2-idx1));
+        if(idx2<files[i].length()){
+            t.push_back(files[i].substr(idx2));
+        }
+        v.push_back({t,i});
     }
     sort(v.begin(),v.end(),compare);
     for(int i=0;i<v.size();i++){
-        answer.push_back(v[i].first);
+        string temp="";
+        for(int j=0;j<v[i].first.size();j++){
+            temp+=v[i].first[j];
+        }
+        answer.push_back(temp);
     }
     return answer;
 }
