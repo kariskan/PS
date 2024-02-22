7c5046e42aca698880d559df177b96ad3f4ff02b
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-22 15:53:23 +0900

    [level 2] Title: 오픈채팅방, Time: 330.58 ms, Memory: 43.3 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -1,33 +1,32 @@
 #include <string>
 #include <vector>
-#include <sstream>
 #include <map>
+#include <sstream>
 using namespace std;
-map<string,string> uid;
+map<string,string> name;
 vector<string> parse(string s){
     stringstream ss(s);
-    vector<string> v;
+    vector<string> res;
     string temp;
     while(getline(ss,temp,' ')){
-        v.push_back(temp);
+        res.push_back(temp);
     }
-    return v;
+    return res;
 }
-
 vector<string> solution(vector<string> record) {
     vector<string> answer;
     for(int i=0;i<record.size();i++){
-        vector<string> p=parse(record[i]);
-        if(p[0]=="Change"||p[0]=="Enter"){
-            uid[p[1]]=p[2];
+        vector<string> v=parse(record[i]);
+        if(v[0]=="Enter"||v[0]=="Change"){
+            name[v[1]]=v[2];
         }
     }
     for(int i=0;i<record.size();i++){
-        vector<string> p=parse(record[i]);
-        if(p[0]=="Enter"){
-            answer.push_back(uid[p[1]]+"님이 들어왔습니다.");
-        }else if(p[0]=="Leave"){
-            answer.push_back(uid[p[1]]+"님이 나갔습니다.");
+        vector<string> v=parse(record[i]);
+        if(v[0]=="Enter"){
+            answer.push_back(name[v[1]]+"님이 들어왔습니다.");
+        }else if(v[0]=="Leave"){
+            answer.push_back(name[v[1]]+"님이 나갔습니다.");
         }
     }
     return answer;
