2fdbe8e6d6639f0b2263433744dc4a3655950149
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-22 16:34:16 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -2,28 +2,14 @@
 #include <vector>
 #include <map>
 using namespace std;
-map<string,int>ma;
-vector<int> v;
-int answer;
-void go(int sum,int idx){
-    answer+=sum;
-    if(idx>=v.size()){
-        return;
-    }
-    for(int i=idx;i<v.size();i++){
-        go(sum*v[i],i+1);
-    }
-}
+map<string,int> ma;
 int solution(vector<vector<string>> clothes) {
-    for(vector<string> cloth:clothes){
-        ma[cloth[1]]++;
+    int answer = 1;
+    for(int i=0;i<clothes.size();i++){
+        ma[clothes[i][1]]++;
     }
     for(auto&i:ma){
-        v.push_back(i.second);
+        answer*=(i.second+1);
     }
-    for(int i=0;i<v.size();i++){
-        go(v[i],i+1);
-        printf("%d\n", answer);
-    }
-    return answer;
+    return answer-1;
 }
