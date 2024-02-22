17fbfcd7e33d7db07f308869991b78c288096908
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-22 15:53:16 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.18 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -2,29 +2,31 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
-string parse(int n){
+int getZeroCnt(string s){
+    int cnt=0;
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='0'){
+            cnt++;
+        }
+    }
+    return cnt;
+}
+string toBinary(int a){
     string res="";
-    while(n){
-        res+=(n%2+'0');
-        n/=2;
+    while(a){
+        res+=(a%2+'0');
+        a/=2;
     }
     reverse(res.begin(),res.end());
     return res;
 }
-
 vector<int> solution(string s) {
-    int term=0,cnt=0;
+    int cnt=0,totalZeroCnt=0;
     while(s!="1"){
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='0'){
-                s.erase(s.begin()+i);
-                i--;
-                cnt++;
-            }
-        }
-        s=parse(s.length());
-        term++;
+        int zeroCnt=getZeroCnt(s);
+        s=toBinary(s.length()-zeroCnt);
+        cnt++;
+        totalZeroCnt+=zeroCnt;
     }
-    return {term,cnt};
+    return {cnt,totalZeroCnt};
 }
