84f21066ef8d9a251404a65e6601e76c39c7b12d
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-23 10:59:51 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -2,16 +2,12 @@
 
 using namespace std;
 
-int solution(int n, int a, int b)
-{
-    if(a>b){
-        swap(a,b);
-    }
-    int answer=1;
-    while(a%2!=1||b-a!=1){
+int solution(int n, int a, int b) {
+    int answer = 0;
+    while(a!=b){
+        answer++;
         a=(a+1)/2;
         b=(b+1)/2;
-        answer++;
     }
     return answer;
 }
