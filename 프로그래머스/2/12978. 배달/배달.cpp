772e5bad62f16d371ab1df3ad87aa6d1d5b345c1
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-23 11:19:43 +0900

    [level 2] Title: 배달, Time: 0.15 ms, Memory: 3.65 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.cpp
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -1,40 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 using namespace std;
-
+int map[51][51];
 int solution(int N, vector<vector<int> > road, int K) {
     int answer = 0;
-    vector<vector<pair<int,int>>> v(N+1);
-    vector<int> dis(N+1,1e9);
+    
     for(int i=0;i<road.size();i++){
-        v[road[i][0]].push_back({road[i][1],road[i][2]});
-        v[road[i][1]].push_back({road[i][0],road[i][2]});
-    }
-    priority_queue<pair<int,int>> pq;
-    dis[1]=0;
-    pq.push({0,1});
-
-    while(!pq.empty()){
-        int node=pq.top().second;
-        int cost=-pq.top().first;
-        pq.pop();
-        
-        if(dis[node]<cost){
-            continue;
+        if(map[road[i][0]][road[i][1]]==0||map[road[i][0]][road[i][1]]>road[i][2]){
+            map[road[i][0]][road[i][1]]=road[i][2];
+            map[road[i][1]][road[i][0]]=road[i][2];   
         }
-
-        for(auto&i:v[node]){
-            int nxCost=i.second+cost;
-            int nxNode=i.first;
-            if(dis[nxNode]>nxCost){
-                dis[nxNode]=nxCost;
-                pq.push({-nxCost,nxNode});
+    }
+    
+    for(int k=1;k<=N;k++){
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N;j++){
+                if(i==j){
+                    continue;
+                }
+                if(map[i][k]&&map[k][j]&&(map[i][j]==0||map[i][j]>map[i][k]+map[k][j])){
+                    map[i][j]=map[i][k]+map[k][j];
+                }
             }
         }
     }
+
     for(int i=1;i<=N;i++){
-        if(dis[i]<=K){
+        if(map[1][i]<=K){
             answer++;
         }
     }
