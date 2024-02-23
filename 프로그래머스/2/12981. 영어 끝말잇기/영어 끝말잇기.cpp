773bc0299f2573bdf97582a103f42eae6e6fcacc
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-23 11:12:37 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.05 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -1,22 +1,17 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <set>
 using namespace std;
 
 vector<int> solution(int n, vector<string> words) {
     set<string> se;
-    int cnt=1;
+    char last=words[0][0];
     for(int i=0;i<words.size();i++){
-        if(words[i].length()<2||(i>0&&words[i-1][words[i-1].length()-1]!=words[i][0])
-          ||se.find(words[i])!=se.end()){
-            return {cnt,(i+n)/n};
-        }
-        cnt++;
-        if(cnt==n+1){
-            cnt=1;
+        if(words[i][0]!=last||words[i].length()<2||se.find(words[i])!=se.end()){
+            return {i%n+1,i/n+1};
         }
         se.insert(words[i]);
+        last=words[i].back();
     }
     return {0,0};
 }
