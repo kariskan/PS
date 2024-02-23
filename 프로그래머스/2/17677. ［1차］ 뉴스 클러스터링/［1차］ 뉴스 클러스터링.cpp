b5cfed603b8583e6a0ee1a8a0aa29b435b18f88d
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-23 10:56:55 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.07 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
@@ -1,62 +1,88 @@
 #include <string>
+#include <algorithm>
 #include <map>
 using namespace std;
-
-string tolower(string s){
+bool isChar(char a){
+    return (a>='A'&&a<='Z')||(a>='a'&&a<='z');
+}
+string toLower(string s){
+    string res="";
     for(int i=0;i<s.length();i++){
         if(s[i]>='A'&&s[i]<='Z'){
-            s[i]=s[i]-'A'+'a';
+            res+=s[i]-'A'+'a';
+        }else{
+            res+=s[i];
         }
     }
-    return s;
+    return res;
 }
-
-bool alphabet(char a){
-    return (a>='a'&&a<='z')||(a>='A'&&a<='Z');
-}
-
 int solution(string str1, string str2) {
     int answer = 0;
-    map<string,int> v1,v2;
+    str1=toLower(str1);
+    str2=toLower(str2);
+    vector<string> v1,v2;
     for(int i=0;i<str1.length()-1;i++){
-        if(!alphabet(str1[i])||!alphabet(str1[i+1])){
+        string temp=str1.substr(i,2);
+        if(!isChar(temp[0])||!isChar(temp[1])){
             continue;
         }
-        v1[tolower(str1.substr(i,2))]++;
+        v1.push_back(temp);
     }
     for(int i=0;i<str2.length()-1;i++){
-        if(!alphabet(str2[i])||!alphabet(str2[i+1])){
+        string temp=str2.substr(i,2);
+        if(!isChar(temp[0])||!isChar(temp[1])){
             continue;
         }
-        v2[tolower(str2.substr(i,2))]++;
+        v2.push_back(temp);
     }
+    sort(v1.begin(),v1.end());
+    sort(v2.begin(),v2.end());
     
-    int n=0,u=0;
-    for(auto&i:v1){
-        for(auto&j:v2){
-            if(i.first==j.first){
-                n+=min(i.second,j.second);
+    map<string,int> ma1,ma2,N,U;
+    for(int i=0;i<v1.size();i++){
+        ma1[v1[i]]++;
+    }
+    for(int i=0;i<v2.size();i++){
+        ma2[v2[i]]++;
+    }
+    for(auto&i:ma1){
+        if(N.find(i.first)==N.end()){
+            if(ma2.find(i.first)!=ma2.end()){
+                N[i.first]=min(i.second,ma2[i.first]);
+            }
+        }
+        if(U.find(i.first)==U.end()){
+            if(ma2.find(i.first)==ma2.end()){
+                U[i.first]=i.second;
+            }else{
+                U[i.first]=max(i.second,ma2[i.first]);                
             }
         }
     }
-    for(auto&i:v1){
-        printf("%s ", i.first.c_str());
-        if(v2.find(i.first)==v2.end()){
-            u+=i.second;
-        }else{
-            u+=max(i.second,v2[i.first]);
+    for(auto&i:ma2){
+        if(N.find(i.first)==N.end()){
+            if(ma1.find(i.first)!=ma1.end()){
+                N[i.first]=min(i.second,ma1[i.first]);
+            }
+        }
+        if(U.find(i.first)==U.end()){
+            if(ma1.find(i.first)==ma1.end()){
+                U[i.first]=i.second;
+            }else{
+                U[i.first]=max(i.second,ma1[i.first]);
+            }
         }
     }
-    printf("\n");
-    for(auto&i:v2){
-        printf("%s ", i.first.c_str());
-        if(v1.find(i.first)==v1.end()){
-            u+=i.second;
-        }
-    }
-    printf("%d %d\n", n,u);
-    if(u==0){
+    
+    if(U.size()==0){
         return 65536;
     }
-    return (int)((65536*(double)n)/u);
+    int nsum=0,usum=0;
+    for(auto&i:N){
+        nsum+=i.second;
+    }
+    for(auto&i:U){
+        usum+=i.second;
+    }
+    return ((double)nsum/usum)*65536;
 }
