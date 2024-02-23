7de45a433a79d819e734a6ce47980d02c7e4f448
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-23 12:51:38 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.01 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.cpp
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.cpp
@@ -2,32 +2,31 @@
 #include <vector>
 
 using namespace std;
-
-long long getNFac(int n) {
-    long long res = 1;
-    for (int i = 2; i <= n; i++) {
-        res *= i;
+long long p(int n){
+    long long res=1;
+    for(int i=2;i<=n;i++){
+        res*=i;
     }
     return res;
 }
-
 int vis[21];
-
 vector<int> solution(int n, long long k) {
     vector<int> answer;
-    for (int i = 0; i < n; i++) {
-        int d = (k - 1) / getNFac(n - i - 1) + 1;
-        k -= (d-1)*getNFac(n - i - 1);
-        for (int j = 1; j <= 20; j++) {
-            if (!vis[j]) {
-                d--;
-                if (d == 0) {
-                    answer.push_back(j);
-                    vis[j] = 1;
-                    break;
-                }
+    for(int i=1;i<=n;i++){
+        long long a=(k+p(n-i)-1)/p(n-i);
+        int c=0;
+        for(int i=1;i<=n;i++){
+            if(vis[i]){
+                continue;
+            }
+            c++;
+            if(c==a){
+                vis[i]=1;
+                answer.push_back(i);
+                break;
             }
         }
+        k-=(a-1)*p(n-i);
     }
     return answer;
 }
