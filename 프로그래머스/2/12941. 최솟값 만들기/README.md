79191adf53803c45cf3f9a226b8868b209e02b84
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-23 12:21:16 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.13 ms, Memory: 3.83 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/README.md
+++ 프로그래머스/2/12941. 최솟값 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최솟값 만들기 - 12941 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12941?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12941) 
 
 ### 성능 요약
 
-메모리: 3.77 MB, 시간: 0.07 ms
+메모리: 3.83 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 5일 19:21:41
+2024년 02월 23일 12:21:14
 
 ### 문제 설명
 
--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cpp
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include<vector>
-#include <algorithm>
+#include <queue>
 using namespace std;
 
-int solution(vector<int> A, vector<int> B)
-{
-    int answer = 0;
-    sort(A.begin(),A.end());
-    sort(B.begin(),B.end());
+int solution(vector<int> A, vector<int> B) {
+    int answer=0;
+    priority_queue<int> p,q;
     for(int i=0;i<A.size();i++){
-        answer+=A[i]*B[B.size()-i-1];
+        q.push(A[i]);
+        p.push(-B[i]);
+    }
+    while(!q.empty()){
+        answer+=q.top()*-p.top();
+        q.pop();
+        p.pop();
     }
     return answer;
 }
