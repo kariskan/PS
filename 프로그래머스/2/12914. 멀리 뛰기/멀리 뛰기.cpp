9f02ef2aa8efb0f49845b080f8641ea2aa39778b
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-26 12:33:59 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -2,11 +2,8 @@
 #include <vector>
 
 using namespace std;
-long long dp[2001];
+int dp[2001]={0,1,2};
 long long solution(int n) {
-    long long answer = 0;
-    dp[1]=1;
-    dp[2]=2;
     for(int i=3;i<=n;i++){
         dp[i]=(dp[i-1]+dp[i-2])%1234567;
     }
