695f6080bc69c168e7771e3e7ea6fc8800e20d9a
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-26 12:31:15 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 6.33 ms, Memory: 42.6 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
 #include<vector>
 using namespace std;
-
-int solution(vector<vector<int>> board)
-{
-    int answer=0;
+int map[1001][1001];
+int solution(vector<vector<int>> board){
     int n=board.size(),m=board[0].size();
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(board[i][j]){
-                answer=max(answer,1);
+    int answer=0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            map[i][j]=board[i-1][j-1];
+        }
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(map[i][j]){
+                if(map[i-1][j]==0||map[i][j-1]==0){
+                    map[i][j]=1;
+                    continue;
+                }
+                int mi=min(map[i-1][j],map[i][j-1]);
+                map[i][j]=mi;
+                if(i>=mi&&j>=mi&&map[i-mi][j-mi]){
+                    map[i][j]++;
+                }
             }
-            if(i==0||j==0||!board[i-1][j]||!board[i][j-1]||!board[i][j]){
-                continue;
-            }
-            int mi=min(board[i-1][j],board[i][j-1]);
-            board[i][j]=mi;
-            if(board[i-mi][j-mi]){
-                board[i][j]++;
-            }
-            answer=max(answer,board[i][j]*board[i][j]);
+        }
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            answer=max(answer,map[i][j]*map[i][j]);
         }
     }
     return answer;
