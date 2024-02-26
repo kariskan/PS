0e0ea8c9186283ff01bfa5bb1d4ed07c7a4796f8
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-26 11:46:15 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.02 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.cpp
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.cpp
@@ -1,33 +1,27 @@
 #include <string>
-#include <cmath>
-#include <iostream>
-using namespace std;
-int a[4] = {1, 2, 4};
-long long getT[20]={0,};
-void go() {
-    int t=0,p=3,cnt=1;
-    while (cnt<20) {
-        getT[cnt]=t;
-        t+=p;
-        p*=3;
-        cnt++;
-    }
-}
+#include <vector>
 
+using namespace std;
+
+int d[3]={1,2,4};
 string solution(int n) {
-    go();
-    int t = 0, p = 3, cnt = 0;
-    while (t < n) {
-        t += p;
-        cnt++;
-        p *= 3;
-    }
     string answer = "";
-    while (cnt) {
-        int nxt = (n - getT[cnt] - 1) / (long long) pow(3, cnt - 1);
-        answer += a[nxt] + '0';
-        n -= (nxt + 1) * (long long) pow(3, cnt - 1);
-        cnt--;
+    int digit=1;
+    int p=3,sum=0;
+    while(sum+p<n){
+        digit++;
+        sum+=p;
+        p*=3;
+    }
+    // digit=3,p=27,sum=12
+    p/=3;
+    for(int i=1;i<=digit;i++){
+        int temp=(n-sum-1)/p; // 0
+        answer+=d[temp]+'0';
+        printf("%d %d %d %d\n",i,n,sum,p,temp);
+        n-=p*(temp+1);
+        sum-=p;
+        p/=3;
     }
     return answer;
 }
