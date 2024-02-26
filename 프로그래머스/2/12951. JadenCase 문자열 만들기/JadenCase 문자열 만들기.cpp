a7a1c00ccd75f8ca4d1e89173ceeaa405b67481f
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-26 10:32:51 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -1,38 +1,42 @@
 #include <string>
 #include <vector>
+#include <sstream>
 using namespace std;
-
-string solution(string s) {
-    string answer = "";
+string toJaden(string s){
+    if(s[0]>='a'&&s[0]<='z'){
+        s[0]=s[0]-'a'+'A';
+    }
+    for(int i=1;i<s.length();i++){
+        if(s[i]>='A'&&s[i]<='Z'){
+            s[i]=s[i]-'A'+'a';
+        }
+    }
+    return s;
+}
+vector<string> parse(string s){
+    vector<string> v;
     string temp="";
     for(int i=0;i<s.length();i++){
         if(s[i]==' '){
             if(!temp.empty()){
-                if(temp[0]>='a'&&temp[0]<='z'){
-                    temp[0]=temp[0]-'a'+'A';
-                }
-                for(int j=1;j<(int)temp.length();j++){
-                    if(temp[j]>='A'&&temp[j]<='Z'){
-                        temp[j]=temp[j]-'A'+'a';
-                    }
-                }
+                v.push_back(temp);
             }
-            answer+=temp+" ";
+            v.push_back(" ");
             temp="";
             continue;
         }
         temp+=s[i];
     }
     if(!temp.empty()){
-                if(temp[0]>='a'&&temp[0]<='z'){
-                    temp[0]=temp[0]-'a'+'A';
-                }
-                for(int j=1;j<(int)temp.length();j++){
-                    if(temp[j]>='A'&&temp[j]<='Z'){
-                        temp[j]=temp[j]-'A'+'a';
-                    }
-                }
-            }
-            answer+=temp;
+        v.push_back(temp);
+    }
+    return v;
+}
+string solution(string s) {
+    string answer="";
+    vector<string> v=parse(s);
+    for(int i=0;i<v.size();i++){
+        answer+=toJaden(v[i]);
+    }
     return answer;
 }
