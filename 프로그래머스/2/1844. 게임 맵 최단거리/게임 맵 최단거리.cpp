dedaea30f259711f5ea744cfd0f8db9b253c1055
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-26 10:55:38 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.18 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,29 +1,27 @@
 #include<vector>
 #include<queue>
 using namespace std;
-
 int vis[100][100];
 int dx[4]={0,0,1,-1};
 int dy[4]={1,-1,0,0};
-
-int solution(vector<vector<int>> maps)
-{
-    queue<pair<int,int>>q;
+int solution(vector<vector<int>> maps){
+    int n=maps.size(),m=maps[0].size();
+    queue<pair<int,int>> q;
     q.push({0,0});
     vis[0][0]=1;
     while(!q.empty()){
         int x=q.front().first;
         int y=q.front().second;
         q.pop();
-        if(x==maps.size()-1&&y==maps[0].size()-1){
+        if(x==n-1&&y==m-1){
             return vis[x][y];
         }
         for(int i=0;i<4;i++){
             int nx=x+dx[i];
             int ny=y+dy[i];
-            if(nx>=0&&nx<maps.size()&&ny>=0&&ny<maps[0].size()&&!vis[nx][ny]&&maps[nx][ny]==1){
-                q.push({nx,ny});
+            if(nx>=0&&nx<n&&ny>=0&&ny<m&&!vis[nx][ny]&&maps[nx][ny]==1){
                 vis[nx][ny]=vis[x][y]+1;
+                q.push({nx,ny});
             }
         }
     }
