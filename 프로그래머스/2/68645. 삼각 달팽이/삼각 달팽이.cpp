0d0e5fb044040656d338a2c5513cf255ab22a134
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-27 10:14:23 +0900

    [level 2] Title: 삼각 달팽이, Time: 158.75 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.cpp
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.cpp
@@ -2,17 +2,18 @@
 #include <vector>
 
 using namespace std;
+
 int dx[3]={1,0,-1};
 int dy[3]={0,1,-1};
-int map[1001][1001];
+int map[1000][1000];
 vector<int> solution(int n) {
     vector<int> answer;
-    int x=-1,y=0,dir=0,cnt=1;
+    int x=-1,y=0,dir=0,num=1;
     for(int i=n;i>=1;i--){
         for(int j=0;j<i;j++){
             x+=dx[dir];
             y+=dy[dir];
-            map[x][y]=cnt++;
+            map[x][y]=num++;
         }
         dir=(dir+1)%3;
     }
