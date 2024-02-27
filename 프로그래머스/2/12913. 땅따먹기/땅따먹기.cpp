07b5c6297b5aca1c33f4f88fef690944463def68
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-27 10:23:51 +0900

    [level 2] Title: 땅따먹기, Time: 7.94 ms, Memory: 27.4 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
 int dp[100000][4];
-int solution(vector<vector<int> > land)
-{
-    int answer = 0;
+int solution(vector<vector<int>> land){
     for(int i=0;i<4;i++){
         dp[0][i]=land[0][i];
     }
+    int answer=0;
     for(int i=1;i<land.size();i++){
         for(int j=0;j<4;j++){
-            int m=0;
             for(int k=0;k<4;k++){
                 if(j==k){
                     continue;
                 }
-                m=max(m,dp[i-1][k]);
+                dp[i][j]=max(dp[i][j],land[i][j]+dp[i-1][k]);
             }
-            dp[i][j]+=land[i][j]+m;
-            answer=max(answer,dp[i][j]);
+            if(i==land.size()-1){
+                answer=max(answer,dp[i][j]);
+            }
         }
     }
     return answer;
