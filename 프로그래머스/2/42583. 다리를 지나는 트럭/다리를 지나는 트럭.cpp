f5c010b75893fefdf4c73a179d009ad5a0229cab
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-27 11:23:51 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -2,29 +2,21 @@
 #include <vector>
 #include <queue>
 using namespace std;
-
-int solution(int len, int weight, vector<int> weights) {
-    int answer = 0, nowWeight=0,idx=0;
+//                2          10            [7,4,5,6]
+int solution(int length, int weight, vector<int> truck) {
+    int answer = 0;
+    int nowWeight=0,idx=0;
     queue<pair<int,int>> q;
-    for(int t=1;;t++){
-        int qSize=q.size();
-        for(int i=0;i<qSize;i++){
-            q.front().second++;
-            pair<int,int> front=q.front();
+    while(!q.empty()||idx<truck.size()){
+        if(!q.empty()&&q.front().first+length<=answer){
+            nowWeight-=truck[q.front().second];
             q.pop();
-            if(front.second==len){
-                nowWeight-=weights[front.first];
-                continue;
-            }
-            q.push(front);
         }
-        if(idx<weights.size()&&q.size()<len&&nowWeight+weights[idx]<=weight){
-            nowWeight+=weights[idx];
-            q.push({idx++,0});
+        if(idx<truck.size()&&nowWeight+truck[idx]<=weight&&q.size()<length){
+            nowWeight+=truck[idx];
+            q.push({answer,idx++});
         }
-        if(idx>=weights.size()&&q.empty()){
-            return t;
-        }
+        answer++;
     }
     return answer;
 }
