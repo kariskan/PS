f2eaa93302f939f63035f4683e867fdb426f8b58
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-29 10:11:38 +0900

    [level 2] Title: 귤 고르기, Time: 29.86 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -1,28 +1,26 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <algorithm>
 #include <map>
 using namespace std;
-map<int,int> ma;
-bool compare(pair<int,int>&p1,pair<int,int>&p2){
-    return p1.second>p2.second;
-}
+
 int solution(int k, vector<int> tangerine) {
     int answer = 0;
-    for(int i=0;i<tangerine.size();i++){
-        ma[tangerine[i]]++;
+    sort(tangerine.begin(),tangerine.end());
+    map<int,int> ma;
+    for(int t:tangerine){
+        ma[t]++;
     }
     priority_queue<int> q;
     for(auto&i:ma){
         q.push(i.second);
     }
-    int s=0;
-    while(!q.empty()){
-        if(s+q.top()>=k){
-            return answer+1;
-        }
-        s+=q.top();
-        answer++;
+    int cnt=0;
+    while(cnt<k){
+        cnt+=q.top();
         q.pop();
+        answer++;
     }
+    return answer;
 }
