58e096609a98c958f31b726d02232f2d55c05097
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-29 11:51:17 +0900

    [level 3] Title: 스티커 모으기(2), Time: 0.77 ms, Memory: 7.09 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-int solution(vector<int> sticker) {
-    int dp[100000][2] = {0,}; //i==0일때 안땜, i==1일때 땜
-    dp[0][1] = dp[1][1] = sticker[0];
-    dp[1][0] = sticker[1];
-    for (int i = 2; i < sticker.size(); i++) {
-        dp[i][0] = max(dp[i - 2][0] + sticker[i], dp[i - 1][0]);
-        if (i == sticker.size() - 1) {
-            dp[i][1] = dp[i - 1][1];
-        } else {
-            dp[i][1] = max(dp[i - 2][1] + sticker[i], dp[i - 1][1]);
+int dp[100001][2];
+int solution(vector<int> sticker){
+    dp[1][1]=sticker[0];
+    for(int i=2;i<=sticker.size();i++){
+        dp[i][0]=max(dp[i-1][0],dp[i-2][0]+sticker[i-1]);
+        dp[i][1]=max(dp[i-1][1],dp[i-2][1]+sticker[i-1]);
+        if(i==sticker.size()){
+            dp[i][1]=dp[i-1][1];
         }
     }
-    return max(dp[sticker.size() - 1][0], dp[sticker.size() - 1][1]);
+    return max(dp[sticker.size()][0],dp[sticker.size()][1]);
 }
-
