a7c08baf95a4a5ad91c4d0407154b91b9343e736
kariskan <73534426+kariskan@users.noreply.github.com>
2024-02-29 11:20:30 +0900

    [level 3] Title: 기지국 설치, Time: 0.06 ms, Memory: 3.96 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
@@ -2,22 +2,17 @@
 #include <vector>
 using namespace std;
 
-int solution(int n, vector<int> stations, int w)
-{
+int solution(int n, vector<int> stations, int w){
     int answer = 0;
-    int last=1;
-    vector<int> v;
+    
+    int left=0,right=(n+w*2)/(w*2+1);
+    int pre=0;
     for(int i=0;i<stations.size();i++){
-        v.push_back(stations[i]-w-1-last+1);
-        last=stations[i]+w+1;
+        int now=stations[i]-w-1;
+        answer+=max(0,(now-pre+w*2)/(w*2+1));
+        pre=stations[i]+w;
     }
-    if(last<=n){
-        v.push_back(n-last+1);
-    }
-    int left=0,right=n-stations.size();
-    int cnt=0,len=1+2*w;
-    for(int i=0;i<v.size();i++){
-        cnt+=(v[i]+len-1)/len;
-    }
-    return cnt;
+    answer+=max(0,(n-pre+w*2)/(w*2+1));
+    
+    return answer;
 }
