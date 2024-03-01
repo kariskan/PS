98e196c1e0b24c77e2ffc71a27787a256536013c
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-01 11:09:54 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 1.46 ms, Memory: 9.63 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
@@ -3,78 +3,38 @@
 
 using namespace std;
 
+long long traversal(int cap, vector<int>& v, long long idx){
+    while(idx>=0&&cap){
+        int mi=min(cap,v[idx]);
+        cap-=mi;
+        v[idx]-=mi;
+        
+        while(idx>=0&&v[idx]==0){
+            idx--;
+        }
+    }
+    return idx;
+}
+
 long long solution(int cap, int n, vector<int> deliveries, vector<int> pickups) {
     long long answer = 0;
-    int totalDelivery=0,totalPickup=0,lastDeliveryIdx=-1,lastPickupIdx=-1;
+    
+    long long deliveryIdx=-1,pickupIdx=-1;
     for(int i=0;i<n;i++){
-        totalDelivery+=deliveries[i];
-        totalPickup+=pickups[i];
         if(deliveries[i]){
-            lastDeliveryIdx=i;
+            deliveryIdx=i;
         }
         if(pickups[i]){
-            lastPickupIdx=i;
+            pickupIdx=i;
         }
     }
-
-    while(lastDeliveryIdx!=-1||lastPickupIdx!=-1){
-        int pickupCnt=0,deliveryCnt=0;
-        if(lastDeliveryIdx>=lastPickupIdx){
-            answer+=(lastDeliveryIdx+1)*2;
-            while(deliveryCnt<cap&&lastDeliveryIdx>=0){
-                int diff=min(deliveries[lastDeliveryIdx], cap-deliveryCnt);
-                deliveryCnt+=diff;
-                deliveries[lastDeliveryIdx]-=diff;
-                if(deliveries[lastDeliveryIdx]==0){
-                    lastDeliveryIdx--;
-                }
-            }
-            int tempCnt=cap;
-            for(int i=lastPickupIdx;i>=0;i--){
-                int diff=min(tempCnt, pickups[i]);
-                tempCnt-=diff;
-                pickups[i]-=diff;
-                if(tempCnt==0){
-                    break;
-                }
-            }
-            // printf("1 %d %d %d\n", answer, lastDeliveryIdx, lastPickupIdx);
-        }else{
-            answer+=(lastPickupIdx+1)*2;
-            while(pickupCnt<cap&&lastPickupIdx>=0){
-                int diff=min(pickups[lastPickupIdx], cap-pickupCnt);
-                pickupCnt+=diff;
-                pickups[lastPickupIdx]-=diff;
-                if(pickups[lastPickupIdx]==0){
-                    lastPickupIdx--;
-                }
-            }
-            int tempCnt=cap;
-            for(int i=lastDeliveryIdx;i>=0;i--){
-                int diff=min(tempCnt, deliveries[i]);
-                tempCnt-=diff;
-                deliveries[i]-=diff;
-                if(tempCnt==0){
-                    if(deliveries[i]==0){
-                        lastDeliveryIdx=i-1;
-                    }else{
-                        lastDeliveryIdx=i;
-                    }
-                    break;
-                }
-                if(i==0){
-                    lastDeliveryIdx=-1;
-                }
-            }
-            // printf("1 %d %d %d\n", answer, lastDeliveryIdx, lastPickupIdx);
-        }
-        while(lastPickupIdx>=0&&!pickups[lastPickupIdx]){
-            lastPickupIdx--;
-        }
-        while(lastDeliveryIdx>=0&&!deliveries[lastDeliveryIdx]){
-            lastDeliveryIdx--;
-        }
+    while(deliveryIdx!=-1||pickupIdx!=-1){
+        answer+=(max(deliveryIdx,pickupIdx)+1)*2;
+        
+        deliveryIdx=traversal(cap,deliveries,deliveryIdx);
+        pickupIdx=traversal(cap,pickups,pickupIdx);
+        
     }
-
+    
     return answer;
 }
