29cffb761b89ec1129cf1c5e5dbc028a81855e98
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-01 10:56:34 +0900

    [level 3] Title: 불량 사용자, Time: 0.33 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
@@ -1,61 +1,53 @@
 #include <string>
 #include <vector>
-#include <set>
 #include <algorithm>
 using namespace std;
-vector<string> ban,user;
-int answer=0;
-set<string> se;
-bool check2(string banned,string origin){
-    for(int i=0;i<banned.length();i++){
-        if(banned[i]=='*'){
-            continue;
-        }
-        if(banned[i]!=origin[i]){
+
+vector<string> user,ban;
+int answer;
+
+bool isValid(vector<string> selected){
+    int n=selected.size();
+    
+    for(int i=0;i<n;i++){
+        if(selected[i].length()!=ban[i].length()){
             return false;
+        }
+        int len=selected[i].length();
+        for(int j=0;j<len;j++){
+            if(ban[i][j]=='*'){
+                continue;
+            }
+            if(selected[i][j]!=ban[i][j]){
+                return false;
+            }
         }
     }
     return true;
 }
-bool check(vector<string> selected){
-    for(int i=0;i<selected.size();i++){
-        if(!check2(ban[i],selected[i])){
-            return false;
-        }
-    }
-    sort(selected.begin(),selected.end());
-    string res="";
-    for(int i=0;i<selected.size();i++){
-        res+=selected[i];
-    }
-    if(se.find(res)!=se.end()){
-        return false;
-    }
-    se.insert(res);
-    return true;
-}
-int vis[8];
-void go(vector<string> selected, int idx){
-    if(idx>=user.size()||selected.size()==ban.size()){
-        if(check(selected)){
-            answer++;
-        }
+
+void dfs(vector<string> selected, int idx){
+    if(selected.size()==ban.size()){
+        sort(selected.begin(),selected.end());
+        do{
+            if(isValid(selected)){
+                answer++;
+                break;
+            }
+        }while(next_permutation(selected.begin(),selected.end()));
         return;
     }
-    for(int i=0;i<user.size();i++){
-        if(user[i].length()==ban[idx].length()&&!vis[i]){
-            selected.push_back(user[i]);
-            vis[i]=1;
-            go(selected,idx+1);
-            selected.pop_back();
-            vis[i]=0;
-        }
+    if(idx>=user.size()){
+        return;
     }
+    dfs(selected,idx+1);
+    selected.push_back(user[idx]);
+    dfs(selected,idx+1);
 }
 
 int solution(vector<string> user_id, vector<string> banned_id) {
-    ban=banned_id;
     user=user_id;
-    go({},0);
+    ban=banned_id;
+    dfs({},0);
     return answer;
 }
