814a490f0bda8ddf3ca2ebee8e3c312a96e811c2
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-04 10:04:03 +0900

    [level 3] Title: 산 모양 타일링, Time: 1.21 ms, Memory: 5.39 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.cpp
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.cpp
@@ -2,24 +2,18 @@
 #include <vector>
 
 using namespace std;
-
-long long dp[100000*2+2];
-long long mod=10007;
+int mod=10007, dp[200002];
 int solution(int n, vector<int> tops) {
-    int answer = 0;
     dp[0]=1;
-    dp[1]=1;
-    for(int i=2;i<=n*2+1;i++){
-        if(i%2==0){
-            if(tops[(i-1)/2]){ //위 삼각형이 존재함
-                dp[i]=dp[i-1]*2+dp[i-2];
-            }else{
-                dp[i]=dp[i-1]+dp[i-2];
-            }
+    dp[1]=(tops[0]==0?2:3);
+    for(int i=2;i<2*n+1;i++){
+        if(i%2==1&&tops[i/2]==1){
+            dp[i]=dp[i-1]*2+dp[i-2];
         }else{
             dp[i]=dp[i-1]+dp[i-2];
         }
         dp[i]%=mod;
     }
-    return dp[2*n+1];
+    
+    return dp[n*2];
 }
