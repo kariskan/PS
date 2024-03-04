4998a4480318456a7fb551e0c7e374a93db62d6d
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-04 12:29:06 +0900

    [level 3] Title: 인사고과, Time: 6115.44 ms, Memory: 25.2 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.cpp
+++ 프로그래머스/3/152995. 인사고과/인사고과.cpp
@@ -1,33 +1,44 @@
 #include <string>
 #include <vector>
-#include <queue>
 #include <algorithm>
 using namespace std;
-
-bool compare(vector<int>& v1, vector<int>& v2){
+int flag[100000];
+bool compare(vector<int>& v1,vector<int>& v2){
     if(v1[0]==v2[0]){
-        return v1[1]<v2[1];
+        return v1[1]>v2[1];
     }
     return v1[0]>v2[0];
 }
-
 int solution(vector<vector<int>> scores) {
-    int answer=0;
-    vector<int> score=scores[0];
-    
-    int ma=0;
+    int answer=1;
+    int x=scores[0][0], y=scores[0][1];
     sort(scores.begin(),scores.end(),compare);
-    for(int i=0;i<scores.size();i++){
-        if(ma>scores[i][1]){
-            if(scores[i][0]==score[0]&&scores[i][1]==score[1]){
-                return -1;
-            }
-        }else{
-            ma=max(ma,scores[i][1]);
-            if(score[0]+score[1]<scores[i][0]+scores[i][1]){
-                answer++;
-            }
+    
+    vector<vector<int>> v={{scores[0][0],scores[0][1]}};
+    for(int i=1;i<scores.size();i++){
+        if(scores[i][0]!=v.back()[0]){
+            v.push_back({scores[i][0],scores[i][1]});
+            
         }
     }
-    return answer+1;
+    
+    for(int i=0;i<scores.size();i++){
+        for(int j=0;j<v.size();j++){
+            // printf("%d %d %d %d \n", scores[i][0],scores[i][1], v[j][0],v[j][1]);
+            if(scores[i][0]>=v[j][0]){
+                break;
+            }
+            if(v[j][1]>scores[i][1]){
+                flag[i]=1;
+                if(scores[i][0]==x&&scores[i][1]==y){
+                    return -1;
+                }
+                break;
+            }
+        }
+        if(!flag[i]&&x+y<scores[i][0]+scores[i][1]){
+            answer++;
+        }
+    }
+    return answer;
 }
