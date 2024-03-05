e3d95f5e1682aa957098d9dfdafab8685aaf36c5
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-05 12:45:49 +0900

    [level 3] Title: 숫자 타자 대회, Time: 60.56 ms, Memory: 49.7 MB -BaekjoonHub

--- 프로그래머스/3/136797. 숫자 타자 대회/숫자 타자 대회.cpp
+++ 프로그래머스/3/136797. 숫자 타자 대회/숫자 타자 대회.cpp
@@ -2,60 +2,36 @@
 #include <vector>
 
 using namespace std;
-
-// 왼손이 i, 오른손이 j에 있고, k번째 숫자를 누를 때 최솟값
-int dp[100001][10][10];
-pair<int,int> pos[10]={
-        {3,1},
-        {0,0},
-        {0,1},
-        {0,2},
-        {1,0},
-        {1,1},
-        {1,2},
-        {2,0},
-        {2,1},
-        {2,2}
-};
-
-int getDis(int thumb, int number){
-    pair<int,int> thumbPos=pos[thumb];
-    pair<int,int> numberPos=pos[number];
-
-    if(thumbPos.first==numberPos.first&&thumbPos.second==numberPos.second){
-        return 1;
-    }else if(thumbPos.first==numberPos.first){
-        return abs(thumbPos.second-numberPos.second)*2;
-    }else if(thumbPos.second==numberPos.second){
-        return abs(thumbPos.first-numberPos.first)*2;
-    }else{
-        int mi=min(abs(numberPos.first-thumbPos.first),abs(numberPos.second-thumbPos.second));
-        int ma=max(abs(numberPos.first-thumbPos.first),abs(numberPos.second-thumbPos.second));
-        return (ma-mi)*2+mi*3;
-    }
-}
-
+pair<int,int> pos[10]={{3,1},{0,0},{0,1},{0,2},{1,0},{1,1},{1,2},{2,0},{2,1},{2,2}};
+int dp[100000][10][10];
 string numbers;
-
-int go(int left, int right, int idx){
+int getCost(int now,int target){
+    if(now==target){
+        return 1;
+    }
+    pair<int,int> p1=pos[now];
+    pair<int,int> p2=pos[target];
+    int mi=min(abs(p1.first-p2.first),abs(p1.second-p2.second));
+    int ma=max(abs(p1.first-p2.first),abs(p1.second-p2.second));
+    return mi*3+(ma-mi)*2;
+}
+int go(int idx,int left,int right){
     if(idx>=numbers.length()){
         return 0;
     }
     if(dp[idx][left][right]){
         return dp[idx][left][right];
     }
-    if(left==numbers[idx]-'0'||right==numbers[idx]-'0'){
-        return dp[idx][left][right]=1+go(left,right,idx+1);
+    if(numbers[idx]-'0'==left||numbers[idx]-'0'==right){
+        return dp[idx][left][right]=1+go(idx+1,left,right);
     }
-    if(left==1&&right==5&&idx==4){
-        int p=1;
-    }
-    int l = getDis(left,numbers[idx]-'0')+go(numbers[idx]-'0',right,idx+1);
-    int r = getDis(right,numbers[idx]-'0')+go(left,numbers[idx]-'0',idx+1);
+    //왼손을 움직임
+    int l=getCost(left,numbers[idx]-'0')+go(idx+1,numbers[idx]-'0',right);
+    //오른손을 움직임
+    int r=getCost(right,numbers[idx]-'0')+go(idx+1,left,numbers[idx]-'0');
     return dp[idx][left][right]=min(l,r);
 }
-
 int solution(string numbers) {
     ::numbers=numbers;
-    return go(4,6,0);
+    return go(0,4,6);
 }
