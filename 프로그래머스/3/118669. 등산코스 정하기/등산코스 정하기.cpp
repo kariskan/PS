99433e18bd7c862d76cf9698faa0b6a835453dff
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-06 11:27:42 +0900

    [level 3] Title: 등산코스 정하기, Time: 74.39 ms, Memory: 53.3 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.cpp
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.cpp
@@ -2,18 +2,21 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
-#include <climits>
 using namespace std;
-
-vector<vector<pair<int, int>>> v;
+vector<vector<pair<int,int>>> v;
+bool isGate[50001], isSummit[50001];
 int n;
-int isGates[50001],isSummits[50001];
+
 vector<int> dijk(){
-    vector<int> dis(n+1,INT_MAX);
+    int des=0,res=2*1e9;
+    
+    vector<int> dis(v.size(), 2*1e9);
     priority_queue<pair<int,int>> pq;
+    
     for(int i=1;i<=n;i++){
-        if(isGates[i]){
-            pq.push({INT_MAX,i});
+        if(isGate[i]){
+            pq.push({0,i});
+            dis[i]=0;
         }
     }
     while(!pq.empty()){
@@ -21,45 +24,45 @@ vector<int> dijk(){
         int cost=-pq.top().first;
         pq.pop();
         
-        for(auto&i:v[node]){
-            int nxCost=max(cost,i.second);
-            int nxNode=i.first;
-            if(isGates[nxNode]){
-                continue;
+        if(isSummit[node]){
+            if(res>cost||(res==cost&&des>node)){
+                des=node;
+                res=cost;
             }
-            if(dis[nxNode]>nxCost){
+            continue;
+        }
+        
+        for(auto&i:v[node]){
+            int nxNode=i.first;
+            int nxCost=max(i.second,cost);
+            if(nxCost<dis[nxNode]){
                 dis[nxNode]=nxCost;
-                if(isSummits[nxNode]){
+                if(isGate[nxNode]){
                     continue;
                 }
                 pq.push({-nxCost,nxNode});
             }
         }
     }
-    return dis;
+    return {des,res};
 }
 
 vector<int> solution(int n, vector<vector<int>> paths, vector<int> gates, vector<int> summits) {
-    vector<int> answer={50000,10000000};
     ::n=n;
+    
+    sort(gates.begin(),gates.end());
+    sort(summits.begin(),summits.end());
+    for(int gate:gates){
+        isGate[gate]=true;
+    }
+    for(int summit:summits){
+        isSummit[summit]=true;
+    }
     v.resize(n+1);
-    for(vector<int> path:paths){
-        v[path[0]].push_back({path[1],path[2]});
-        v[path[1]].push_back({path[0],path[2]});
+    for(int i=0;i<paths.size();i++){
+        v[paths[i][0]].push_back({paths[i][1],paths[i][2]});
+        v[paths[i][1]].push_back({paths[i][0],paths[i][2]});
     }
-    int gSize=gates.size(),sSize=summits.size();
-    for(int i=0;i<gSize;i++){
-        isGates[gates[i]]=1;
-    }
-    for(int i=0;i<sSize;i++){
-        isSummits[summits[i]]=1;
-    }
-    vector<int> d=dijk();
-    for(int j=0;j<sSize;j++){
-        int ans=d[summits[j]];
-        if(answer[1]>ans||(answer[1]==ans&&answer[0]>summits[j])){
-            answer={summits[j],ans};
-        }
-    }
-    return answer;
+    
+    return dijk();
 }
