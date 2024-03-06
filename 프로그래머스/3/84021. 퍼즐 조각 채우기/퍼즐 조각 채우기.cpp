b490c37d5b7bd1b95edf69c2a5215c49b989043b
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-06 19:36:49 +0900

    [level 3] Title: 퍼즐 조각 채우기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.cpp
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.cpp
@@ -1,109 +1,112 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <cstring>
 using namespace std;
+int n,m,vis[50][50];
 int dx[4]={0,0,1,-1};
 int dy[4]={1,-1,0,0};
-int n;
-vector<vector<int>> rotate(vector<vector<int>> v) {
-    int r=v.size(),c=v[0].size();
-    vector<vector<int>> res(c,vector<int>(r));
-    for(int i=0;i<c;i++){
-        for(int j=0;j<r;j++){
-            res[i][j]=v[j][c-i-1];
+vector<vector<int>> rotate(vector<vector<int>> r){
+    vector<vector<int>> res(r[0].size(),vector<int>(r.size()));
+    for(int i=0;i<res.size();i++){
+        for(int j=0;j<res[0].size();j++){
+            res[i][j]=r[j][res.size()-i-1];
         }
     }
     return res;
 }
-vector<vector<int>> bfs(vector<vector<int>>& vis,vector<vector<int>> board, int sx, int sy){
-    queue<pair<int,int>>q,t;
-    vis[sx][sy]=1;
-    q.push({sx,sy});
-    int minx=50,maxx=0,miny=50,maxy=0;
-    while(!q.empty()){
-        int x=q.front().first;
-        int y=q.front().second;
-        q.pop();
 
-        t.push({x,y});
-        minx=min(minx,x);
-        maxx=max(maxx,x);
-        miny=min(miny,y);
-        maxy=max(maxy,y);
-
-        for(int i=0;i<4;i++){
-            int nx=x+dx[i];
-            int ny=y+dy[i];
-            if(nx>=0&&nx<n&&ny>=0&&ny<n&&!vis[nx][ny]&&board[nx][ny]==board[x][y]){
-                vis[nx][ny]=1;
-                q.push({nx,ny});
+vector<vector<vector<int>>> dfs(vector<vector<int>> board,int flag){
+    memset(vis,0,sizeof(vis));
+    vector<vector<vector<int>>> v;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(board[i][j]==flag&&!vis[i][j]){
+                queue<pair<int,int>> temp,q;
+                
+                q.push({i,j});
+                vis[i][j]=1;
+                
+                int minx=n,miny=m,maxx=0,maxy=0;
+                
+                while(!q.empty()){
+                    int x=q.front().first;
+                    int y=q.front().second;
+                    q.pop();
+                    
+                    temp.push({x,y});
+                    minx=min(minx,x);
+                    miny=min(miny,y);
+                    maxx=max(maxx,x);
+                    maxy=max(maxy,y);
+                    
+                    for(int k=0;k<4;k++){
+                        int nx=x+dx[k];
+                        int ny=y+dy[k];
+                        if(nx>=0&&nx<n&&ny>=0&&ny<m&&!vis[nx][ny]&&board[nx][ny]==flag){
+                            vis[nx][ny]=1;
+                            q.push({nx,ny});
+                        }
+                    }
+                }
+                
+                vector<vector<int>> insert(maxx-minx+1,vector<int>(maxy-miny+1));
+                while(!temp.empty()){
+                    int x=temp.front().first;
+                    int y=temp.front().second;
+                    temp.pop();
+                    insert[x-minx][y-miny]=1;
+                }
+                v.push_back(insert);
             }
         }
     }
-    vector<vector<int>> res(maxx-minx+1,vector<int>(maxy-miny+1));
-    while(!t.empty()){
-        res[t.front().first-minx][t.front().second-miny]=1;
-        t.pop();
+    return v;
+}
+
+bool check(vector<vector<int>> v1,vector<vector<int>> v2){
+    for(int r=0;r<4;r++){
+        v2=rotate(v2);
+        if(v1.size()!=v2.size()||v1[0].size()!=v2[0].size()){
+            continue;
+        }
+        bool flag=true;
+        for(int i=0;i<v1.size();i++){
+            for(int j=0;j<v1[0].size();j++){
+                if(v1[i][j]!=v2[i][j]){
+                    flag=false;
+                    break;
+                }
+            }
+        }
+        if(flag){
+            return true;
+        }
     }
-    return res;
+    return false;
 }
 
 int solution(vector<vector<int>> board, vector<vector<int>> table) {
-    n=board.size();
-    int answer = 0;
-    vector<vector<vector<int>>> v1,v2;
-    vector<vector<int>> vis1(50,vector<int>(50)),vis2(50,vector<int>(50));
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(!vis1[i][j]&&!board[i][j]){
-                v1.push_back(bfs(vis1,board,i,j));
-            }
-            if(!vis2[i][j]&&table[i][j]){
-                v2.push_back(bfs(vis2,table,i,j));
-            }
-        }
-    }
-    int vis3[2500]={0,},vis4[2500]={0,};
-    for(int i=0;i<v1.size();i++){
-        if(vis3[i]){
-            continue;
-        }
-        for(int j=0;j<v2.size();j++){
-            if(vis4[j]){
+    n=board.size(),m=board[0].size();
+    vector<vector<vector<int>>> bp=dfs(board,0);
+    vector<vector<vector<int>>> tp=dfs(table,1);
+    
+    int answer=0;
+    vector<int> tpVis(tp.size());
+    for(int i=0;i<bp.size();i++){
+        for(int j=0;j<tp.size();j++){
+            if(tpVis[j]){
                 continue;
             }
-            bool flag = false;
-            for(int k=0;k<4;k++){
-                v1[i]=rotate(v1[i]);
-                if(v1[i].size()==v2[j].size()&&v1[i][0].size()==v2[j][0].size()){
-                    bool flag2=true;
-                    for(int i1=0;i1<v1[i].size();i1++){
-                        for(int j1=0;j1<v1[i][0].size();j1++){
-                            if(v1[i][i1][j1]!=v2[j][i1][j1]){
-                                flag2=false;
-                                break;
-                            }
-                        }
-                        if(!flag2){
-                            break;
-                        }
-                    }
-                    if(flag2){
-                        flag=true;
-                        break;
-                    }
-                }
-            }
-            if(flag){
-                vis4[j]=1;
-                vis3[i]=1;
-                for(int i1=0;i1<v1[i].size();i1++){
-                    for(int j1=0;j1<v1[i][0].size();j1++){
-                        if(v1[i][i1][j1]==1){
+            if(check(bp[i],tp[j])){
+                for(int i1=0;i1<tp[j].size();i1++){
+                    for(int j1=0;j1<tp[j][0].size();j1++){
+                        if(tp[j][i1][j1]==1){
                             answer++;
                         }
                     }
                 }
+                tpVis[j]=1;
                 break;
             }
         }
