a48e6e4eb19b9f57a7a506b3efcdc047440927db
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-06 16:02:42 +0900

    [level 3] Title: 양과 늑대, Time: 0.16 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -3,35 +3,32 @@
 #include <queue>
 using namespace std;
 vector<vector<int>> v;
-vector<int> info;
 int answer;
-void go(int now, int sheep, int wolf, queue<int> q) {
-    sheep += 1 - info[now];
-    wolf += info[now];
+vector<int> info;
+int cnt;
+void go(int now,int sheep, int wolf, queue<int> q){
     if(sheep<=wolf){
         return;
     }
     answer=max(answer,sheep);
-
     for(auto&i:v[now]){
         q.push(i);
     }
-
     int siz=q.size();
     for(int i=0;i<siz;i++){
         int node=q.front();
         q.pop();
-        go(node,sheep,wolf,q);
+        go(node,sheep+1-info[node],wolf+info[node],q);
         q.push(node);
     }
 }
-
 int solution(vector<int> info, vector<vector<int>> edges) {
     ::info=info;
     v.resize(info.size());
     for(int i=0;i<edges.size();i++){
         v[edges[i][0]].push_back(edges[i][1]);
     }
-    go(0,0,0,{});
+    queue<int> q;
+    go(0,1,0,q);
     return answer;
 }
