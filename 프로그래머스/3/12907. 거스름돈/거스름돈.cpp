bdd51a8256484a3dceeb40e05a3d5980d6333f7e
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-07 12:43:32 +0900

    [level 3] Title: 거스름돈, Time: 4.93 ms, Memory: 3.91 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.cpp
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.cpp
@@ -3,14 +3,10 @@
 
 using namespace std;
 int dp[100001];
-
 int solution(int n, vector<int> money) {
     dp[0]=1;
     for(int i=0;i<money.size();i++){
-        for(int j=1;j<=n;j++){
-            if(j<money[i]){
-                continue;
-            }
+        for(int j=money[i];j<=n;j++){
             dp[j]=(dp[j]+dp[j-money[i]])%1000000007;
         }
     }
