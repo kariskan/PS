24c36829b1c46d507987379fb7b952ce4943ec87
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-07 10:49:37 +0900

    [level 3] Title: 아이템 줍기, Time: 0.05 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
@@ -1,59 +1,65 @@
 #include <string>
 #include <vector>
 #include <queue>
-
 using namespace std;
-int vis[101][101],map[101][101];
+int map[101][101],vis[101][101];
 int dx[4]={0,0,1,-1};
 int dy[4]={1,-1,0,0};
-
+bool isValid(int x,int y,int x1,int y1,int x2,int y2){
+    return !(x>x1&&x<x2&&y>y1&&y<y2);
+}
 int solution(vector<vector<int>> rectangle, int characterX, int characterY, int itemX, int itemY) {
     for(int i=0;i<rectangle.size();i++){
-        rectangle[i][0]*=2;
-        rectangle[i][1]*=2;
-        rectangle[i][2]*=2;
-        rectangle[i][3]*=2;
+        for(int j=0;j<4;j++){
+            rectangle[i][j]*=2;
+        }
     }
-    for(int r1=0;r1<rectangle.size();r1++){
-        for(int i=rectangle[r1][0];i<=rectangle[r1][2];i++){
-            for(int j=rectangle[r1][1];j<=rectangle[r1][3];j++){
-                int flag=1;
-                if(i==rectangle[r1][0]||i==rectangle[r1][2]||j==rectangle[r1][1]||j==rectangle[r1][3]){
-                    for(int r2=0;r2<rectangle.size();r2++){
-                        if(r1==r2){
+    int mx=0,my=0;
+    for(int i=0;i<rectangle.size();i++){
+        for(int i1=rectangle[i][0];i1<=rectangle[i][2];i1++){
+            for(int j1=rectangle[i][1];j1<=rectangle[i][3];j1++){
+                if(i1==rectangle[i][0]||i1==rectangle[i][2]||j1==rectangle[i][1]||j1==rectangle[i][3]){
+                    bool flag=true;
+                    for(int j=0;j<rectangle.size();j++){
+                        if(i==j){
                             continue;
                         }
-
-                        if(i>rectangle[r2][0]&&i<rectangle[r2][2]&&j>rectangle[r2][1]&&j<rectangle[r2][3]){
-                            flag=0;
+                        vector<int> rec=rectangle[j];
+                        if(!isValid(i1,j1,rec[0],rec[1],rec[2],rec[3])){
+                            flag=false;
                             break;
                         }
                     }
                     if(flag){
-                        map[i][j]=1;
+                        map[i1][j1]=1;
+                        mx=max(mx,i1);
+                        my=max(my,j1);
                     }
                 }
             }
         }
     }
     queue<pair<int,int>> q;
-    q.push({characterX*2,characterY*2});
     vis[characterX*2][characterY*2]=1;
-
+    q.push({characterX*2,characterY*2});
+    
     while(!q.empty()){
         int x=q.front().first;
         int y=q.front().second;
         q.pop();
+        
         if(x==itemX*2&&y==itemY*2){
-            return (vis[x][y] - 1)/2;
+            return (vis[x][y]-1)/2;
         }
-        for(int k=0;k<4;k++){
-            int nx=x+dx[k];
-            int ny=y+dy[k];
-            if(nx>=0&&nx<=100&&ny>=0&&ny<=100&&map[nx][ny]&&!vis[nx][ny]){
-                q.push({nx,ny});
+        
+        for(int i=0;i<4;i++){
+            int nx=x+dx[i];
+            int ny=y+dy[i];
+            if(nx>=0&&nx<=100&ny>=0&&ny<=100&&!vis[nx][ny]&&map[nx][ny]){
                 vis[nx][ny]=vis[x][y]+1;
+                q.push({nx,ny});
             }
         }
     }
+    return 0;
 }
