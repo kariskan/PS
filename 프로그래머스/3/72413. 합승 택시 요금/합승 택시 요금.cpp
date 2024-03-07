8f2f0a71c246029a2718e275652f38d9d5f86cfa
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-07 12:24:12 +0900

    [level 3] Title: 합승 택시 요금, Time: 1.57 ms, Memory: 3.79 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -1,15 +1,14 @@
 #include <string>
 #include <vector>
-
+#include <queue>
 using namespace std;
-
 int map[201][201];
 
 int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
-    int answer = 1e9;
+    int answer=1e9;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            map[i][j]=-1;
+            map[i][j]=5*1e8;
         }
     }
     for(int i=0;i<fares.size();i++){
@@ -23,19 +22,15 @@ int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
                     map[i][j]=0;
                     continue;
                 }
-                if(map[i][k]!=-1&&map[k][j]!=-1){
-                    if(map[i][j]==-1||map[i][j]>map[i][k]+map[k][j]){
-                        map[i][j]=map[i][k]+map[k][j];
-                    }
+                if(map[i][j]==0||map[i][j]>map[i][k]+map[k][j]){
+                    map[i][j]=map[i][k]+map[k][j];
                 }
             }
         }
     }
     
     for(int i=1;i<=n;i++){
-        if(map[a][i]!=-1&&map[b][i]!=-1&&map[i][s]!=-1){
-            answer=min(answer,map[a][i]+map[b][i]+map[i][s]);
-        }
+        answer=min(answer,map[s][i]+map[i][a]+map[i][b]);
     }
     return answer;
 }
