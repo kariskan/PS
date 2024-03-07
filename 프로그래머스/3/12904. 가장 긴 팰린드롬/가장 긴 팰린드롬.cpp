ac393f48ae8afb81fe9f3266c6ccdc66f0f8036e
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-07 13:05:18 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 55.87 ms, Memory: 23.5 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.cpp
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.cpp
@@ -1,34 +1,26 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
 using namespace std;
-int dp[2501][2501];
+int dp[2500][2500];
 string s;
-bool ispal(int l, int r){
-    string ss=s.substr(l,r-l+1);
-    string t=ss;
-    reverse(t.begin(),t.end());
-    return ss==t;
-}
-int go(int l,int r){
-    if(l>r){
+int go(int left,int right){
+    if(left>right){
         return 0;
     }
-    if(l==r){
-        return 1;
+    if(left==right){
+        return dp[left][right]=1;
     }
-    if(dp[l][r]){
-        return dp[l][r];
+    if(dp[left][right]){
+        return dp[left][right];
     }
+    int l=go(left,right-1);
+    int r=go(left+1,right);
+    int mid=go(left+1,right-1);
     
-    int left=go(l,r-1);
-    int right=go(l+1,r);
-    int mid=go(l+1,r-1);
-    if(mid==r-1-l&&r>l&&s[l]==s[r]){
+    if(mid==right-left-1&&s[left]==s[right]){
         mid+=2;
     }
-    // printf("%d %d %d %d %d\n", l, r, left, right, mid);
-    return dp[l][r]=max(left,max(right,mid));
+    return dp[left][right]=max(l,max(r,mid));
 }
 int solution(string s) {
     ::s=s;
