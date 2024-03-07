3d3fa03133e75dc42da1cf6302f08ff566c90e39
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-07 10:08:50 +0900

    [level 2] Title: 순위 검색, Time: 525.89 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.cpp
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.cpp
@@ -1,77 +1,103 @@
 #include <string>
 #include <vector>
-#include <map>
 #include <sstream>
+#include <map>
 #include <algorithm>
-
 using namespace std;
 
-vector<string> parse(string s) {
-    stringstream ss(s);
-    string temp;
+vector<string> t[4]={
+    {"-","cpp","java","python"},
+    {"-","backend","frontend"},
+    {"-","junior","senior"},
+    {"-","chicken","pizza"}
+};
+vector<vector<string>> infos;
+map<string,vector<int>> ma;
+
+vector<string> parse(string s){
     vector<string> v;
-    while (getline(ss, temp, ' ')) {
+    string temp;
+    stringstream ss(s);
+    while(getline(ss,temp,' ')){
         v.push_back(temp);
     }
     return v;
 }
-
-map<string, vector<int>> ma;
-
-void go(int idx, string now, vector<string> infos, int score) {
-    if (idx >= 4) {
-        ma[now].push_back(score);
+pair<string,int> parse2(string s){
+    string v="";
+    string temp;
+    stringstream ss(s);
+    while(getline(ss,temp,' ')){
+        if(temp=="and"){
+            continue;
+        }
+        if(temp[0]>='0'&&temp[0]<='9'){
+            return {v,stoi(temp)};
+        }
+        v+=temp;
+    }
+}
+string join(vector<string> k){
+    string res="";
+    for(int i=0;i<k.size();i++){
+        res+=k[i];
+    }
+    return res;
+}
+void go(int idx,vector<string> now){
+    if(idx>=4){
+        for(int i=0;i<infos.size();i++){
+            int flag=true;
+            for(int j=0;j<now.size();j++){
+                if(now[j]=="-"){
+                    continue;
+                }
+                if(now[j]!=infos[i][j]){
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag){
+                ma[join(now)].push_back(stoi(infos[i].back()));
+            }
+        }
         return;
     }
-    go(idx + 1, now, infos, score);
-    go(idx + 1, now + infos[idx], infos, score);
+    for(int i=0;i<t[idx].size();i++){
+        now.push_back(t[idx][i]);
+        go(idx+1,now);
+        now.pop_back();
+    }
 }
-
 vector<int> solution(vector<string> info, vector<string> query) {
     vector<int> answer;
-    for (int i = 0; i < info.size(); i++) {
-        vector<string> infos = parse(info[i]);
-        go(0, "", infos, stoi(infos[4]));
+    for(int i=0;i<info.size();i++){
+        infos.push_back(parse(info[i]));
     }
-    for (auto &i: ma) {
-        sort(i.second.begin(), i.second.end());
+    go(0,{});
+    for(auto&i:ma){
+        sort(i.second.begin(),i.second.end());
     }
-    for (int i = 0; i < query.size(); i++) {
-        vector<string> queries = parse(query[i]);
-        string q = "";
-        for (int j = 0; j <= 6; j += 2) {
-            if (queries[j] == "-") {
-                continue;
-            }
-            q += queries[j];
-        }
-        vector<int> v = ma[q];
-        int l = 0, r = v.size() - 1;
-        int idx = -1;
-        while (l <= r) {
-            int mid = (l + r) / 2;
-
-            if (v[mid] >= stoi(queries[7])) {
-                r = mid - 1;
-                idx = mid;
-            } else {
-                l = mid + 1;
+    for(string q:query){
+        pair<string,int> p=parse2(q);
+        int siz=ma[p.first].size();
+        int left=0,right=siz-1;
+        int temp=-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            
+            if(ma[p.first][mid]>=p.second){
+                temp=mid;
+                right=mid-1;
+            }else{
+                left=mid+1;
             }
         }
-        if (idx == -1) {
+        if(temp==-1){
             answer.push_back(0);
-        } else {
-            answer.push_back(v.size() - idx);
+        }else{
+            answer.push_back(siz-temp);
         }
     }
     return answer;
-}
-
-int main() {
-    solution({"java backend junior pizza 150", "python frontend senior chicken 210",
-              "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80",
-              "python backend senior chicken 50"},
-             {"java and backend and junior and pizza 100", "python and frontend and senior and chicken 200",
-              "cpp and - and senior and pizza 250", "- and backend and senior and - 150",
-              "- and - and - and chicken 100", "- and - and - and - 150"});
 }
