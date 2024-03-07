176f1361abfed8da8732e392f2fcabe6433ecc42
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-07 10:57:42 +0900

    [level 3] Title: 풍선 터트리기, Time: 9.61 ms, Memory: 46.3 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.cpp
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.cpp
@@ -4,19 +4,21 @@
 using namespace std;
 int lmin[1000000],rmin[1000000];
 int solution(vector<int> a) {
-    int answer = min((int)a.size(),2);
-    lmin[0]=a[0];
-    rmin[a.size()-1]=a[a.size()-1];
+    int answer = 0;
+    lmin[0]=a[0],rmin[a.size()-1]=a[a.size()-1];
     for(int i=1;i<a.size();i++){
         lmin[i]=min(lmin[i-1],a[i]);
         rmin[a.size()-i-1]=min(rmin[a.size()-i],a[a.size()-i-1]);
     }
-    for(int i=1;i<a.size()-1;i++){
+    for(int i=0;i<a.size();i++){
+        if(i==0||i==a.size()-1){
+            answer++;
+            continue;
+        }
         if(lmin[i]<a[i]&&rmin[i]<a[i]){
             continue;
         }
         answer++;
     }
-
     return answer;
 }
