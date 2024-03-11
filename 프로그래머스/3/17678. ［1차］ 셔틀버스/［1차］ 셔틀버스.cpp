0fba122cbbc34025c87132b1c0249be64aefe1f6
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-11 10:18:41 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 2.60 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.cpp
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.cpp
@@ -1,17 +1,14 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <queue>
 using namespace std;
 
-int toTime(string s){
-    return stoi(s.substr(0,2))*60+stoi(s.substr(3));
-}
-string toTime(int t){
+vector<int> vis(24*60+1);
+string toStringTime(int t){
     string res="";
     if(t/60<10){
         res+="0"+to_string(t/60);
-    }else{
+    }else {
         res+=to_string(t/60);
     }
     res+=":";
@@ -22,28 +19,28 @@ string toTime(int t){
     }
     return res;
 }
-
 string solution(int n, int t, int m, vector<string> timetable) {
-    string answer = "";
-    priority_queue<int, vector<int>, greater<int>> q;
-    for(int i=0;i<timetable.size();i++){
-        q.push(toTime(timetable[i]));
+    string answer="";
+    for(string t:timetable){
+        vis[stoi(t.substr(0,2))*60+stoi(t.substr(3))]++;
     }
-    
-    for(int time=9*60+(n-1)*t;time>=0;time--){
-        priority_queue<int,vector<int>,greater<int>> temp=q;
-        temp.push(time);
-        for(int i=9*60,cnt=0;cnt<n;cnt++,i+=t){
-            int mCnt=m;
-            while(!temp.empty()&&temp.top()<=i&&mCnt){
-                mCnt--;
-                temp.pop();
+    for(int i=0;i<=23*60+59;i++){
+        vector<int> temp=vis;
+        temp[i]++;
+        int lastIdx=0;
+        for(int j=9*60;j<=9*60+(n-1)*t;j+=t){
+            int cnt=m;
+            while(cnt&&lastIdx<=j){
+                int mi=min(cnt,temp[lastIdx]);
+                cnt-=mi;
+                temp[lastIdx]-=mi;
+                if(temp[lastIdx]==0){
+                    lastIdx++;
+                }
             }
         }
-        if(temp.empty()||temp.top()>time){
-            printf("%d\n", time);
-            answer=toTime(time);
-            break;
+        if(temp[i]==0){
+            answer=toStringTime(i);
         }
     }
     return answer;
