30c5ee33d401f5d7b330a14b6d000aed7a92546f
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-11 10:42:01 +0900

    [level 3] Title: 고고학 최고의 발견, Time: 54.73 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/3/131702. 고고학 최고의 발견/고고학 최고의 발견.cpp
+++ 프로그래머스/3/131702. 고고학 최고의 발견/고고학 최고의 발견.cpp
@@ -2,53 +2,50 @@
 #include <vector>
 
 using namespace std;
-
-int dx[5]={0,0,0,1,-1};
-int dy[5]={0,1,-1,0,0};
-int n,answer=-1;
+int n,rot[8],answer=-1;
+int dx[4]={0,0,1,-1};
+int dy[4]={1,-1,0,0};
 vector<vector<int>> clockHands;
-int rot[8];
+bool range(int x,int y){
+    return x>=0&&x<n&&y>=0&&y<n;
+}
+
 void go(int idx){
     if(idx==n){
         vector<vector<int>> v=clockHands;
-        int total=0;
+        int cnt=0;
         for(int i=0;i<n;i++){
-            for(int k=0;k<5;k++){
+            v[0][i]=(v[0][i]+rot[i])%4;
+            cnt+=rot[i];
+            for(int k=0;k<4;k++){
                 int nx=dx[k];
                 int ny=i+dy[k];
-                if(nx>=0&&nx<n&&ny>=0&&ny<n){
+                if(range(nx,ny)){
                     v[nx][ny]=(v[nx][ny]+rot[i])%4;
                 }
             }
-            total+=rot[i];
         }
         for(int i=1;i<n;i++){
             for(int j=0;j<n;j++){
-                if(v[i-1][j]==0){
-                    continue;
-                }
-                int c=4-v[i-1][j];
-                for(int k=0;k<5;k++){
+                int rotCnt=(4-v[i-1][j])%4;
+                cnt+=rotCnt;
+                v[i][j]=(v[i][j]+rotCnt)%4;
+                for(int k=0;k<4;k++){
                     int nx=i+dx[k];
                     int ny=j+dy[k];
-                    if(nx>=0&&nx<n&&ny>=0&&ny<n){
-                        v[nx][ny]=(v[nx][ny]+c)%4;
+                    if(range(nx,ny)){
+                        v[nx][ny]=(v[nx][ny]+rotCnt)%4;
                     }
                 }
-                total+=c;
             }
         }
-        bool flag=true;
         for(int j=0;j<n;j++){
             if(v[n-1][j]){
-                flag=false;
-                break;
+                return;
             }
         }
-        if(flag){
-            if(answer==-1||answer>total){
-                answer=total;
-            }
+        if(answer==-1||answer>cnt){
+            answer=cnt;
         }
         return;
     }
@@ -57,10 +54,9 @@ void go(int idx){
         go(idx+1);
     }
 }
-
 int solution(vector<vector<int>> clockHands) {
-    ::clockHands=clockHands;
     n=clockHands.size();
+    ::clockHands=clockHands;
     go(0);
     return answer;
 }
