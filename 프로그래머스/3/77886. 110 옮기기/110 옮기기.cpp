d76041f7f28d91f3abd4919eb5d3532fdf7afd3c
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-20 12:15:05 +0900

    [level 3] Title: 110 옮기기, Time: 12.31 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.cpp
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.cpp
@@ -5,60 +5,48 @@
 using namespace std;
 
 string go(string s){
-    if(s.length()<=3){
-        return s;
-    }
-    stack<char> q;
+    stack<char> st;
     int cnt=0;
     for(int i=0;i<s.length();i++){
-        q.push(s[i]);
-        if(q.size()>=3){
-            char a1=q.top();
-            q.pop();
-            char a2=q.top();
-            q.pop();
-            char a3=q.top();
-            q.pop();
-
-            if(a1=='0'&&a2=='1'&&a3=='1'){
+        st.push(s[i]);
+        if(st.size()>=3){
+            char c1=st.top();
+            st.pop();
+            char c2=st.top();
+            st.pop();
+            char c3=st.top();
+            st.pop();
+            
+            if(c1=='0'&&c2=='1'&&c3=='1'){
                 cnt++;
-            }else {
-                q.push(a3);
-                q.push(a2);
-                q.push(a1);
+                continue;
             }
+            st.push(c3);
+            st.push(c2);
+            st.push(c1);
         }
     }
     string res="";
-    while(!q.empty()){
-        res+=q.top();
-        q.pop();
+    while(!st.empty()){
+        res+=st.top();
+        st.pop();
     }
     reverse(res.begin(),res.end());
-    string p="";
+    int idx=0;
+    for(int i=0;i<(int)res.length();i++){
+        if(i<res.length()-2&&res[i]=='1'&&res[i+1]=='1'&&res[i+2]=='1'){
+            idx=i;
+            break;
+        }
+        if(res[i]=='0'){
+            idx=i+1;
+        }
+    }
+    string dup="";
     for(int i=0;i<cnt;i++){
-        p+="110";
+        dup+="110";
     }
-    if(res.empty()){
-        return p;
-    }
-    if(res.length()==1){
-        if(res[0]=='0'){
-            return res+p;
-        }else{
-            return p+res;
-        }
-    }else{
-        for(int i=0;i<res.length()-1;i++){
-            if(res.substr(i,2)=="11"){
-                return res.substr(0,i)+p+res.substr(i);
-            }
-        }
-        if(res[res.length()-1]=='1'){
-            return res.substr(0,res.length()-1)+p+res.substr(res.length()-1);
-        }
-        return res+p;
-    }
+    return res.substr(0,idx)+dup+res.substr(idx);
 }
 
 vector<string> solution(vector<string> s) {
