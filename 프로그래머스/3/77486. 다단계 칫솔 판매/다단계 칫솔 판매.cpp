0cfc42826645d84c4ddb32078a3e3fdf55216a88
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-20 10:22:15 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 41.37 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
@@ -2,27 +2,38 @@
 #include <vector>
 #include <map>
 using namespace std;
-map<string, string> tree;
-map<string, int> res;
-void get(string s, int amount) {
-    int div = amount / 10;
-    res[s] += amount - div;
-    if(div == 0) return;
-    get(tree[s], div);
-}
+
+map<string,int> idMap;
+map<int,int> totalSell;
+int p[10001];
+
 vector<int> solution(vector<string> enroll, vector<string> referral, vector<string> seller, vector<int> amount) {
-    vector<int> answer;
-    
+    vector<int> answer(enroll.size());
     for(int i=0;i<enroll.size();i++){
-        tree[enroll[i]] = referral[i];
+        idMap[enroll[i]]=i;
     }
-    
+    for(int i=0;i<enroll.size();i++){
+        if(referral[i]=="-"){
+            p[i]=-1;
+        }else{
+            p[i]=idMap[referral[i]];
+        }
+    }
     for(int i=0;i<seller.size();i++){
-        get(seller[i], amount[i] * 100);
+        totalSell[idMap[seller[i]]]+=amount[i]*100;
     }
-    
-    for(int i=0;i<enroll.size();i++){
-        answer.push_back(res[enroll[i]]);
+    for(int i=0;i<amount.size();i++){
+        int id=idMap[seller[i]];
+        int sellCount=amount[i]*100;
+        while(id!=-1){
+            int benefit=sellCount/10;
+            answer[id]+=sellCount-benefit;
+            id=p[id];
+            sellCount=benefit;
+            if(benefit==0){
+                break;
+            }
+        }
     }
     
     return answer;
