e5d1fe1746ca2e7b5365998b90b897b3988f804c
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-21 10:08:40 +0900

    [level 3] Title: 순위, Time: 0.94 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.cpp
+++ 프로그래머스/3/49191. 순위/순위.cpp
@@ -5,11 +5,9 @@ using namespace std;
 
 int map[101][101];
 int solution(int n, vector<vector<int>> results) {
-    int answer = 0;
     for(int i=0;i<results.size();i++){
         map[results[i][0]][results[i][1]]=1;
     }
-    
     for(int k=1;k<=n;k++){
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
@@ -19,17 +17,15 @@ int solution(int n, vector<vector<int>> results) {
             }
         }
     }
-    for(int k=1;k<=n;k++){
-        int r=0,c=0;
-        for(int i=1;i<=n;i++){
-            if(map[k][i]){
-                r++;
-            }
-            if(map[i][k]){
-                c++;
+    int answer=0;
+    for(int i=1;i<=n;i++){
+        int cnt=0;
+        for(int j=1;j<=n;j++){
+            if(map[i][j]||map[j][i]){
+                cnt++;
             }
         }
-        if(r+c==n-1){
+        if(cnt==n-1){
             answer++;
         }
     }
