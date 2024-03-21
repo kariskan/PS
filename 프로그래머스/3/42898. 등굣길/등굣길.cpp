755bac2cde59521619c70200a368e06788122bf2
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-21 16:40:53 +0900

    [level 3] Title: 등굣길, Time: 0.13 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -2,22 +2,23 @@
 #include <vector>
 
 using namespace std;
-int dp[101][101],map[101][101],mod=1000000007;
+int map[101][101],dp[101][101];
+int mod=1000000007;
 int solution(int m, int n, vector<vector<int>> puddles) {
     int answer = 0;
-    for(vector<int> p:puddles){
-        map[p[1]][p[0]]=1;
+    for(int i=0;i<puddles.size();i++){
+        map[puddles[i][1]][puddles[i][0]]=1;
     }
+    dp[1][1]=1;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
             if(i==1&&j==1){
-                dp[i][j]=1;
                 continue;
             }
             if(map[i][j]){
                 continue;
             }
-            dp[i][j]=(dp[i-1][j]+dp[i][j-1])%mod;
+            dp[i][j]=(dp[i-1][j]%mod+dp[i][j-1]%mod)%mod;
         }
     }
     return dp[n][m];
