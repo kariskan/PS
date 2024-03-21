e398ebcce570867c3f8132abd5414fe1d07b2ccd
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-21 16:26:53 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,51 +1,42 @@
 #include <string>
 #include <vector>
-#include <queue>
-#include <map>
+#include <algorithm>
 using namespace std;
-map<string,int> vis;
-map<string,vector<string>> v;
-bool valid(string s1, string s2){
-    for(int i=1;i<s1.length()-1;i++){
-        if(s1.substr(0,i)+s1.substr(i+1)==s2.substr(0,i)+s2.substr(i+1)){
-            return true;
+bool can(string s1,string s2){
+    int cnt=0;
+    for(int i=0;i<s1.length();i++){
+        if(s1[i]!=s2[i]){
+            cnt++;
         }
     }
-    if(s1.substr(1)==s2.substr(1)||s1.substr(0,s1.length()-1)==s2.substr(0,s2.length()-1)){
-        return true;
-    }
-    return false;
+    return cnt==1;
 }
+int map[51][51];
 int solution(string begin, string target, vector<string> words) {
-    int answer = 0;
     words.push_back(begin);
     for(int i=0;i<words.size();i++){
         for(int j=0;j<words.size();j++){
             if(i==j){
                 continue;
-            }
-            if(valid(words[i],words[j])){
-                v[words[i]].push_back(words[j]);
-                v[words[j]].push_back(words[i]);
+            }else{
+                if(can(words[i],words[j])){
+                    map[i][j]=1;
+                }
             }
         }
     }
-    queue<string> q;
-    q.push(begin);
-    vis[begin]=0;
-    while(!q.empty()){
-        string now=q.front();
-        q.pop();
-        
-        if(now==target){
-            return vis[now];
-        }
-        
-        for(auto&i:v[now]){
-            if(vis.find(i)==vis.end()){
-                vis[i]=vis[now]+1;
-                q.push(i);
+    for(int k=0;k<words.size();k++){
+        for(int i=0;i<words.size();i++){
+            for(int j=0;j<words.size();j++){
+                if(map[i][k]&&map[k][j]&&(map[i][j]==0||map[i][j]>map[i][k]+map[k][j])){
+                    map[i][j]=map[i][k]+map[k][j];
+                }
             }
+        }
+    }
+    for(int i=0;i<words.size();i++){
+        if(words[i]==target){
+            return map[words.size()-1][i];
         }
     }
     return 0;
