10f7d0bd3a0892e5918c60651ea5169e41c120b4
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-21 21:38:50 +0900

    [level 3] Title: 기둥과 보 설치, Time: 10.07 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.cpp
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.cpp
@@ -2,49 +2,53 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
-int map[102][102][2],n; // first=기둥,second=보
-
-bool valid(){
-    for(int i=0;i<=n;i++){
-        for(int j=0;j<=n;j++){
-            if(map[i][j][0]){ // 기둥
-                if(!(i==0||(j>0&&map[i][j-1][1])||map[i][j][1]||(i>0&&map[i-1][j][0]))){
-                    return false;
-                }
-            }
-            if(map[i][j][1]){
-                if(!(i>0&&(map[i-1][j][0]||(j<=n-1&&map[i-1][j+1][0])||(j>0&&j<=n-2&&map[i][j-1][1]&&map[i][j+1][1])))){
-                    return false;
-                }
-            }
-        }
-    }
-    return true;
-}
-
+//기둥일때 first, 보일때 second
+pair<int,int> map[102][102];
 vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
-    ::n=n;
     vector<vector<int>> answer;
-    
-    for(vector<int> build:build_frame){
-        int x=build[1];
-        int y=build[0];
-        int type=build[2];
-        int erase=build[3];
+    for(vector<int>& build:build_frame){
+        int x=build[1],y=build[0];
+        if(build[2]==0){
+            map[x][y].first=build[3];
+        }else{
+            map[x][y].second=build[3];
+        }
         
-        map[x][y][type]=erase;
-        if(!valid()){
-            map[x][y][type]=1-erase;
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=n;j++){
+                if(map[i][j].first){
+                    if(!(i==0
+                         ||map[i][j].second
+                         ||(j>0&&map[i][j-1].second)
+                         ||(i>0&&map[i-1][j].first))){
+                        if(build[2]==0){
+                            map[x][y].first=1-build[3];
+                        }else{
+                            map[x][y].second=1-build[3];
+                        }
+                    }
+                }
+                if(map[i][j].second){
+                    if(!((i>0&&map[i-1][j].first)
+                         ||(i>0&&map[i-1][j+1].first)
+                         ||(j>0&&map[i][j-1].second&&map[i][j+1].second))){
+                        if(build[2]==0){
+                            map[x][y].first=1-build[3];
+                        }else{
+                            map[x][y].second=1-build[3];
+                        }
+                    }
+                }
+            }
         }
     }
-    
     for(int i=0;i<=n;i++){
         for(int j=0;j<=n;j++){
-            for(int k=0;k<2;k++){
-                if(map[i][j][k]){
-                    answer.push_back({j,i,k});
-                }
+            if(map[i][j].first){
+                answer.push_back({j,i,0});
+            } 
+            if(map[i][j].second){
+                answer.push_back({j,i,1});
             }
         }
     }
