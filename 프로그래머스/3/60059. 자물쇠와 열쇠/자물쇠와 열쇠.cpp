e4142f569180701511e62749eb36b341553c2db2
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-21 19:27:27 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 0.04 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.cpp
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.cpp
@@ -2,44 +2,41 @@
 #include <vector>
 
 using namespace std;
-int map[40][40];
-vector<vector<int>> rotate(vector<vector<int>> key){
-    vector<vector<int>> res(key.size(),vector<int>(key.size()));
-    for(int i=0;i<key.size();i++){
-        for(int j=0;j<key.size();j++){
-            res[i][j]=key[j][key.size()-i-1];
+
+vector<vector<int>> rotate(vector<vector<int>> v){
+    vector<vector<int>> res=v;
+    for(int i=0;i<v.size();i++){
+        for(int j=0;j<v.size();j++){
+            res[i][j]=v[j][(int)v.size()-i-1];
         }
     }
     return res;
 }
+
+
 bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
     int n=key.size(),m=lock.size();
-    for(int i=n-1;i<n-1+m;i++){
-        for(int j=n-1;j<n-1+m;j++){
-            map[i][j]=lock[i-n+1][j-n+1];
-        }
-    }
-    for(int i=0;i<=n+m-2;i++){
-        for(int j=0;j<=n+m-2;j++){
-            for(int r=0;r<4;r++){
+    
+    for(int i=0;i<n+m;i++){
+        for(int j=0;j<n+m;j++){
+            for(int k=0;k<4;k++){
                 key=rotate(key);
-                bool flag=true;
-                for(int i1=n-1;i1<=n+m-2;i1++){
-                    for(int j1=n-1;j1<=n+m-2;j1++){
-                        if(i1>=max(i,n-1)&&i1<=min(n+m-2,i+n-1)&&j1>=max(j,n-1)&&j1<=min(n+m-2,j+n-1)){
-                            if((map[i1][j1]^key[i1-i][j1-j])==0){
-                                flag=false;
-                                break;
-                            }
-                        }else{
-                            if(map[i1][j1]==0){
-                                flag=false;
-                                break;
-                            }
+                vector<vector<int>> temp=lock;
+                for(int i1=0;i1<n;i1++){
+                    for(int j1=0;j1<n;j1++){
+                        int x=i+i1-n+1;
+                        int y=j+j1-n+1;
+                        if(x>=0&&x<m&&y>=0&&y<m){
+                            temp[x][y]^=key[i1][j1];
                         }
                     }
-                    if(!flag){
-                        break;
+                }
+                bool flag=true;
+                for(int i1=0;i1<m;i1++){
+                    for(int j1=0;j1<m;j1++){
+                        if(temp[i1][j1]==0){
+                            flag=false;
+                        }
                     }
                 }
                 if(flag){
