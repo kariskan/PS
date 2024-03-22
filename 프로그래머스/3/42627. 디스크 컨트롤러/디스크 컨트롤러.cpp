4cb730ea87e44423a8ffc73e8eba98fa147f8353
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-22 17:30:56 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -1,38 +1,25 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <queue>
 using namespace std;
-int vis[500];
+
 int solution(vector<vector<int>> jobs) {
     int answer = 0;
-    int cnt=jobs.size();
-    priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>> q;
-    for(int t=0;;t++){
-        for(int i=0;i<jobs.size();i++){
-            if(!vis[i]&&t>=jobs[i][0]){
-                vis[i]=1;
-                q.push({jobs[i][1],jobs[i][0]});
-                cnt--;
-            }
+    sort(jobs.begin(),jobs.end());
+    int t=0,idx=0;
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+    while(idx<jobs.size()||!pq.empty()){
+        if(idx<jobs.size()&&pq.empty()&&jobs[idx][0]>t){
+            t=jobs[idx][0];
         }
-        while(!q.empty()&&q.top().second<=t){
-            // printf("%d %d %d\n", t, q.top().first, q.top().second);
-            answer += t+q.top().first-q.top().second;
-            t+=q.top().first;
-            q.pop();
-            // printf("%d\n", answer);
-            
-        for(int i=0;i<jobs.size();i++){
-            if(!vis[i]&&t>=jobs[i][0]){
-                vis[i]=1;
-                q.push({jobs[i][1],jobs[i][0]});
-                cnt--;
-            }
+        while(idx<jobs.size()&&jobs[idx][0]<=t){
+            pq.push({jobs[idx][1],jobs[idx][0]});
+            idx++;
         }
-        }
-        if(!cnt&&q.empty()){
-            break;
-        }
+        answer+=t+pq.top().first-pq.top().second;
+        t+=pq.top().first;
+        pq.pop();
     }
     return answer/jobs.size();
 }
