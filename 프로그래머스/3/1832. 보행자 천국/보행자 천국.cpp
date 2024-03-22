f05d3255c438aa1a206e28116ef993a8065858fc
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-22 19:41:38 +0900

    [level 3] Title: 보행자 천국, Time: 39.28 ms, Memory: 7.45 MB -BaekjoonHub

--- 프로그래머스/3/1832. 보행자 천국/보행자 천국.cpp
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.cpp
@@ -1,38 +1,30 @@
 #include <vector>
-
+#include <iostream>
 using namespace std;
 
 int MOD = 20170805;
 
 int solution(int n, int m, vector<vector<int>> city_map) {
     int dp[501][501][2]={0,};
-    dp[1][1][0]=dp[1][1][1]=1;
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-            if(city_map[i-1][j-1]==1||(i==1&&j==1)){
+        if(city_map[i-1][0]==1){
+            break;
+        }
+        dp[i][1][1]=1;
+    }
+    for(int i=1;i<=m;i++){
+        if(city_map[0][i-1]==1){
+            break;
+        }
+        dp[1][i][0]=1;
+    }
+    for(int i=2;i<=n;i++){
+        for(int j=2;j<=m;j++){
+            if(city_map[i-1][j-1]==1){
                 continue;
             }
-            if(i==1){
-                dp[i][j][0]=dp[i][j-1][0];
-                continue;
-            }
-            if(j==1){
-                dp[i][j][1]=dp[i-1][j][1];
-                continue;
-            }
-            
-            if(city_map[i-1][j-2]==2){
-                dp[i][j][0]=dp[i][j-1][0];
-            }else{
-                dp[i][j][0]=dp[i][j-1][0]+dp[i][j-1][1];
-            }
-            if(city_map[i-2][j-1]==2){
-                dp[i][j][1]=dp[i-1][j][1];
-            }else{
-                dp[i][j][1]=dp[i-1][j][0]+dp[i-1][j][1];
-            }
-            dp[i][j][0]%=MOD;
-            dp[i][j][1]%=MOD;
+            dp[i][j][0]=(dp[i][j-1][0]+(city_map[i-1][j-2]==0?dp[i][j-1][1]:0))%MOD;
+            dp[i][j][1]=((city_map[i-2][j-1]==0?dp[i-1][j][0]:0)+dp[i-1][j][1])%MOD;
         }
     }
     return (dp[n][m][0]+dp[n][m][1])%MOD;
