df833fec68c869597a13bc5abc573d27a60be924
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-22 16:17:16 +0900

    [level 3] Title: 단속카메라, Time: 0.61 ms, Memory: 3.96 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -1,22 +1,21 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
-
-bool compare(vector<int>&v1,vector<int>&v2){
+bool compare(vector<int>& v1,vector<int>& v2){
     return v1[1]<v2[1];
 }
-
 int solution(vector<vector<int>> routes) {
     int answer = 0;
     sort(routes.begin(),routes.end(),compare);
+    int k=routes[0][1];
+    answer=1;
     for(int i=0;i<routes.size();i++){
-        answer++;
-        int idx=i+1;
-        while(idx<routes.size()&&routes[idx][0]<=routes[i][1]){
-            idx++;
+        if(k<routes[i][0]){
+            answer++;
+            k=routes[i][1];
         }
-        i=idx-1;
     }
     return answer;
 }
