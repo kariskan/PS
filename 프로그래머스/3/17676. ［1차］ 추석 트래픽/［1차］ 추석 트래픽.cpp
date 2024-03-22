61f496176a7399e7efb255db7241622869e7ca98
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-22 11:20:36 +0900

    [level 3] Title: [1차] 추석 트래픽, Time: 0.04 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.cpp
+++ 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.cpp
@@ -1,84 +1,38 @@
-// #include <string>
-// #include <vector>
-
-// using namespace std;
-
-// int sum[3600000*24+2];
-// int toTime(string s){
-//     s=s.substr(11);
-//     int hour=stoi(s.substr(0,2));
-//     int minute=stoi(s.substr(3,2));
-//     int second=stoi(s.substr(6,2));
-//     int milli=stoi(s.substr(9,3));
-//     return hour*3600000+minute*60000+second*1000+milli;
-// }
-// int getDuration(string s){
-//     return stod(s.substr(24, s.length()-25))*1000;
-// }
-// int solution(vector<string> lines) {
-//     int answer = 0;
-//     for(string line:lines){
-//         int end=toTime(line);
-//         int duration=getDuration(line);
-//         int start=max(0,end-duration+1);
-//         printf("%d %d %d\n", start, duration, end);
-//         sum[start]++;
-//         sum[end+1]--;
-//     }
-//     for(int i=1;i<=3600000*24;i++){
-//         sum[i]+=sum[i-1];
-//     }
-//     for(int i=1;i<=3600000*24;i++){
-//         sum[i]+=sum[i-1];
-//     }
-    
-//     answer=sum[0];
-//     printf("%d\n", sum[3605001]);
-//     for(int i=0;i<=3600000*24;i++){
-//         if(i<=1000){
-//             answer=max(answer,sum[i]);
-//         }else{
-//             answer=max(answer,sum[i]-sum[i-1001]);
-//         }
-//     }
-//     return answer;
-// }
-
 #include <string>
 #include <vector>
+#include <sstream>
 #include <queue>
 #include <algorithm>
 using namespace std;
-
-int toTime(string s){
-    s=s.substr(11);
-    int hour=stoi(s.substr(0,2));
-    int minute=stoi(s.substr(3,2));
-    int second=stoi(s.substr(6,2));
-    int milli=stoi(s.substr(9,3));
-    return hour*3600000+minute*60000+second*1000+milli;
-}
-int getDuration(string s){
-    return stod(s.substr(24, s.length()-25))*1000;
+pair<int,int> parse(string s){
+    stringstream ss(s);
+    vector<string> v;
+    string temp;
+    while(getline(ss,temp,' ')){
+        v.push_back(temp);
+    }
+    string start=v[1];
+    string duration=v[2];
+    duration.pop_back();
+    int end=stoi(start.substr(0,2))*3600000+stoi(start.substr(3,2))*60000
+            +stoi(start.substr(6,2))*1000+stoi(start.substr(9));
+    int st=end-stod(duration.substr(0))*1000+1;
+    return {st,end};
 }
 int solution(vector<string> lines) {
-    int answer = 0;
-    priority_queue<int> q;
+    int answer = 1;
     vector<pair<int,int>> v;
     for(string line:lines){
-        int end=toTime(line);
-        int duration=getDuration(line);
-        int start=max(0,end-duration+1);
-        v.push_back({start,end});
+        v.push_back(parse(line));
     }
     sort(v.begin(),v.end());
+    priority_queue<int> q;
     for(int i=0;i<v.size();i++){
         q.push(-v[i].second);
-        while(!q.empty()&&-q.top()+999<v[i].first){
+        while(!q.empty()&&-q.top()+1000<=v[i].first){
             q.pop();
         }
         answer=max(answer,(int)q.size());
     }
-    
     return answer;
 }
