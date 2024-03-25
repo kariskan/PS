bbda147caf4c2aab16cdc67136cd1bcbe8a4e13a
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-25 11:33:09 +0900

    [level 3] Title: 단속카메라, Time: 0.60 ms, Memory: 4.1 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -12,8 +12,10 @@ int solution(vector<vector<int>> routes) {
     int k=routes[0][1];
     answer=1;
     for(int i=0;i<routes.size();i++){
+        // 최근 카메라 설치 지점보다 진입 지점이 크면
         if(k<routes[i][0]){
             answer++;
+            // 나가는 지점에 카메라 설치
             k=routes[i][1];
         }
     }
