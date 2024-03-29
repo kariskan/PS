dc9d74fa5c9216bf9c0e9f32d1e2d6ab41fa33e8
kariskan <73534426+kariskan@users.noreply.github.com>
2024-03-29 10:33:48 +0900

    [level 3] Title: 금과 은 운반하기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.cpp
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.cpp
@@ -1,31 +1,34 @@
 #include <string>
 #include <vector>
-
+#include <climits>
 using namespace std;
 
 long long solution(int a, int b, vector<int> g, vector<int> s, vector<int> w, vector<int> t) {
-    long long answer = 0;
-    long long left=1,right=4*10e14;
+    long long answer = -1;
+    
+    long long left=0,right=1e14*5;
     while(left<=right){
         long long mid=(left+right)/2;
-        
-        long long total=0,totalG=0,totalS=0;
-        for(int i=0;i<g.size();i++){
-            long long cnt=mid/(2*t[i]);
-            if(mid-cnt*2*t[i]>=t[i]){
+        long long gSum=0,sSum=0,totalSum=0;
+        for(int i=0;i<s.size();i++){
+            long long cnt=mid/(t[i]*2);
+            if(mid-cnt*t[i]*2>=t[i]){
                 cnt++;
             }
-            
-            total+=min(cnt*w[i],(long long)g[i]+s[i]);
-            totalG+=min((long long)g[i],cnt*w[i]);
-            totalS+=min((long long)s[i],cnt*w[i]);
+            long long gMin=min((long long)g[i],cnt*w[i]);
+            long long sMin=min((long long)s[i],cnt*w[i]);
+            gSum+=gMin;
+            sSum+=sMin;
+            totalSum+=min(gMin+sMin,cnt*w[i]);
         }
-        if(total>=a+b&&totalG>=a&&totalS>=b){
-            right=mid-1;
+        
+        if(gSum>=a&&sSum>=b&&totalSum>=a+b){
             answer=mid;
+            right=mid-1;
         }else{
             left=mid+1;
         }
     }
+    
     return answer;
 }
