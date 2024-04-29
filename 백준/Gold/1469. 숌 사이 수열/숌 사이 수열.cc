14174e4f6b801143c796b90d362990d71ae73c38
kariskan <73534426+kariskan@users.noreply.github.com>
2024-04-29 10:32:01 +0900

    [Gold V] Title: 숌 사이 수열, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1469. 숌 사이 수열/숌 사이 수열.cc
+++ 백준/Gold/1469. 숌 사이 수열/숌 사이 수열.cc
@@ -1,54 +1,68 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
 #include <algorithm>
-using namespace std;
-int n, a[8];
-int cnt[17], v[17];
-void go(int idx, int c)
-{
-	if (v[idx] != -1)
-	{
-		go(idx + 1, c);
-		return;
-	}
-	if (c == 0)
-	{
-		for (int i = 0; i < n * 2; i++)
-		{
-			cout << v[i] << ' ';
-		}
-		exit(0);
-	}
-	for (int i = 0; i < n; i++)
-	{
-		// i번째 수가 한 번도 쓰이지 않았고
-		// 올바른 범위에 들어갈 수 있으며,
-		// 들어갈 두 곳이 모두 비어있을 때
-		if (cnt[a[i]] == 0 && idx + a[i] + 1 < n * 2 && v[idx] == -1 && v[idx + a[i] + 1] == -1)
-		{ 
-			cnt[a[i]] = 1;
-			v[idx] = v[idx + a[i] + 1] = a[i];
-			go(idx + 1, c - 1);
-			v[idx] = v[idx + a[i] + 1] = -1;
-			cnt[a[i]] = 0;
-		}
-		else
-		{
-			continue;
-		}
-	}
-}
+#include <string>
+#include <set>
+#include <map>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <stack>
+#include <queue>
 
-int main()
-{
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> a[i];
-	}
-	sort(a, a + n);
-	memset(v, -1, sizeof(v));
-	go(0, n);
-	cout << -1;
+using namespace std;
+
+int main() {
+    int n;
+    vector<int> v,seq;
+
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int a;cin>>a;
+        v.push_back(a);
+        seq.push_back(i);
+    }
+    vector<int> ans;
+    do{
+        vector<int> temp(n*2, 17);
+        int idx=0;
+        bool flag = true;
+        for(int s:seq){
+            while(idx<n*2&&temp[idx]!=17){
+                idx++;
+            }
+            temp[idx]=v[s];
+            if(temp[idx+v[s]+1]!=17){
+                flag = false;
+                break;
+            }
+            if(idx+v[s]+1>=2*n){
+                flag=false;
+                break;
+            }
+            temp[idx+v[s]+1]=v[s];
+        }
+        if(flag){
+            if(ans.empty()){
+                ans=temp;
+                continue;
+            }
+            for(int i=0;i<n*2;i++){
+                if(ans[i]<temp[i]){
+                    break;
+                }
+                if(ans[i]>temp[i]){
+                    ans=temp;
+                    break;
+                }
+            }
+        }
+    }while(next_permutation(seq.begin(), seq.end()));
+    if(ans.empty()){
+        cout<<-1;
+        return 0;
+    }
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<' ';
+    }
 }
