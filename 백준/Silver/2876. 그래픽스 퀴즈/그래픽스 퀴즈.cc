632a9556bd5a9c61c165b7ab2230244ec007b296
kariskan <73534426+kariskan@users.noreply.github.com>
2024-04-29 11:09:07 +0900

    [Silver II] Title: 그래픽스 퀴즈, Time: 36 ms, Memory: 3568 KB -BaekjoonHub

--- 백준/Silver/2876. 그래픽스 퀴즈/그래픽스 퀴즈.cc
+++ 백준/Silver/2876. 그래픽스 퀴즈/그래픽스 퀴즈.cc
@@ -1,34 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <map>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <stack>
+#include <queue>
 
 using namespace std;
 
-int n, a, b;
-int ar[100001][6];
-int ans, ansi;
-
-int main(void)
-{
-    cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> a >> b;
-        ar[i][a] = ar[i - 1][a] + 1;
-        if (a != b)
-        {
-            ar[i][b] = ar[i - 1][b] + 1;
-        }
+int main() {
+    int n;
+    vector<pair<int,int>> v;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int a,b;cin>>a>>b;
+        v.push_back({a,b});
     }
-
-    for (int i = 1; i <= 5; i++)
-    {
-        for (int j = 1; j <= n; j++)
-        {
-            if (ans < ar[j][i])
-            {
-                ans = ar[j][i];
-                ansi = i;
+    int ans=0,grade;
+    for(int i=1;i<=5;i++){
+        vector<int> prefix(n);
+        if(v[0].first==i||v[0].second==i){
+            prefix[0]=1;
+        }
+        for(int j=1;j<n;j++){
+            if(v[j].first==i||v[j].second==i){
+                prefix[j]=prefix[j-1]+1;
+            }
+        }
+        for(int j=0;j<n;j++){
+            if(ans<prefix[j]){
+                ans=prefix[j];
+                grade=i;
             }
         }
     }
-    cout << ans << " " << ansi << '\n';
+    cout<<ans<<' '<<grade;
 }
