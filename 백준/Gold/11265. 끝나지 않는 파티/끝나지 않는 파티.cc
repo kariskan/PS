3b65b8100ef546d5c99403ff1fa48ca77ae21ba6
kariskan <73534426+kariskan@users.noreply.github.com>
2024-05-02 12:08:28 +0900

    [Gold V] Title: 끝나지 않는 파티, Time: 48 ms, Memory: 3000 KB -BaekjoonHub

--- 백준/Gold/11265. 끝나지 않는 파티/끝나지 않는 파티.cc
+++ 백준/Gold/11265. 끝나지 않는 파티/끝나지 않는 파티.cc
@@ -4,6 +4,9 @@ using namespace std;
 int map[501][501];
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 	int n, m;
 	cin >> n >> m;
 	for (int i = 1; i <= n; i++) {
