52f032856cdedd23fa234c30447a17bdd50397fd
kariskan <73534426+kariskan@users.noreply.github.com>
2024-05-06 10:44:39 +0900

    [Gold III] Title: 소형기관차, Time: 12 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Gold/2616. 소형기관차/소형기관차.cc
+++ 백준/Gold/2616. 소형기관차/소형기관차.cc
@@ -1,23 +1,38 @@
 #include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-int n, m, a[50001], sum[50001], dp[4][50001];
-
+// j번 소형 기관차가 i번째에 있는 객차까지 관리할 때, 최대 승객 수
+int a[50001], sum[50001], dp[50001][3];
 int main() {
+	int n, m;
 	cin >> n;
-
 	for (int i = 1; i <= n; i++) {
 		cin >> a[i];
-		sum[i] = sum[i - 1] + a[i];
 	}
-
 	cin >> m;
 
-	for (int i = 1; i <= 3; i++) {
-		for (int j = i * m; j <= n - (3 - i) * m; j++) {
-			dp[i][j] = max(dp[i][j - 1], sum[j] - sum[j - m] + dp[i - 1][j - m]);
+	for (int i = 1; i <= n; i++) {
+		sum[i] = sum[i - 1] + a[i];
+		if (i >= m) {
+			dp[i][0] = sum[i] - sum[i - m];
 		}
+		dp[i][0] = max(dp[i][0], dp[i - 1][0]);
 	}
 
-	cout << dp[3][n];
+	for (int i = 2 * m; i <= n - m; i++) {
+		dp[i][1] = dp[i - m][0] + sum[i] - sum[i - m];
+		dp[i][1] = max(dp[i][1], dp[i - 1][1]);
+	}
+	for (int i = 3 * m; i <= n; i++) {
+		dp[i][2] = dp[i - m][1] + sum[i] - sum[i - m];
+		dp[i][2] = max(dp[i][2], dp[i - 1][2]);
+	}
+	cout << dp[n][2];
+	// 35 75 125 135 165 210 270
+
+	//    75 90  60  30  45  60
+	//    
 }
