3208e428d391475dc5deb3ad4be8a36dd8e9207b
kariskan <73534426+kariskan@users.noreply.github.com>
2024-05-07 10:18:43 +0900

    [Gold V] Title: 평범한 배낭, Time: 32 ms, Memory: 41476 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,26 +1,26 @@
 #include <iostream>
+#include <queue>
 #include <algorithm>
+#include <vector>
 using namespace std;
-int bag[101][100001] = { 0, };
-int weight[101] = { 0, };
-int value[101] = { 0, };
 
-int main(){
-    ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int n, k;
+int n, k, dp[101][100001];
+pair<int, int> a[101];
+int main() {
 	cin >> n >> k;
 	for (int i = 1; i <= n; i++) {
-		cin >> weight[i] >> value[i];
+		cin >> a[i].first >> a[i].second;
 	}
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=k;j++){
-            bag[i][j] = bag[i - 1][j];
-            if(j >= weight[i]) {
-                bag[i][j] = max(bag[i][j], bag[i - 1][j - weight[i]] + value[i]);
-            }
-        }
-    }
-    
-    cout<<bag[n][k];
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= k; j++) {
+			if (j < a[i].first) {
+				dp[i][j] = dp[i - 1][j];
+				continue;
+			}
+			dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - a[i].first] + a[i].second);
+		}
+	}
+
+	cout << dp[n][k];
 }
