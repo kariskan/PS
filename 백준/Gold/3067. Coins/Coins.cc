f2f569ce8f0cfb03f07c41a6d7e2effa6abda3e9
kariskan <73534426+kariskan@users.noreply.github.com>
2024-05-08 11:00:05 +0900

    [Gold V] Title: Coins, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/3067. Coins/Coins.cc
+++ 백준/Gold/3067. Coins/Coins.cc
@@ -1,31 +1,31 @@
 #include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <climits>
+#include <cstring>
 using namespace std;
 
-int main()
-{
+int main() {
 	int t;
 	cin >> t;
-	while (t--)
-	{
-		int n;
+	while (t--) {
+		int n, m;
 		cin >> n;
-		int money[21] = {
-			0,
-		};
-		for (int i = 1; i <= n; i++)
-		{
-			cin >> money[i];
+		vector<int> money;
+		for (int i = 0; i < n; i++) {
+			int a; 
+			cin >> a;
+			money.push_back(a);
 		}
-		int m;
 		cin >> m;
 
-		int dp[10001] = {
-			1,
-		};
-		for (int i = 1; i <= n; i++)
-		{
-			for (int j = money[i]; j <= m; j++)
-			{
+		// 1 1 1 1 1 1 1 1
+		// 1 1 2 2 3 3
+		int dp[10001] = { 1, };
+		for (int i = 0; i < n; i++) {
+			for (int j = money[i]; j <= m; j++) {
 				dp[j] += dp[j - money[i]];
 			}
 		}
