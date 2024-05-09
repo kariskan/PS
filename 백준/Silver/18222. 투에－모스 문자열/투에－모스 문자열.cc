a6c9d234cc9425c8d7266a7d720e0268b7912dc8
kariskan <73534426+kariskan@users.noreply.github.com>
2024-05-09 10:11:48 +0900

    [Silver II] Title: 투에-모스 문자열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/18222. 투에－모스 문자열/투에－모스 문자열.cc
+++ 백준/Silver/18222. 투에－모스 문자열/투에－모스 문자열.cc
@@ -1,30 +1,32 @@
 #include <iostream>
-#include <cmath>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <climits>
+#include <cstring>
+#include <map>
 using namespace std;
 
-long long k;
-
-void go(long long n, int cnt) {
-	if (n == 1) {
-		if (cnt % 2 == 0)cout << 0;
-		else cout << 1;
-		return;
-	}
-	else if (n == 2) {
-		if (cnt % 2 == 0)cout << 1;
-		else cout << 0;
-		return;
-	}
-	for (long long i = 62; i >= 1; i--) {
-		if ((long long)pow(2, i) < n) {
-			go(n - (long long)pow(2, i), 1 - cnt);
-			break;
-		}
-	}
-}
-
 int main() {
+	long long k;
 	cin >> k;
 
-	go(k, 0);
+	int cnt = 0;
+	while (k > 1) {
+		for (long long i = 1;; i *= 2) {
+			if (k - i <= 0) {
+				k = k - i / 2;
+				cnt++;
+				break;
+			}
+		}
+	}
+
+	if (cnt % 2 == 0) {
+		cout << 0;
+	}
+	else {
+		cout << 1;
+	}
 }
