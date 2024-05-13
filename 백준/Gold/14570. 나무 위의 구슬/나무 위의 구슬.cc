f5ed9c33d2cfa54328c9ea57cfb7dee50c091e2d
kariskan <73534426+kariskan@users.noreply.github.com>
2024-05-13 11:16:45 +0900

    [Gold I] Title: 나무 위의 구슬, Time: 140 ms, Memory: 4364 KB -BaekjoonHub

--- 백준/Gold/14570. 나무 위의 구슬/나무 위의 구슬.cc
+++ 백준/Gold/14570. 나무 위의 구슬/나무 위의 구슬.cc
@@ -1,55 +1,53 @@
 #include <iostream>
+#include <queue>
+#include <algorithm>
 #include <vector>
+#include <stack>
+#include <climits>
+#include <cstring>
+#include <map>
 using namespace std;
 
-int n;
-pair<int, int> a[200001];
-int leaf[200001];
+int n, p[200001];
+pair<int, int> child[200001];
 
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+void traversal(int node, long long k) {
+	if (child[node].first == -1 && child[node].second == -1) {
+		cout << node;
+		exit(0);
+	}
+	if (child[node].first == -1) {
+		traversal(child[node].second, k);
+	}
+	else if (child[node].second == -1) {
+		traversal(child[node].first, k);
+	}
+	else {
+		if (k % 2 == 1) {
+			traversal(child[node].first, k - k / 2);
+		}
+		else {
+			traversal(child[node].second, k - k / 2);
+		}
+	}
+}
+
+int main() {
 
 	cin >> n;
-
-	for (int i = 1; i <= n; i++)
-	{
+	for (int i = 0; i < n; i++) {
 		int u, v;
 		cin >> u >> v;
-
-		a[i].first = u;
-		a[i].second = v;
-
-		if (u == -1 && v == -1)
-		{
-			leaf[i] = 1;
+		if (u != -1) {
+			p[u] = i + 1;
 		}
+		if (v != -1) {
+			p[v] = i + 1;
+		}
+		child[i + 1] = { u,v };
 	}
 
 	long long k;
 	cin >> k;
-	int now = 1;
-
-	while (k)
-	{
-		if (leaf[now])
-		{
-			break;
-		}
-		else if (a[now].first == -1) now = a[now].second;
-		else if (a[now].second == -1) now = a[now].first;
-		else if (k % 2 == 0)
-		{
-			k /= 2;
-			now = a[now].second;
-		}
-		else
-		{
-			k = k / 2 + 1;
-			now = a[now].first;
-		}
-	}
-	
-	cout << now;
+	traversal(1, k);
 }
