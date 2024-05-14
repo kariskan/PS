0840bd4b19674f7d21d49d58390903dc026b6135
kariskan <73534426+kariskan@users.noreply.github.com>
2024-05-14 10:49:17 +0900

    [Gold III] Title: 나머지 합, Time: 576 ms, Memory: 9840 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.cc
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -1,24 +1,29 @@
 #include <iostream>
+#include <queue>
+#include <algorithm>
 #include <vector>
+#include <stack>
+#include <climits>
+#include <cstring>
+#include <map>
 using namespace std;
 
-int n, m;
-long long sum[1000001], cnt[1000001] = { 1, };
+long long arr[1000001], mod[1001];
 int main() {
+	int n, m;
 	cin >> n >> m;
-	vector<vector<long long>> v(m);
-	v[0].push_back(0);
+
+	long long answer = 0;
+
 	for (int i = 1; i <= n; i++) {
-		int a; cin >> a;
-		sum[i] = sum[i - 1] + a;
-		v[sum[i] % m].push_back(sum[i]);
-		cnt[i] = 1;
+		int a;
+		cin >> a;
+		arr[i] = arr[i - 1] + a;
+		if (arr[i] % m == 0) {
+			answer++;
+		}
+		answer += mod[arr[i] % m];
+		mod[arr[i] % m]++;
 	}
-	long long ans = 0;
-	for (int i = 1; i <= n; i++) {
-		if (sum[i] < m && sum[i] != 0)continue;
-		if (v[sum[i] % m].empty())continue;
-		ans += v[sum[i] % m].size() - (cnt[sum[i] % m]++);
-	}
-	cout << ans;
+	cout << answer << ' ';
 }
