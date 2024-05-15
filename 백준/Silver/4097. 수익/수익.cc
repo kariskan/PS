ea522e79023af96c9ebb7f9d700377a4d0778f75
kariskan <73534426+kariskan@users.noreply.github.com>
2024-05-15 10:31:11 +0900

    [Silver II] Title: 수익, Time: 96 ms, Memory: 3848 KB -BaekjoonHub

--- 백준/Silver/4097. 수익/수익.cc
+++ 백준/Silver/4097. 수익/수익.cc
@@ -1,36 +1,35 @@
-#include <climits>
 #include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <climits>
+#include <cstring>
+#include <map>
 using namespace std;
 
-int main()
-{
+int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	int n;
-	while (cin >> n, n != 0)
-	{
-		int arr[250001] = {
-			0,
-		};
-		for (int i = 1; i <= n; i++)
-		{
+	cout.tie(0);
+
+	while (1) {
+		int n;
+		cin >> n;
+		if (n == 0) {
+			break;
+		}
+
+		long long arr[250001] = { 0, };
+		// -3 4 9 -2 -5 8
+		// -3 1 6  4 -1 7
+		long long mi = 0, answer = LLONG_MIN;
+		for (int i = 1; i <= n; i++) {
 			cin >> arr[i];
 			arr[i] += arr[i - 1];
+			answer = max(answer, arr[i] - mi);
+			mi = min(mi, arr[i]);
 		}
-
-		int minValue = 0;
-		int ans = INT_MIN;
-
-		for (int i = 1; i <= n; i++)
-		{	
-			ans = max(ans, arr[i] - minValue);
-			
-			if (minValue > arr[i])
-			{
-				minValue = arr[i];
-			}
-		}
-		
-		cout << ans << '\n';
+		cout << answer << '\n';
 	}
 }
