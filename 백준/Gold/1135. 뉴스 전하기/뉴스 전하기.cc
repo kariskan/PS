3284843e3bc2b6549fcf423207d89ffd4fb69fe5
kariskan <73534426+kariskan@users.noreply.github.com>
2024-05-20 10:45:26 +0900

    [Gold II] Title: 뉴스 전하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.cc
+++ 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.cc
@@ -1,36 +1,47 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
+#include <set>
+#include <map>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <stack>
+#include <queue>
+
 using namespace std;
 
 int n;
-vector<vector<int>> tree;
+vector<vector<int>> v;
 
-int dfs(int root) {
-
-    int ret = 0;
-    vector<int> v;
-
-    for (int i = 0; i < tree[root].size(); i++) {
-        v.push_back(dfs(tree[root][i]));
+int go(int node) {
+    if (v[node].empty()) {
+        return 0;
     }
-
-    sort(v.begin(), v.end());
-    int subSize = v.size();
-
-    for (int i = 0; i < v.size(); i++) {
-        ret = max(ret, v[i] + subSize - i);
+    vector<int> child;
+    for (int i = 0; i < v[node].size(); i++) {
+        child.push_back(go(v[node][i]));
     }
-    return ret;
+    sort(child.begin(), child.end());
+    int res = 0;
+    for (int i = 0; i < child.size(); i++) {
+        res = max(res, child[i] + (int) child.size() - i);
+    }
+    return res;
 }
 
 int main() {
     cin >> n;
-    tree.resize(n);
+    v.resize(n);
     for (int i = 0; i < n; i++) {
-        int a; cin >> a;
-        if (!i)continue;
-        tree[a].push_back(i);
+        int a;
+        cin >> a;
+        if (i == 0) {
+            continue;
+        }
+        v[a].push_back(i);
     }
-    cout << dfs(0);
+
+    cout << go(0);
 }
