0db6aab6623803ee428296b804a40396e9e12e4a
kariskan <73534426+kariskan@users.noreply.github.com>
2024-05-20 10:55:13 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -1,30 +1,48 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-using namespace std;
-int n, m, a[8];
-void go(vector<int> v, int idx) {
-	if (v.size() == m) {
-		for (auto& i : v)cout << i << ' ';
-		cout << '\n';
-		return;
-	}
+#include <algorithm>
+#include <string>
+#include <set>
+#include <map>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <stack>
+#include <queue>
 
-	for (int i = idx; i < n; i++) {
-		v.push_back(a[i]);
-		go(v, i + 1);
-		v.pop_back();
-	}
-	
+using namespace std;
+
+int n, m;
+vector<int> num;
+vector<vector<int>> answer;
+
+void go(int idx, vector<int> seq) {
+    if (seq.size() == m) {
+        answer.push_back(seq);
+        return;
+    }
+    if (idx >= num.size()) {
+        return;
+    }
+    go(idx + 1, seq);
+    seq.push_back(num[idx]);
+    go(idx + 1, seq);
 }
 
 int main() {
-	cin >> n >> m;
-
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-	}
-
-	sort(a, a + n);
-	go({}, 0);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        num.push_back(a);
+    }
+    sort(num.begin(), num.end());
+    go(0, {});
+    sort(answer.begin(), answer.end());
+    for (auto &i: answer) {
+        for (auto &j: i) {
+            cout << j << ' ';
+        }
+        cout << '\n';
+    }
 }
