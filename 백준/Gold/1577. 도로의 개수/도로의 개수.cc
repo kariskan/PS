ce8b50577982a767889a7eb49328925bdb67f9f8
kariskan <73534426+kariskan@users.noreply.github.com>
2024-05-22 10:49:27 +0900

    [Gold V] Title: 도로의 개수, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Gold/1577. 도로의 개수/도로의 개수.cc
+++ 백준/Gold/1577. 도로의 개수/도로의 개수.cc
@@ -1,55 +1,57 @@
 #include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <climits>
+#include <cstring>
+#include <map>
 #include <set>
+#include <cmath>
+#include <string>
 using namespace std;
 
 long long dp[101][101];
-int n, m, k;
-set<pair<pair<int, int>, pair<int, int>>> se;
-int main()
-{
-	cin >> m >> n >> k;
-	for (int i = 0; i < k; i++)
-	{
+set <pair<pair<int, int>, pair<int, int>>> se;
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, m, k;
+	cin >> n >> m >> k;
+
+	for (int i = 0; i < k; i++) {
 		int a, b, c, d;
 		cin >> a >> b >> c >> d;
-		swap(a, b);
-		swap(c, d);
-		se.insert({{a, b}, {c, d}});
-		se.insert({{c, d}, {a, b}});
+		se.insert({ {min(a,c),min(b,d)},{max(a,c),max(b,d)} });
 	}
+
+	for (int j = 1; j <= m; j++) {
+		if (se.find({ {0,j - 1},{0,j} }) == se.end()) {
+			dp[0][j] = 1;
+		}
+		else {
+			break;
+		}
+	}
+	for (int i = 1; i <= n; i++) {
+		if (se.find({ {i - 1,0},{i,0} }) == se.end()) {
+			dp[i][0] = 1;
+		}
+		else {
+			break;
+		}
+	}
+
 	dp[0][0] = 1;
-	for (int i = 0; i <= n; i++)
-	{
-		for (int j = 0; j <= m; j++)
-		{
-			if (i == 0 && j == 0)
-			{
-				continue;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (se.find({ {i - 1,j},{i,j} }) == se.end()) {
+				dp[i][j] += dp[i - 1][j];
 			}
-			if (i == 0)
-			{
-				if (!se.count({{i, j-1}, {i, j}}))
-				{
-					dp[i][j] = dp[i][j-1];
-				}
-			}
-			else if (j == 0)
-			{
-				if (!se.count({{i-1, j}, {i, j}}))
-				{
-					dp[i][j] = dp[i-1][j];
-				}
-			}
-			else
-			{
-				if (!se.count({{i, j - 1}, {i, j}}))
-				{
-					dp[i][j] += dp[i][j - 1];
-				}
-				if (!se.count({{i - 1, j}, {i, j}}))
-				{
-					dp[i][j] += dp[i - 1][j];
-				}
+			if (se.find({ {i,j - 1},{i,j} }) == se.end()) {
+				dp[i][j] += dp[i][j - 1];
 			}
 		}
 	}
