432b8cf9b60ab395cea8d13e1e4cb176121601cd
kariskan <73534426+kariskan@users.noreply.github.com>
2024-05-23 11:42:00 +0900

    [Gold V] Title: 배열 돌리기 1, Time: 120 ms, Memory: 2372 KB -BaekjoonHub

--- 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.cc
+++ 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.cc
@@ -39,7 +39,9 @@ void rotate() {
 }
 
 int main() {
-
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     cin >> n >> m >> k;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
