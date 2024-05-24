2ac51fb08823c887996dcf885715c84a3b7bb145
kariskan <73534426+kariskan@users.noreply.github.com>
2024-05-24 10:31:35 +0900

    [Gold V] Title: 1학년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/5557. 1학년/1학년.cc
+++ 백준/Gold/5557. 1학년/1학년.cc
@@ -1,24 +1,44 @@
 #include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <climits>
+#include <cstring>
+#include <map>
+#include <set>
+#include <cmath>
+#include <string>
 using namespace std;
-long long a[101], dp[101][21];
+
 int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	// -97 -6 -2 6 98
+	// -24 -6 -3 -2 61 98 100
+
+	long long a[100], dp[100][21] = { 0, };
 	int n;
+
 	cin >> n;
-	for (int i = 1; i <= n; i++) {
+	for (int i = 0; i < n; i++) {
 		cin >> a[i];
 	}
-	dp[1][a[1]] = 1;
-	for (int i = 2; i < n; i++) {
-		for (int j = 0; j < 21; j++) {
-			if (dp[i - 1][j]) {
-				if (j + a[i] <= 20) {
-					dp[i][j + a[i]] += dp[i - 1][j];
-				}
-				if (j - a[i] >= 0) {
-					dp[i][j - a[i]] += dp[i - 1][j];
-				}
+
+	dp[0][a[0]] = 1;
+
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j <= 20; j++) {
+			if (j + a[i] >= 0 && j + a[i] <= 20) {
+				dp[i][j + a[i]] += dp[i - 1][j];
+			}
+			if (j - a[i] >= 0 && j - a[i] <= 20) {
+				dp[i][j - a[i]] += dp[i - 1][j];
 			}
 		}
 	}
-	cout << dp[n - 1][a[n]];
+
+	cout << dp[n - 2][a[n - 1]];
 }
