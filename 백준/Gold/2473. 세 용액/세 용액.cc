038e38d918ccbbcdc81d43965ada216d78e30ab5
kariskan <73534426+kariskan@users.noreply.github.com>
2024-05-24 10:59:07 +0900

    [Gold III] Title: 세 용액, Time: 660 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.cc
+++ 백준/Gold/2473. 세 용액/세 용액.cc
@@ -1,54 +1,67 @@
 #include <iostream>
+#include <queue>
 #include <algorithm>
+#include <vector>
+#include <stack>
 #include <climits>
+#include <cstring>
+#include <map>
+#include <set>
+#include <cmath>
+#include <string>
 using namespace std;
 
-long long n, a[5000], an[3];
+long long n, a[5000];
+
+// -97 -6 -2 6 98
+// -24 -6 -3 -2 61 98 100
+long long get(int l, int r, long long s) {
+	if (r < 0 || r >= n || l < 0 || l >= n) {
+		return (long long)2000000000 * 3;
+	}
+
+	long long mi = LLONG_MAX, k = LLONG_MAX;
+	while (l <= r) {
+		int mid = (l + r) / 2;
+
+		if (mi > abs(s + a[mid])) {
+			mi = abs(s + a[mid]);
+			k = a[mid];
+		}
+		if (s + a[mid] < 0) {
+			l = mid + 1;
+		}
+		else {
+			r = mid - 1;
+		}
+	}
+	return k;
+}
 
 int main() {
-
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 
 	cin >> n;
-
 	for (int i = 0; i < n; i++) {
 		cin >> a[i];
 	}
-	
+
 	sort(a, a + n);
 
-	long long ans = LLONG_MAX;
-
-	for (int i = 0; i < n - 2; i++) {
-		for (int j = i + 1; j < n - 1; j++) {
-
-			int left = j + 1;
-			int right = n - 1;
-			int mid = (left + right) / 2;
-
-			while (left <= right) {
-				mid = (left + right) / 2;
-				if (a[i] + a[j] + a[mid] > 0) {
-					right = mid - 1;
-				}
-				else {
-					left = mid + 1;
-				}
-
-				if (ans > abs(a[i] + a[j] + a[mid])) {
-					ans = abs(a[i] + a[j] + a[mid]);
-					an[0] = a[i];
-					an[1] = a[j];
-					an[2] = a[mid];
-				}
+	vector<long long> ans = { 1000000000, 1000000000, 1000000000 };
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			long long l = get(0, i - 1, a[i] + a[j]);
+			if (abs(ans[0] + ans[1] + ans[2]) >= abs(a[i] + a[j] + l)) {
+				ans = { l,a[i],a[j] };
+			}
+			long long r = get(j + 1, n - 1, a[i] + a[j]);
+			if (abs(ans[0] + ans[1] + ans[2]) >= abs(a[i] + a[j] + r)) {
+				ans = { a[i],a[j],r };
 			}
 		}
-		
 	}
-
-	sort(an, an + 3);
-
-	cout << an[0] << ' ' << an[1] << ' ' << an[2];
+	cout << ans[0] << ' ' << ans[1] << ' ' << ans[2];
 }
