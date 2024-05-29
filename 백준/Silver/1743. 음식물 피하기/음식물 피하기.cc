8f49408aea766dd63bd0a267f3524e558af36526
kariskan <73534426+kariskan@users.noreply.github.com>
2024-05-29 10:06:07 +0900

    [Silver I] Title: 음식물 피하기, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.cc
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.cc
@@ -1,43 +1,62 @@
 #include <iostream>
 #include <queue>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <climits>
+#include <cstring>
+#include <set>
+#include <cmath>
+#include <string>
+
 using namespace std;
-int map[101][101];
-int visit[101][101];
+
+int board[100][100], vis[100][100];
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
+
 int main() {
-	int n, m, d;
-	cin >> n >> m >> d;
-	for (int i = 0; i < d; i++) {
-		int a, b;
-		cin >> a >> b;
-		map[a][b] = 1;
-	}
-	int x[4] = { 0,0,1,-1 };
-	int y[4] = { 1,-1,0,0 };
-	int ans = 0;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= m; j++) {
-			if (map[i][j] && !visit[i][j]) {
-				visit[i][j] = 1;
-				queue<pair<int, int>> q;
-				q.push({ i,j });
-				int cnt = 0;
-				while (!q.empty()) {
-					int a = q.front().first;
-					int b = q.front().second;
-					q.pop();
-					cnt++;
-					for (int k = 0; k < 4; k++) {
-						int nx = a + x[k];
-						int ny = b + y[k];
-						if (nx > 0 && nx <= n && ny > 0 && ny <= m && !visit[nx][ny] && map[nx][ny] == 1) {
-							visit[nx][ny] = 1;
-							q.push({ nx,ny });
-						}
-					}
-				}
-				ans = max(ans, cnt);
-			}
-		}
-	}
-	cout << ans;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m, k;
+    cin >> n >> m >> k;
+    for (int i = 0; i < k; i++) {
+        int a, b;
+        cin >> a >> b;
+        board[a - 1][b - 1] = 1;
+    }
+
+    int answer = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (board[i][j] == 1 && !vis[i][j]) {
+                int cnt = 0;
+                queue<pair<int, int>> q;
+
+                q.push({i, j});
+                vis[i][j] = 1;
+
+                while (!q.empty()) {
+                    int x = q.front().first;
+                    int y = q.front().second;
+                    q.pop();
+
+                    cnt++;
+
+                    for (int l = 0; l < 4; l++) {
+                        int nx = x + dx[l];
+                        int ny = y + dy[l];
+                        if (nx >= 0 && nx < n && ny >= 0 && ny < m && !vis[nx][ny] && board[nx][ny] == 1) {
+                            vis[nx][ny] = 1;
+                            q.push({nx, ny});
+                        }
+                    }
+                }
+                answer = max(answer, cnt);
+            }
+        }
+    }
+    cout << answer;
 }
