8c450f6d2853fcd58467bdc3f336ee8e7f4e6914
kariskan <73534426+kariskan@users.noreply.github.com>
2024-05-29 10:20:12 +0900

    [Gold V] Title: 갤러리, Time: 28 ms, Memory: 18624 KB -BaekjoonHub

--- 백준/Gold/2115. 갤러리/갤러리.cc
+++ 백준/Gold/2115. 갤러리/갤러리.cc
@@ -1,64 +1,56 @@
 #include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <climits>
+#include <cstring>
+#include <set>
+#include <cmath>
+#include <string>
+
 using namespace std;
 
-int n, m, check[1000][1000];
-char a[1000][1000];
-int ans;
+char board[1000][1000];
+int vis[1000][1000][4];
 
-// (x1,y1): 현재 있는 곳, (x2,y2): 다음 있는 곳, (cx,cy): 다음 벽 체크할 곳
-bool can(int x1, int y1, int x2, int y2, int cx, int cy)
-{
-	return a[x1][y1] == '.' && a[x2][y2] == '.' && a[x1 + cx][y1 + cy] == 'X' && a[x2 + cx][y2 + cy] == 'X';
-}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-//
-void go(int dirx, int diry, int cx, int cy)
-{
-	for (int i = 1; i < n - 1; i++) // 맨 위, 맨 아래 행 제외
-	{
-		for (int j = 1; j < m - 1; j++) // 맨 왼쪽, 맨 아래쪽 열 제회
-		{
-			if (can(i, j, i + dirx, j + diry, cx, cy)) // 지금 있는 곳이 빈 공간이고, 다음 있는 곳이 벽이면
-			{
-				i += dirx;
-				j += diry;
-				ans++;
-			}
-		}
-	}
-}
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> board[i][j];
+        }
+    }
 
-void go2(int dirx, int diry, int cx, int cy)
-{
-	for (int j = 1; j < m - 1; j++) // 맨 위, 맨 아래 행 제외
-	{
-		for (int i = 1; i < n - 1; i++) // 맨 왼쪽, 맨 아래쪽 열 제회
-		{
-			if (can(i, j, i + dirx, j + diry, cx, cy)) // 지금 있는 곳이 빈 공간이고, 다음 있는 곳이 벽이면
-			{
-				i += dirx;
-				j += diry;
-				ans++;
-			}
-		}
-	}
-}
-
-int main()
-{
-	cin >> n >> m;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			cin >> a[i][j];
-		}
-	}
-
-	go(0, 1, -1, 0);
-	go(0, 1, 1, 0);
-	go2(1, 0, 0, -1);
-	go2(1, 0, 0, 1);
-
-	cout << ans;
+    int answer = 0;
+    for (int i = 1; i < n - 1; i++) {
+        for (int j = 1; j < m - 1; j++) {
+            if (board[i][j] == '.' && board[i][j + 1] == '.' && !vis[i][j][0] && !vis[i][j + 1][0] &&
+                board[i - 1][j] == 'X' && board[i - 1][j + 1] == 'X') {
+                answer++;
+                vis[i][j][0] = vis[i][j + 1][0] = 1;
+            }
+            if (board[i][j] == '.' && board[i][j + 1] == '.' && !vis[i][j][2] && !vis[i][j + 1][2] &&
+                board[i + 1][j] == 'X' && board[i + 1][j + 1] == 'X') {
+                answer++;
+                vis[i][j][2] = vis[i][j + 1][2] = 1;
+            }
+            if (board[i][j] == '.' && board[i + 1][j] == '.' && !vis[i][j][3] && !vis[i + 1][j][3] &&
+                board[i][j - 1] == 'X' && board[i + 1][j - 1] == 'X') {
+                answer++;
+                vis[i][j][3] = vis[i + 1][j][3] = 1;
+            }
+            if (board[i][j] == '.' && board[i + 1][j] == '.' && !vis[i][j][1] && !vis[i + 1][j][1] &&
+                board[i][j + 1] == 'X' && board[i + 1][j + 1] == 'X') {
+                answer++;
+                vis[i][j][1] = vis[i + 1][j][1] = 1;
+            }
+        }
+    }
+    cout << answer;
 }
