b34d0f363377ae83473cccb9d45abd2321d5c802
kariskan <73534426+kariskan@users.noreply.github.com>
2024-05-30 11:03:37 +0900

    [Gold IV] Title: 우유 도시, Time: 76 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Gold/14722. 우유 도시/우유 도시.cc
+++ 백준/Gold/14722. 우유 도시/우유 도시.cc
@@ -1,50 +1,55 @@
 #include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <climits>
+#include <cstring>
+#include <set>
+#include <cmath>
+#include <string>
+
 using namespace std;
 
-int n, map[1001][1001], dp[1001][1001][3];
+int board[1001][1001], dp[1001][1001][3];
 
-int getM(int i, int j)
-{
-	return max(dp[i][j][0], max(dp[i][j][1], dp[i][j][2]));
-}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-int main()
-{
-	cin >> n;
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = 1; j <= n; j++)
-		{
-			cin >> map[i][j];
-		}
-	}
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> board[i][j];
+            if (board[i][j] == 0) {
+                dp[i][j][0] = 1;
+            }
+        }
+    }
 
-	for (int i = 0; i <= n; i++)
-	{
-		for (int j = 0; j <= n; j++)
-		{
-			dp[i][j][0] = dp[i][j][1] = dp[i][j][2] = -1000000000;
-		}
-	}
-
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = 1; j <= n; j++)
-		{
-			if (map[i][j] == 0)
-			{
-				dp[i][j][0] = 1;
-			}
-			for (int k = 0; k <= 2; k++)
-			{
-				dp[i][j][k] = max(dp[i][j][k], max(dp[i - 1][j][k], dp[i][j - 1][k]));
-				if (map[i][j] == k)
-				{
-					dp[i][j][k] = max(dp[i][j][k], max(dp[i - 1][j][(k - 1 + 3) % 3], dp[i][j - 1][(k - 1 + 3) % 3]) + 1);
-				}
-			}
-		}
-	}
-
-	cout << (getM(n, n) < 0 ? 0 : getM(n, n));
+    if (board[1][1] == 0) {
+        dp[1][1][board[1][1]] = 1;
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i == 1 && j == 1) {
+                continue;
+            }
+            for (int k = 0; k < 3; k++) {
+                if (board[i][j] != k) {
+                    dp[i][j][k] = max(dp[i - 1][j][k], dp[i][j - 1][k]);
+                    continue;
+                }
+                if (dp[i - 1][j][(k + 2) % 3]) {
+                    dp[i][j][k] = max(dp[i][j][k], dp[i - 1][j][(k + 2) % 3] + 1);
+                }
+                if (dp[i][j - 1][(k + 2) % 3]) {
+                    dp[i][j][k] = max(dp[i][j][k], dp[i][j - 1][(k + 2) % 3] + 1);
+                }
+            }
+        }
+    }
+    cout << max(dp[n][n][0], max(dp[n][n][1], dp[n][n][2]));
 }
