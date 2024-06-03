6388074c8d504a96db6b523f2617f1bc691b82ba
kariskan <73534426+kariskan@users.noreply.github.com>
2024-06-03 10:04:29 +0900

    [Silver IV] Title: 상근이의 여행, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <climits>
+#include <cstring>
+#include <set>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m;
+        cin>>n>>m;
+        for(int i=0;i<m;i++){
+            int a,b;
+            cin>>a>>b;
+        }
+        cout<<n-1<<'\n';
+    }
+}
