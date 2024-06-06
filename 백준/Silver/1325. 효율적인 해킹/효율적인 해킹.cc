39bac7b9328629a0c86ea0a77649305639f127a4
kariskan <73534426+kariskan@users.noreply.github.com>
2024-06-06 10:55:40 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2464 ms, Memory: 3508 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -1,38 +1,62 @@
 #include <iostream>
+#include <queue>
+#include <algorithm>
 #include <vector>
+#include <stack>
+#include <climits>
 #include <cstring>
+#include <map>
+#include <set>
+#include <cmath>
+#include <string>
 using namespace std;
 
+int g[10001], n, m;
 vector<vector<int>> v;
-int n, m;
-int arr[10001], visit[10001];
 
-int dfs(int idx) {
-	visit[idx] = 1;
-	int sum = 0;
-	for (int i = 0; i < v[idx].size(); i++) {
-		if (!visit[v[idx][i]])sum += dfs(v[idx][i]);
+int go(int node) {
+	queue<int> q;
+	int vis[10001] = { 0, };
+
+	q.push(node);
+	vis[node] = 1;
+
+	int res = 0;
+	while (!q.empty()) {
+		int now = q.front();
+		q.pop();
+
+		res++;
+
+		for (auto& i : v[now]) {
+			if (!vis[i]) {
+				q.push(i);
+				vis[i] = 1;
+			}
+		}
 	}
-	return sum + 1;
+	return g[node] = res;
 }
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
+
 	cin >> n >> m;
 	v.resize(n + 1);
 	for (int i = 0; i < m; i++) {
-		int a, b; cin >> a >> b;
+		int a, b;
+		cin >> a >> b;
 		v[b].push_back(a);
 	}
-	int ans = 0;
+	int ma = 0;
 	for (int i = 1; i <= n; i++) {
-		arr[i] = dfs(i);
-		ans = max(ans, arr[i]);
-		memset(visit, 0, sizeof(visit));
+		ma = max(ma, go(i));
 	}
 	for (int i = 1; i <= n; i++) {
-		if (ans == arr[i])cout << i << " ";
+		if (g[i] == ma) {
+			cout << i << ' ';
+		}
 	}
 }
