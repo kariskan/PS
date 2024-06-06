e2c2bcdb621779056594095e0f03e4e210036176
kariskan <73534426+kariskan@users.noreply.github.com>
2024-06-06 10:39:39 +0900

    [Gold IV] Title: 연산자 끼워넣기 (3), Time: 12 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/15659. 연산자 끼워넣기 （3）/연산자 끼워넣기 （3）.cc
+++ 백준/Gold/15659. 연산자 끼워넣기 （3）/연산자 끼워넣기 （3）.cc
@@ -1,83 +1,76 @@
-#include <climits>
-#include <deque>
 #include <iostream>
+#include <queue>
+#include <algorithm>
 #include <vector>
+#include <stack>
+#include <climits>
+#include <cstring>
+#include <map>
+#include <set>
+#include <cmath>
+#include <string>
 using namespace std;
 
-int n, a[11], op[4], c;
-int minAns = INT_MAX, maxAns = INT_MIN;
-
-int calc(int a, int b, int c) {
-    if (c == 0) {
-        return a + b;
-    }
-    if (c == 1) {
-        return a - b;
-    }
-    if (c == 2) {
-        return a * b;
-    }
-    return a / b;
-}
-
-int process(vector<int> seq) {
-    deque<int> st;
-    vector<int> opst = seq;
-    st.push_back(a[0]);
-    int idx = 0;
-    for (int i = 0; i < opst.size(); i++) {
-        if (opst[i] == 2 || opst[i] == 3) {
-            int res = calc(st.back(), a[i + idx + 1], opst[i]);
-            st.pop_back();
-            st.push_back(res);
-            opst.erase(opst.begin() + i);
-            i--;
-            idx++;
-        } else {
-            st.push_back(a[i + idx + 1]);
-        }
-    }
-    while (st.size() != 1) {
-        int a = st.front();
-        st.pop_front();
-        int b = st.front();
-        st.pop_front();
-
-        int res = calc(a, b, opst.front());
-
-        opst.erase(opst.begin());
-        st.push_front(res);
-    }
-    return st[0];
-}
-
-void go(vector<int> o, vector<int> seq, int cnt) {
-    if (cnt == n - 1) {
-        int res = process(seq);
-        minAns = min(minAns, res);
-        maxAns = max(maxAns, res);
-        return;
-    }
-
-    for (int i = 0; i < 4; i++) {
-        if (o[i] < op[i]) {
-            o[i]++;
-            seq.push_back(i);
-            go(o, seq, cnt + 1);
-            seq.pop_back();
-            o[i]--;
-        }
-    }
+int n, a[11], op[4];
+int ma = INT_MIN, mi = INT_MAX;
+void go(vector<int> seq) {
+	if (seq.size() == n - 1) {
+		vector<int> v;
+		v.push_back(a[0]);
+		for (int i = 0; i < n - 1; i++) {
+			if (seq[i] == 2 || seq[i] == 3) {
+				int pre = v.back();
+				v.pop_back();
+				if (seq[i] == 2) {
+					v.push_back(pre * a[i + 1]);
+				}
+				else {
+					v.push_back(pre / a[i + 1]);
+				}
+			}
+			else {
+				v.push_back(a[i + 1]);
+			}
+		}
+		int res = v[0], idx = 1;
+		for (int i = 0; i < seq.size(); i++) {
+			if (seq[i] == 0 || seq[i] == 1) {
+				if (seq[i] == 0) {
+					res += v[idx++];
+				}
+				else {
+					res -= v[idx++];
+				}
+			}
+		}
+		mi = min(mi, res);
+		ma = max(ma, res);
+		return;
+	}
+	for (int i = 0; i < 4; i++) {
+		if (op[i] > 0) {
+			op[i]--;
+			seq.push_back(i);
+			go(seq);
+			op[i]++;
+			seq.pop_back();
+		}
+	}
 }
 
 int main() {
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    cin >> op[0] >> op[1] >> op[2] >> op[3];
-    vector<int> o(4);
-    go(o, {}, 0);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-    cout << maxAns << '\n' << minAns;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	for (int i = 0; i < 4; i++) {
+		cin >> op[i];
+	}
+	go({});
+
+	cout << ma << '\n' << mi;
 }
