3e12856eff87cf8a540247d12af3430042617aae
kariskan <73534426+kariskan@users.noreply.github.com>
2024-06-07 10:52:29 +0900

    [Gold V] Title: 공약수, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2436. 공약수/공약수.cc
+++ 백준/Gold/2436. 공약수/공약수.cc
@@ -1,44 +1,50 @@
 #include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <climits>
+#include <cstring>
+#include <set>
 #include <cmath>
+#include <string>
+#include <sstream>
+
 using namespace std;
 
-long long arr[1000000] = { 0, };
-
 long long gcd(long long a, long long b) {
-	if (a == 0)return b;
-	return gcd(b % a, a);
-}
-int arrcount = 0;
-void Prime(int m) {
-	for (int i = 1; i <= sqrt(m); i++) {
-		if (m % i == 0) {
-			if (i == m / i)arr[arrcount++] = i;
-			else {
-				arr[arrcount++] = i;
-				arr[arrcount++] = m / i;
-			}
-		}
-	}
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
 }
 
 int main() {
-	int n, m;
-	cin >> n >> m;
-	int ans = 300000000;
-	Prime(m);
-	long long ans1, ans2;
-	for (int i = 0; i < arrcount; i++) {
-		for (int j = i; j < arrcount; j++) {
-			long long p1 = arr[i] * arr[j];
-			if (gcd(arr[i], arr[j]) == n && p1 / gcd(arr[i], arr[j]) == m) {
-				if (ans > arr[i] + arr[j]) {
-					ans = arr[i] + arr[j];
-					ans1 = arr[i];
-					ans2 = arr[j];
-				}
-			}
-		}
-	}
-	if (ans1 > ans2)swap(ans1, ans2);
-	cout << ans1 << " " << ans2;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    long long a, b;
+    cin >> a >> b;
+
+    long long s = a * b;
+    long long ansg = LLONG_MAX / 10, ansG = LLONG_MAX / 10;
+    for (long long i = a; i * i <= s; i++) {
+        if (s % i != 0) {
+            continue;
+        }
+        long long g = gcd(i, s / i);
+        long long G = s / g;
+        if (g > G) {
+            swap(g, G);
+        }
+        if (!(g == a && G == b)) {
+            continue;
+        }
+        if (ansg + ansG > i + s / i) {
+            ansg = min(i, s / i);
+            ansG = max(i, s / i);
+        }
+    }
+    cout << ansg << ' ' << ansG;
 }
