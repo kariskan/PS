078e8b372602e04ea2469636cb84bd883112612b
kariskan <73534426+kariskan@users.noreply.github.com>
2024-06-10 10:28:13 +0900

    [Gold IV] Title: 수들의 합 4, Time: 32 ms, Memory: 9396 KB -BaekjoonHub

--- 백준/Gold/2015. 수들의 합 4/수들의 합 4.cc
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.cc
@@ -1,28 +1,37 @@
 #include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <climits>
+#include <cstring>
+#include <set>
+#include <cmath>
+#include <string>
+#include <sstream>
 #include <map>
+
 using namespace std;
 
-int n, k, a[200001], sum[200001];
+int a[200001], s[200001];
 
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	cin >> n >> k;
-	map<int, int> m;
-	long long ans = 0;
-	for (int i = 1; i <= n; i++)
-	{
-		cin >> a[i];
-		sum[i] = sum[i - 1] + a[i];
-		if (sum[i] == k)
-		{
-			ans++;
-		}
-		ans += m[sum[i] - k];
-		m[sum[i]]++;
-	}
-
-	cout << ans;
+    int n, k;
+    cin >> n >> k;
+    map<int, long long> ma;
+    long long ans = 0;
+    ma[0]++;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        s[i] = s[i - 1] + a[i];
+        if (ma.find(s[i] - k) != ma.end()) {
+            ans += ma[s[i] - k];
+        }
+        ma[s[i]]++;
+    }
+    cout << ans;
 }
