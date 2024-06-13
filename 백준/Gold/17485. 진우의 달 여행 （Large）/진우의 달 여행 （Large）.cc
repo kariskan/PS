0a5ee02e41de565033b6b2bb9e5a78170c33f27c
kariskan <73534426+kariskan@users.noreply.github.com>
2024-06-13 10:33:55 +0900

    [Gold V] Title: 진우의 달 여행 (Large), Time: 76 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.cc
+++ 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.cc
@@ -1,55 +1,50 @@
 #include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <stack>
 #include <climits>
+#include <cstring>
+#include <set>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <map>
+
 using namespace std;
 
-int n, m, map[1001][1002], dp[1001][1002][3];
+int a[1001][1001], dp[1001][1002][3];
 
-int main()
-{
-	cin >> n >> m;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = 1; j <= m; j++)
-		{
-			cin >> map[i][j];
-			if (i == 1)
-			{
-				dp[i][j][0] = dp[i][j][1] = dp[i][j][2] = map[i][j];
-			}
-			else if (j == 1)
-			{
-				dp[i][j][2] = 1000000000;
-			}
-			else if (j == m)
-			{
-				dp[i][j][0] = 1000000000;
-			}
-		}
-	}
-
-	int ans = INT_MAX;
-	for (int i = 2; i <= n; i++)
-	{
-		for (int j = 1; j <= m; j++)
-		{
-			if (j != 1)
-			{
-				dp[i][j][2] = min(dp[i - 1][j - 1][0], dp[i - 1][j - 1][1]) + map[i][j];
-			}
-			if (j != m)
-			{
-				dp[i][j][0] = min(dp[i - 1][j + 1][1], dp[i - 1][j + 1][2]) + map[i][j];
-			}
-			dp[i][j][1] = min(dp[i - 1][j][0], dp[i - 1][j][2]) + map[i][j];
-			if (i == n)
-			{
-				ans = min(ans, dp[i][j][0]);
-				ans = min(ans, dp[i][j][1]);
-				ans = min(ans, dp[i][j][2]);
-			}
-		}
-	}
-
-	cout << ans;
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cin >> a[i][j];
+            if (i == 1) {
+                dp[i][j][0] = dp[i][j][1] = dp[i][j][2] = a[i][j];
+            }
+        }
+        dp[i][1][2] = dp[i][m][0] = INT_MAX;
+    }
+    int ans = INT_MAX;
+    for (int i = 2; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (j < m) {
+                dp[i][j][0] = min(dp[i - 1][j + 1][1], dp[i - 1][j + 1][2]) + a[i][j];
+            }
+            dp[i][j][1] = min(dp[i - 1][j][0], dp[i - 1][j][2]) + a[i][j];
+            if (j > 1) {
+                dp[i][j][2] = min(dp[i - 1][j - 1][0], dp[i - 1][j - 1][1]) + a[i][j];
+            }
+            if (i == n) {
+                ans = min(ans, min(dp[i][j][0], min(dp[i][j][1], dp[i][j][2])));
+            }
+        }
+    }
+    cout << ans;
 }
