f07ab1c4a0224069ed885e45ea8ce734c5380afb
kariskan <73534426+kariskan@users.noreply.github.com>
2024-06-18 10:06:09 +0900

    [Silver I] Title: 완전 이진 트리, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
@@ -1,35 +1,44 @@
 #include <iostream>
+#include <queue>
+#include <algorithm>
 #include <vector>
+#include <stack>
+#include <climits>
+#include <cstring>
+#include <set>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <map>
+
 using namespace std;
 
-int k;
-int a[2000];
-vector<vector<int>> ans(11);
+int a[10000], tree[10000], n, inp = 1;
 
-void go(int left, int right, int h) {
-	if (left > right) return;
-
-	int mid = (left + right) / 2;
-	ans[h].push_back(a[mid]);
-
-	go(left, mid - 1, h + 1);
-	go(mid + 1, right, h + 1);
-
+void traversal(int idx) {
+    if (idx >= (1 << n)) {
+        return;
+    }
+    traversal(idx * 2);
+    tree[idx] = a[inp++];
+    traversal(idx * 2 + 1);
 }
 
 int main() {
-	cin >> k;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	for (int i = 1; i < (1 << k); i++) {
-		cin >> a[i];
-	}
-
-	go(1, (1 << k) - 1, 1);
-
-	for (int i = 1; i <= k; i++) {
-		for (int j = 0; j < ans[i].size(); j++) {
-			cout << ans[i][j] << ' ';
-		}
-		cout << "\n";
-	}
+    cin >> n;
+    for (int i = 1; i < (1 << n); i++) {
+        cin >> a[i];
+    }
+    traversal(1);
+    int idx = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < (1 << i); j++) {
+            cout << tree[idx++] << ' ';
+        }
+        cout << '\n';
+    }
 }
