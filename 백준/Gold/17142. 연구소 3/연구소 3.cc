1d3466cda5ebeb21efb799ee6526cb8efb5fa2aa
kariskan <73534426+kariskan@users.noreply.github.com>
2024-06-18 10:50:55 +0900

    [Gold III] Title: 연구소 3, Time: 16 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.cc
+++ 백준/Gold/17142. 연구소 3/연구소 3.cc
@@ -1,68 +1,93 @@
 #include <iostream>
 #include <queue>
 #include <algorithm>
+#include <vector>
+#include <stack>
+#include <climits>
+#include <cstring>
+#include <set>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <map>
+
 using namespace std;
-queue<pair<int, int>> q;
-int board[50][50];
-vector<pair<int, int>> virus;
-vector<int> t;
-int x[4] = { 0,0,1,-1 };
-int y[4] = { 1,-1,0,0 };
-int n, m;
+
+int a[50][50], n, m, ans = -1, blank;
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
+vector<pair<int, int>> v;
+
+bool range(int x, int y) {
+    return x >= 0 && x < n && y >= 0 && y < n;
+}
+
+void go(vector<pair<int, int>> seq, int idx) {
+    if (seq.size() == m) {
+        int vis[50][50] = {0,};
+        queue<pair<int, int>> q;
+        for (auto &i: seq) {
+            q.push({i.first, i.second});
+            vis[i.first][i.second] = 1;
+        }
+
+        int ma = 0, b = 0;
+        while (!q.empty()) {
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+
+            if (a[x][y] == 0) {
+                b++;
+            }
+            if (a[x][y] != 2) {
+                ma = max(ma, vis[x][y] - 1);
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (range(nx, ny) && (vis[nx][ny] == 0 || vis[nx][ny] > vis[x][y] + 1) && a[nx][ny] != 1) {
+                    q.push({nx, ny});
+                    vis[nx][ny] = vis[x][y] + 1;
+                }
+            }
+        }
+        if (b == blank && (ans == -1 || ans > ma)) {
+            if (ma == 0) {
+                ans = 0;
+            } else {
+                ans = ma;
+            }
+        }
+        return;
+    }
+    if (idx >= v.size()) {
+        return;
+    }
+    go(seq, idx + 1);
+    seq.push_back(v[idx]);
+    go(seq, idx + 1);
+}
 
 int main() {
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> board[i][j];
-			if (board[i][j] == 2) {
-				virus.push_back({ i,j });
-			}
-		}
-	}
-	t.resize(virus.size() - m);
-	for (int i = 0; i < m; i++) {
-		t.push_back(1);
-	}
-	int ans = -1;
-	do {
-		int visit[50][50] = { 0, };
-		int res = 1;
-		for (int i = 0; i < virus.size(); i++) {
-			if (t[i] == 1) {
-				q.push({ virus[i].first,virus[i].second });
-				visit[virus[i].first][virus[i].second] = 1;
-			}
-		}
-		while (!q.empty()) {
-			int a = q.front().first;
-			int b = q.front().second;
-			q.pop();
-			for (int i = 0; i < 4; i++) {
-				int nx = a + x[i];
-				int ny = b + y[i];
-				if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visit[nx][ny] && board[nx][ny] != 1) {
-					visit[nx][ny] = visit[a][b] + 1;
-					if(board[nx][ny]!=2) res = visit[nx][ny];
-					q.push({ nx,ny });
-				}
-			}
-		}
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (visit[i][j] == 0 && board[i][j] != 1 && board[i][j] != 2) {
-					res = -1;
-					break;
-				}
-			}
-			if (res == -1)break;
-		}
-		if (ans == -1) {
-			if (res != -1)ans = res - 1;
-		}
-		else {
-			if (res != -1)ans = min(ans, res - 1);
-		}
-	} while (next_permutation(t.begin(), t.end()));
-	cout << ans;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> a[i][j];
+            if (a[i][j] == 2) {
+                v.push_back({i, j});
+            }
+            if (a[i][j] == 0) {
+                blank++;
+            }
+        }
+    }
+
+    go({}, 0);
+    cout << ans;
 }
