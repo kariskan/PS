d96ca7ba49f741705fe6cb47050cefaab18d01a7
kariskan <73534426+kariskan@users.noreply.github.com>
2024-06-19 11:40:28 +0900

    [Gold I] Title: K번째 수, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.cc
+++ 백준/Gold/1300. K번째 수/K번째 수.cc
@@ -1,27 +1,42 @@
 #include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <climits>
+#include <cstring>
+#include <set>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <map>
+
 using namespace std;
 
-int n, k;
-
 int main() {
-	cin >> n >> k;
-	int left = 1;
-	int right = k;
-	int mid = (left + right) / 2;
-	long long ans;
-	while (left <= right) {
-		mid = (left + right) / 2;
-		long long sum = 0;
-		for (int i = 1; i <= n; i++) {
-			sum += min(n, mid / i);
-		}
-		if (sum < k) {
-			left = mid + 1;
-		}
-		else {
-			ans = mid;
-			right = mid - 1;
-		}
-	}
-	cout << ans;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    long long n, m;
+    cin >> n >> m;
+
+    long long left = 1, right = n * n, ans = n * n;
+    while (left <= right) {
+        long long mid = (left + right) / 2;
+
+        long long sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += min(n, mid / i);
+        }
+
+        if (sum >= m) {
+            ans = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    cout << ans;
 }
--- 백준/Gold/1300. K번째 수/README.md
+++ 백준/Gold/1300. K번째 수/README.md
@@ -1,14 +1,18 @@
-# [Gold II] K번째 수 - 1300 
+# [Gold I] K번째 수 - 1300 
 
 [문제 링크](https://www.acmicpc.net/problem/1300) 
 
 ### 성능 요약
 
-메모리: 2020 KB, 시간: 12 ms
+메모리: 2020 KB, 시간: 28 ms
 
 ### 분류
 
-이분 탐색(binary_search), 매개 변수 탐색(parametric_search)
+이분 탐색, 매개 변수 탐색
+
+### 제출 일자
+
+2024년 6월 19일 11:40:22
 
 ### 문제 설명
 
