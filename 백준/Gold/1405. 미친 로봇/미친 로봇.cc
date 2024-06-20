cdcc2df4fbac2b76a7abb64edf3ad8af16ba9329
kariskan <73534426+kariskan@users.noreply.github.com>
2024-06-20 11:18:16 +0900

    [Gold IV] Title: 미친 로봇, Time: 32 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1405. 미친 로봇/미친 로봇.cc
+++ 백준/Gold/1405. 미친 로봇/미친 로봇.cc
@@ -1,39 +1,55 @@
 #include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <climits>
+#include <cstring>
+#include <set>
 #include <cmath>
+#include <string>
+#include <sstream>
+#include <map>
+
 using namespace std;
 
-int n;
+int n, vis[30][30];
 double a[4];
-int visit[30][30];
-int x[4] = { 0,0,1,-1 };
-int y[4] = { 1,-1,0,0 };
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
 double ans;
-void go(double rate, int i, int j, int c) {
-	if (c == n) {
-		ans += rate;
-		return;
-	}
-	for (int k = 0; k < 4; k++) {
-		if (a[k]) {
-			int nx = i + x[k];
-			int ny = j + y[k];
-			if (!visit[nx][ny]) {
-				visit[nx][ny] = 1;
-				go(rate * a[k], nx, ny, c + 1);
-				visit[nx][ny] = 0;
-			}
-		}
-	}
+
+void go(double r, int x, int y, int c) {
+    if (c == n) {
+        ans += r;
+        return;
+    }
+    for (int k = 0; k < 4; k++) {
+        if (a[k] > 0) {
+            int nx = x + dx[k];
+            int ny = y + dy[k];
+            if (!vis[nx][ny]) {
+                vis[nx][ny] = 1;
+                go(r * a[k], nx, ny, c + 1);
+                vis[nx][ny] = 0;
+            }
+        }
+    }
 }
+
 int main() {
-	cin >> n;
-	for (int i = 0; i < 4; i++) {
-		int num; cin >> num;
-		a[i] = (double)num / 100;
-	}
-	visit[15][15] = 1;
-	go(1, 15, 15, 0);
-	cout << fixed;
-	cout.precision(9);
-	cout << ans;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+    for (int i = 0; i < 4; i++) {
+        cin >> a[i];
+        a[i] /= 100;
+    }
+    vis[15][15] = 1;
+    go(1, 15, 15, 0);
+    cout << fixed;
+    cout.precision(9);
+    cout << ans;
 }
