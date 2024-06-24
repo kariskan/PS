4f05a290fe87a5dc0130f82b996044503176daf3
kariskan <73534426+kariskan@users.noreply.github.com>
2024-06-24 20:51:18 +0900

    [Gold II] Title: 문제집, Time: 32 ms, Memory: 3956 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.cc
+++ 백준/Gold/1766. 문제집/문제집.cc
@@ -1,37 +1,52 @@
 #include <iostream>
+#include <queue>
 #include <algorithm>
 #include <vector>
-#include <queue>
+#include <stack>
+#include <climits>
+#include <cstring>
+#include <set>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <map>
+
 using namespace std;
-int n, m;
-int inp[32001];
+
+int n, m, in[32001];
+vector<vector<int>> v;
+
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	cin >> n >> m;
-	vector<vector<int>> v(n + 1);
+    cin >> n >> m;
+    v.resize(n + 1);
 
-	for (int i = 0; i < m; i++) {
-		int a, b;
-		cin >> a >> b;
-		inp[b]++;
-		v[a].push_back(b);
-	}
-	priority_queue<int, vector<int>, greater<int>> q;
-	for (int i = 1; i <= n; i++) {
-		if (inp[i] == 0) {
-			q.push(i);
-		}
-	}
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        in[b]++;
+        v[a].push_back(b);
+    }
 
-	while (!q.empty()) {
-		int a = q.top();
-		q.pop();
-		cout << a << ' ';
+    priority_queue<int, vector<int>, greater<>> pq;
+    for (int i = 1; i <= n; i++) {
+        if (in[i] == 0) {
+            pq.push(i);
+        }
+    }
+    while (!pq.empty()) {
+        int node = pq.top();
+        pq.pop();
 
-		for (int i = 0; i < v[a].size(); i++) {
-			if (--inp[v[a][i]] == 0) {
-				q.push(v[a][i]);
-			}
-		}
-	}
+        cout << node << ' ';
+        for (auto &i: v[node]) {
+            in[i]--;
+            if (in[i] == 0) {
+                pq.push(i);
+            }
+        }
+    }
 }
