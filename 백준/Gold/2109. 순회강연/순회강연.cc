2bf4af3a19b2f6db9cf84295ef04197fc6f05495
kariskan <73534426+kariskan@users.noreply.github.com>
2024-06-26 19:38:25 +0900

    [Gold III] Title: 순회강연, Time: 4 ms, Memory: 2276 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.cc
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -1,33 +1,47 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
 #include <algorithm>
 using namespace std;
 
-pair<int, int> arr[10000];
-int n;
-int day[10001];
-bool compare(pair<int, int>& p1, pair<int, int>& p2) {
-	if (p1.first == p2.first) {
-		return p1.second < p2.second;
-	}
-	return p1.first > p2.first;
+int n, m[10001];
+pair<int, int> a[10001];
+
+bool compare(pair<int, int> &i, pair<int, int> &j) {
+    return i.second < j.second;
 }
 
 int main() {
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i].first >> arr[i].second;
-	}
-	sort(arr, arr + n, compare);
-	int ans = 0;
-	for (int i = 0; i < n; i++) {
-		int date = arr[i].second;
-		while (day[date]) {
-			date--;
-		}
-		if (date != 0) {
-			day[date] = arr[i].first;
-			ans += day[date];
-		}
-	}
-	cout << ans;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i].first >> a[i].second;
+    }
+
+    int day = 1;
+    int ans = 0;
+    priority_queue<int, vector<int>, greater<int> > q;
+
+    sort(a + 1, a + 1 + n, compare);
+
+    int idx = 1;
+    while (idx <= n) {
+        while (day == a[idx].second) {
+            q.push(a[idx++].first);
+        }
+        while (q.size() > day) {
+            q.pop();
+        }
+        day++;
+    }
+
+    while (!q.empty()) {
+        ans += q.top();
+        q.pop();
+    }
+    cout << ans;
 }
