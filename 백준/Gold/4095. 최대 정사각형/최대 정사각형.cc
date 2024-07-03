d981b52a4dfbfdde86b75599085af943f1977a93
kariskan <73534426+kariskan@users.noreply.github.com>
2024-07-03 19:00:33 +0900

    [Gold IV] Title: 최대 정사각형, Time: 856 ms, Memory: 9720 KB -BaekjoonHub

--- 백준/Gold/4095. 최대 정사각형/최대 정사각형.cc
+++ 백준/Gold/4095. 최대 정사각형/최대 정사각형.cc
@@ -1,46 +1,42 @@
 #include <iostream>
-#include <cstring>
+#include <vector>
+#include <queue>
+#include <climits>
+#include <algorithm>
+#include <map>
 using namespace std;
 
-int map[1001][1001], dp[1001][1001], n, m;
-
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    
-	int n, m;
-	while (cin >> n >> m, n != 0 && m != 0)
-	{
-		for (int i = 1; i <= n; i++)
-		{
-			for (int j = 1; j <= m; j++)
-			{
-				cin >> map[i][j];
-			}
-		}
-		
-		int ans = 0;
 
-		for (int i = 1; i <= n; i++)
-		{
-			for (int j = 1; j <= m; j++)
-			{
-				if (map[i][j] == 0)
-				{
-					continue;
-				}
-				int len = min(dp[i - 1][j], dp[i][j - 1]);
-				dp[i][j] = map[i - len][j - len] == 1 ? len + 1 : len;
-				ans = max(ans, dp[i][j]);
-			}
-		}
-		
-		cout << ans << '\n';
-
-		memset(dp, 0, sizeof(dp));
-	}
-	
-	
+    while (1) {
+        int a[1001][1001] = {0,}, dp[1001][1001] = {0,};
+        int n, m;
+        cin >> n >> m;
+        if (n == 0 && m == 0) {
+            break;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                cin >> a[i][j];
+            }
+        }
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (a[i][j] == 0) {
+                    continue;
+                }
+                int mi = min(dp[i - 1][j], dp[i][j - 1]);
+                dp[i][j] = mi;
+                if (a[i - mi][j - mi]) {
+                    dp[i][j]++;
+                }
+                ans = max(ans, dp[i][j]);
+            }
+        }
+        cout << ans << '\n';
+    }
 }
