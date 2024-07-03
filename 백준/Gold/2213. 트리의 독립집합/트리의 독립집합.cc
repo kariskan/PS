7a3cfbf03a98285e2ac44ac0145c9ad22edbefbe
kariskan <73534426+kariskan@users.noreply.github.com>
2024-07-03 19:02:27 +0900

    [Gold I] Title: 트리의 독립집합, Time: 4 ms, Memory: 2812 KB -BaekjoonHub

--- 백준/Gold/2213. 트리의 독립집합/트리의 독립집합.cc
+++ 백준/Gold/2213. 트리의 독립집합/트리의 독립집합.cc
@@ -1,73 +1,77 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <climits>
 #include <algorithm>
+#include <map>
 using namespace std;
 
-int n, w[10001], dp[10001][2]; //i번째 노드를 j==0 선택했을 때, j==1 선택하지 않았을 때 최대 가중치
-vector<vector<int>> v;
+int dp[10001][2], w[10001], n, vis[10001];
+vector<vector<int> > v;
+vector<int> ans;
 
-int dfs(int e, int c, int pre) {
-	if (dp[e][c])return dp[e][c];
-
-	int ret = 0;
-	if (c == 0) {
-		ret = w[e];
-	}
-
-	for (auto& i : v[e]) {
-		if (i != pre) {
-			if (c == 0) {
-				ret += dfs(i, 1, e);
-			}
-			else {
-				ret += max(dfs(i, 1, e), dfs(i, 0, e));
-			}
-		}
-	}
-
-	return dp[e][c] = ret;
+int go(int node, int flag, int pre) {
+    if (dp[node][flag]) {
+        return dp[node][flag];
+    }
+    int res = 0;
+    for (auto &i: v[node]) {
+        if (i == pre) {
+            continue;
+        }
+        if (flag) {
+            res += go(i, 0, node);
+        } else {
+            res += max(go(i, 1, node), go(i, 0, node));
+        }
+    }
+    return dp[node][flag] = res + (flag ? w[node] : 0);
 }
 
-vector<int> answer;
-
-void go(int node, int pre, int c) {
-
-	if (c == 0)answer.push_back(node);
-
-	for (auto& i : v[node]) {
-		if (i != pre) {
-			if (c == 0) {
-				go(i, node, 1);
-			}
-			else {
-				if (dp[i][1] > dp[i][0])go(i, node, 1);
-				else go(i, node, 0);
-			}
-		}
-	}
+/**
+ * 1,10 -> 130, '140'
+ * 2,30 -> '130', 120
+ * 3,40 -> 20, '60'
+ * 4,10 -> '20', 10
+ * 5,20 -> 0, '20'
+ * 6,20 -> '70', 20
+ * 7,70 -> 0, '70'
+ * 바로 전에 방문된 정점이 최대 독립 집합에 속하지 않음
+ */
+void go2(int node, int pre) {
+    if (dp[node][1] > dp[node][0] && !vis[pre]) {
+        ans.push_back(node);
+        vis[node] = 1;
+    }
+    for (auto &i: v[node]) {
+        if (i != pre) {
+            go2(i, node);
+        }
+    }
 }
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	cin >> n;
-	v.resize(n + 1);
+    cin >> n;
+    v.resize(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> w[i];
+    }
 
-	for (int i = 1; i <= n; i++) {
-		cin >> w[i];
-	}
+    for (int i = 1; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
 
-	for (int i = 0; i < n - 1; i++) {
-		int a, b; cin >> a >> b;
-		v[a].push_back(b);
-		v[b].push_back(a);
-	}
-
-	int ans = max(dfs(1, 0, 0), dfs(1, 1, 0));
-	if (dp[1][0] > dp[1][1])go(1, 0, 0);
-	else go(1, 0, 1);
-	sort(answer.begin(), answer.end());
-
-	cout << ans << '\n';
-	for (auto& i : answer)cout << i << ' ';
-	return 0;
+    cout << max(go(1, 0, 0), go(1, 1, 0)) << '\n';
+    go2(1, 0);
+    sort(ans.begin(), ans.end());
+    for (auto &i: ans) {
+        cout << i << ' ';
+    }
 }
