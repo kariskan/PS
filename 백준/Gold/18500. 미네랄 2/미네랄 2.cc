b6c1e188682e8c73ae6ed123299c7be91b8aaa32
kariskan <73534426+kariskan@users.noreply.github.com>
2024-07-04 19:15:40 +0900

    [Gold I] Title: 미네랄 2, Time: 28 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Gold/18500. 미네랄 2/미네랄 2.cc
+++ 백준/Gold/18500. 미네랄 2/미네랄 2.cc
@@ -1,103 +1,144 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
+#include <queue>
+#include <climits>
 #include <algorithm>
+#include <map>
 using namespace std;
 
-int n, m, t;
-int map[100][100];
-vector<vector<int>> v;
-int x[4] = { 0,0,1,-1 };
-int y[4] = { 1,-1,0,0 };
-int visit[100][100];
-int dis = 100;
-bool run(int i, int j) {
-	if (i == n - 1)return true;
-	for (int k = 0; k < 4; k++) {
-		int nx = i + x[k];
-		int ny = j + y[k];
-		if (nx >= 0 && nx < n && ny >= 0 && ny < m && !visit[nx][ny] && map[nx][ny]) {
-			visit[nx][ny] = 1;
-			if (run(nx, ny))return true;
-		}
-	}
-	return false;
+char board[101][101];
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
+
+bool compare(pair<int, int> &p1, pair<int, int> &p2) {
+    return p1.first > p2.first;
 }
 
 int main() {
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			char c; cin >> c;
-			if (c == 'x')map[i][j] = 1;
-		}
-	}
-	cin >> t;
-	for (int i = 0; i < t; i++) {
-		int a; cin >> a;
-		int idx = -1;
-		dis = 100;
-		if (i % 2 == 0) {
-			for (int j = 0; j < m; j++) {
-				if (map[n - a][j]) {
-					map[n - a][j] = 0;
-					idx = j;
-					break;
-				}
-			}
-		}
-		else {
-			for (int j = m - 1; j >= 0; j--) {
-				if (map[n - a][j]) {
-					map[n - a][j] = 0;
-					idx = j;
-					break;
-				}
-			}
-		}
-		if (idx != -1) {
-			for (int k = 0; k < 4; k++) {
-				int nx = n - a + x[k];
-				int ny = idx + y[k];
-				if (nx >= 0 && nx < n && ny >= 0 && ny < m && !visit[nx][ny] && map[nx][ny]) {
-					visit[nx][ny] = 1;
-					if (!run(nx, ny)) {
-						for (int j1 = 0; j1 < m; j1++) {
-							for (int i1 = n - 1; i1 >= 0; i1--) {
-								if (visit[i1][j1]) {
-									int temp = 0;
-									for (int k = i1 + 1; k < n; k++) {
-										if (map[k][j1] && !visit[k][j1])break;
-										temp++;
-									}
-									if (temp != 0)
-										dis = min(temp, dis);
-								}
-							}
-						}
-						for (int i1 = n - 1; i1 >= 0; i1--) {
-							for (int j1 = 0; j1 < m; j1++) {
-								if (visit[i1][j1]) {
-									map[i1 + dis][j1] = map[i1][j1];
-									map[i1][j1] = 0;
-								}
-							}
-						}
-						break;
-					}
-					memset(visit, false, sizeof(visit));
-					dis = 100;
-				}
-			}
-			memset(visit, false, sizeof(visit));
-			dis = 100;
-		}
-	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (map[i][j])cout << 'x';
-			else cout << '.';
-		}
-		cout << '\n';
-	}
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+    int c;
+    cin >> c;
+    for (int l = 0; l < c; l++) {
+        int a;
+        cin >> a;
+
+        if (l % 2 == 0) {
+            for (int j = 1; j <= m; j++) {
+                if (board[n - a + 1][j] == 'x') {
+                    board[n - a + 1][j] = '.';
+                    break;
+                }
+            }
+        } else {
+            for (int j = m; j >= 1; j--) {
+                if (board[n - a + 1][j] == 'x') {
+                    board[n - a + 1][j] = '.';
+                    break;
+                }
+            }
+        }
+
+        int vis[101][101] = {0,};
+        int visIdx = 0;
+        bool brk = false;
+        queue<pair<int, int> > q;
+        for (int i = 1; i <= n; i++) {
+            bool flag = true;
+            for (int j = 1; j <= m; j++) {
+                if (board[i][j] == '.' || vis[i][j]) {
+                    continue;
+                }
+                vector<pair<int, int> > v;
+                q.push({i, j});
+                visIdx++;
+                vis[i][j] = visIdx;
+                while (!q.empty()) {
+                    int x = q.front().first;
+                    int y = q.front().second;
+                    q.pop();
+
+                    v.push_back({x, y});
+                    if (x == n) {
+                        flag = false;
+                    }
+
+                    for (int k = 0; k < 4; k++) {
+                        int nx = x + dx[k];
+                        int ny = y + dy[k];
+                        if (nx >= 1 && nx <= n && ny >= 1 && ny <= m && vis[nx][ny] == 0 && board[nx][ny] == 'x') {
+                            vis[nx][ny] = visIdx;
+                            q.push({nx, ny});
+                        }
+                    }
+                }
+                if (flag) {
+                    int dropHeight = n;
+                    for (int k = 0; k < v.size(); k++) {
+                        int x = v[k].first;
+                        int y = v[k].second;
+                        int idx = x;
+                        while (1) {
+                            idx++;
+                            if (idx > n || (vis[x][y] != vis[idx][y] && board[idx][y] == 'x')) {
+                                break;
+                            }
+                        }
+                        if (idx > n) {
+                            dropHeight = min(dropHeight, n - x);
+                            // cout << x << ' ' << idx << '\n';
+                        } else {
+                            dropHeight = min(dropHeight, idx - x - 1);
+                            // cout << x << ' ' << idx << '\n';
+                        }
+                    }
+                    // cout << dropHeight << '\n';
+                    sort(v.begin(), v.end(), compare);
+                    for (int i = 0; i < v.size(); i++) {
+                        int x = v[i].first;
+                        int y = v[i].second;
+                        board[x + dropHeight][y] = 'x';
+                        board[x][y] = '.';
+                        // cout << x << ' ' << y << ' ' << x + dropHeight << ' ' << y << '\n';
+                    }
+                    brk = true;
+                    break;
+                }
+            }
+            if (brk) {
+                break;
+            }
+        }
+        // for (int i = 1; i <= n; i++) {
+            // for (int j = 1; j <= m; j++) {
+                // cout << board[i][j];
+            // }
+            // cout << '\n';
+        // }
+        // cout << '\n';
+        // for (int i = 1; i <= n; i++) {
+            // for (int j = 1; j <= m; j++) {
+                // cout << vis[i][j];
+            // }
+            // cout << '\n';
+        // }
+        // cout << '\n';
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cout << board[i][j];
+        }
+        cout << '\n';
+    }
 }
+// 11 12 12 7
