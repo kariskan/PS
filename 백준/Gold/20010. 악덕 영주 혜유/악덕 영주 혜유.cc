f658d341b28dbe517cf6430a0af11616c5725b9c
kariskan <73534426+kariskan@users.noreply.github.com>
2024-07-05 18:41:56 +0900

    [Gold II] Title: 악덕 영주 혜유, Time: 84 ms, Memory: 6788 KB -BaekjoonHub

--- 백준/Gold/20010. 악덕 영주 혜유/악덕 영주 혜유.cc
+++ 백준/Gold/20010. 악덕 영주 혜유/악덕 영주 혜유.cc
@@ -1,26 +1,20 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <climits>
+#include <algorithm>
+#include <map>
+#include <cmath>
 using namespace std;
 
-int n, k, p[1000], ans1, ans2;
-vector<vector<pair<int, int>>> v;
-vector<pair<int, pair<int, int>>> inp;
-
-void dfs(int node, int pre, int cost) {
-    if (ans2 < cost) {
-        ans2 = cost;
-    }
-
-    for (auto& i : v[node]) {
-        if (i.first != pre) {
-            dfs(i.first, node, cost + i.second);
-        }
-    }
-}
+int n, k, p[1001], lastNode, maxWeight;
+vector<vector<pair<int, int> > > ad;
+vector<pair<int, pair<int, int> > > v;
 
 int Find(int x) {
-    if (p[x] == x) return x;
+    if (p[x] == x) {
+        return x;
+    }
     return p[x] = Find(p[x]);
 }
 
@@ -28,41 +22,54 @@ void Union(int a, int b) {
     a = Find(a);
     b = Find(b);
 
-    p[a] = b;
+    p[b] = a;
+}
+
+int dfs(int node, int pre, int w) {
+    if (w > maxWeight) {
+        maxWeight = w;
+        lastNode = node;
+    }
+    int res = 0;
+    for (auto &i: ad[node]) {
+        if (i.first == pre) {
+            continue;
+        }
+        res = max(res, dfs(i.first, node, w + i.second));
+    }
+
+    return res;
 }
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
     cin >> n >> k;
-
-    v.resize(n);
-
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         p[i] = i;
     }
+    ad.resize(n + 1);
 
     for (int i = 0; i < k; i++) {
         int a, b, c;
         cin >> a >> b >> c;
-        inp.push_back({c, {a, b}});
+        v.push_back({c, {a + 1, b + 1}});
     }
-
-    sort(inp.begin(), inp.end());
-
-    for (int i = 0; i < inp.size(); i++) {
-        int a = inp[i].second.first;
-        int b = inp[i].second.second;
-        if (Find(a) != Find(b)) {
-            Union(a, b);
-
-            v[a].push_back({b, inp[i].first});
-            v[b].push_back({a, inp[i].first});
-            ans1 += inp[i].first;
+    sort(v.begin(), v.end(), [](auto &a, auto &b) { return a.first < b.first; });
+    int ans1 = 0;
+    for (auto &i: v) {
+        if (Find(i.second.first) != Find(i.second.second)) {
+            Union(i.second.first, i.second.second);
+            ans1 += i.first;
+            ad[i.second.first].push_back({i.second.second, i.first});
+            ad[i.second.second].push_back({i.second.first, i.first});
         }
     }
 
-    for (int i = 0; i < n; i++) {
-        dfs(i, -1, 0);
-    }
-
-    cout << ans1 << '\n' << ans2;
+    cout << ans1 << '\n';
+    dfs(1, 0, 0);
+    dfs(lastNode, 0, 0);
+    cout << maxWeight;
 }
