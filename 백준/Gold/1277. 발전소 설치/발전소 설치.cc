d34ea9e906227c8f03542e4918312859ec99f6be
kariskan <73534426+kariskan@users.noreply.github.com>
2024-07-05 18:22:30 +0900

    [Gold IV] Title: 발전소 설치, Time: 28 ms, Memory: 19072 KB -BaekjoonHub

--- 백준/Gold/1277. 발전소 설치/발전소 설치.cc
+++ 백준/Gold/1277. 발전소 설치/발전소 설치.cc
@@ -1,74 +1,75 @@
 #include <iostream>
-#include <queue>
 #include <vector>
+#include <queue>
 #include <climits>
+#include <algorithm>
+#include <map>
 #include <cmath>
 using namespace std;
 
-int n, m;
-double limit;
-pair<int, int> coor[1001];
+typedef long long ll;
+int n, w;
+double m;
+vector<vector<pair<int, double> > > v;
+pair<ll, ll> coor[1001];
 
-double getDis(int a, int b)
-{
-	double x = coor[a].first - coor[b].first;
-	double y = coor[a].second - coor[b].second;
-	return sqrt(x * x + y * y);
+double getDistance(int i, int j) {
+    return sqrt(
+        (coor[i].first - coor[j].first) * (coor[i].first - coor[j].first) +
+        (coor[i].second - coor[j].second) * (coor[i].second - coor[j].second)
+    );
 }
 
-int main()
-{
-	cin >> n >> m >> limit;
-	vector<vector<pair<int, double>>> v(n + 1);
-	for (int i = 1; i <= n; i++)
-	{
-		cin >> coor[i].first >> coor[i].second;
-	}
-	for (int i = 0; i < m; i++)
-	{
-		int a, b;
-		cin >> a >> b;
-		v[a].push_back({b, 0});
-		v[b].push_back({a, 0});
-	}
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = i + 1; j <= n; j++)
-		{
-			double dis = getDis(i, j);
-			if (dis <= limit)
-			{
-				v[i].push_back({j, dis});
-				v[j].push_back({i, dis});
-			}
-		}
-	}
-	vector<double> dis(n + 1, 5000000000);
-	priority_queue<pair<double, int>> pq;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	dis[1] = 0;
-	pq.push({0, 1});
+    cin >> n >> w >> m;
+    for (int i = 1; i <= n; i++) {
+        cin >> coor[i].first >> coor[i].second;
+    }
+    v.resize(n + 1);
+    for (int i = 0; i < w; i++) {
+        int a, b;
+        cin >> a >> b;
+        v[a].push_back({b, 0});
+        v[b].push_back({a, 0});
+    }
 
-	while (!pq.empty())
-	{
-		double cost = -pq.top().first;
-		int node = pq.top().second;
-		pq.pop();
+    for (int i = 1; i <= n; i++) {
+        for (int j = i + 1; j <= n; j++) {
+            double dis = getDistance(i, j);
+            if (dis <= m) {
+                v[i].push_back({j, dis});
+                v[j].push_back({i, dis});
+            }
+        }
+    }
 
-		if (dis[node] < cost)
-			continue;
+    vector<double> dis(n + 1, -1);
+    priority_queue<pair<double, int> > q;
 
-		for (auto &i : v[node])
-		{
-			int nxNode = i.first;
-			double nxDis = cost + i.second;
-			if (i.second <= limit && dis[nxNode] > nxDis)
-			{
-				dis[nxNode] = nxDis;
-				pq.push({-nxDis, nxNode});
-			}
-		}
-	}
+    q.push({0, 1});
+    dis[1] = 0;
 
-	cout << (long long)(dis[n] * 1000);
+    while (!q.empty()) {
+        double cost = -q.top().first;
+        int node = q.top().second;
+        q.pop();
+
+        if (dis[node] < cost) {
+            continue;
+        }
+
+        for (auto &i: v[node]) {
+            double nxCost = i.second + cost;
+            int nxNode = i.first;
+            if (dis[nxNode] == -1 || nxCost < dis[nxNode]) {
+                dis[nxNode] = nxCost;
+                q.push({-nxCost, nxNode});
+            }
+        }
+    }
+    cout << (long) (dis[n] * 1000);
 }
