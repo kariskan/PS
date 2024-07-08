dab01e890d88d19caaa0cf3ae44fd8841d39c9f4
kariskan <73534426+kariskan@users.noreply.github.com>
2024-07-08 17:17:42 +0900

    [Gold V] Title: 숫자고르기, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.cc
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.cc
@@ -1,37 +1,51 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <queue>
+#include <climits>
+#include <algorithm>
+#include <map>
+#include <cmath>
+#include <mutex>
 using namespace std;
 
-vector<int> ans;
-int map[101][101];
+int board[101][101];
+
 int main() {
-	int n;
-	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		int a; cin >> a;
-		map[i][a] = 1;
-	}
-	for (int k = 1; k <= n; k++) {
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= n; j++) {
-				if (i == j)continue;
-				if (map[i][k] && map[k][j]) {
-					map[i][j] = 1;
-				}
-			}
-		}
-	}
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			if (map[i][j] && map[j][i]) {
-				ans.push_back(i);
-				ans.push_back(j);
-			}
-		}
-	}
-	sort(ans.begin(), ans.end());
-	ans.erase(unique(ans.begin(), ans.end()), ans.end());
-	cout << ans.size() << '\n';
-	for (auto i : ans)cout << i << '\n';
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        int a;
+        cin >> a;
+        board[i][a] = 1;
+    }
+
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (board[i][k] && board[k][j]) {
+                    board[i][j] = 1;
+                }
+            }
+        }
+    }
+
+    vector<int> ans;
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j <= n; j++) {
+            if (board[i][j] && board[j][i]) {
+                ans.push_back(i);
+                ans.push_back(j);
+            }
+        }
+    }
+    sort(ans.begin(), ans.end());
+    ans.erase(unique(ans.begin(), ans.end()), ans.end());
+    cout << ans.size() << '\n';
+    for (auto &i: ans) {
+        cout << i << '\n';
+    }
 }
