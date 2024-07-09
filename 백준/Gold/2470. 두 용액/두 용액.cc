8397825ce769f6d8ba6b8c1b3c9b8fa4f401cc9e
kariskan <73534426+kariskan@users.noreply.github.com>
2024-07-09 18:21:26 +0900

    [Gold V] Title: 두 용액, Time: 16 ms, Memory: 2920 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.cc
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -1,54 +1,48 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
 #include <algorithm>
+#include <map>
+#include <cmath>
+#include <mutex>
 using namespace std;
 
-int n;
-int a[100000];
-
 int main() {
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-	}
-	sort(a, a + n);
-	int ans = 2000000000;
-	int ansx = 0, ansy = n - 1;
-	/*for (int i = 0; i < n - 1; i++) {
-		int start = a[i];
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-		int left = i + 1;
-		int right = n - 1;
-		int mid = (left + right) / 2;
-		while (left <= right) {
-			mid = (left + right) / 2;
+    int n;
+    cin >> n;
+    vector<int> v;
 
-			if (a[mid] + start < 0) {
-				left = mid + 1;
-			}
-			else {
-				right = mid - 1;
-			}
-			if (ans > abs(start + a[mid])) {
-				ans = abs(start + a[mid]);
-				ansx = start;
-				ansy = a[mid];
-			}
-		}
-	}*/
-	int left = 0, right = n - 1;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
 
-	while (left < right) {
-		if (ans > abs(a[left] + a[right])) {
-			ans = abs(a[left] + a[right]);
-			ansx = a[left];
-			ansy = a[right];
-		}
-		if (left<right - 1 && abs(a[left + 1] + a[right])>abs(a[left] + a[right - 1])) {
-			right--;
-		}
-		else {
-			left++;
-		}
-	}
-	cout << ansx << ' ' << ansy;
+    sort(v.begin(), v.end());
+    // -99 -2 -1 4 98
+
+    int l = 0, r = n - 1;
+    int ans=2000000000, ansl = 0, ansr = n - 1;
+    while (l < r) {
+        int s = v[l] + v[r];
+
+        if (ans > abs(s)) {
+            ans = abs(s);
+            ansl = v[l];
+            ansr = v[r];
+        }
+
+        if (s < 0) {
+            l++;
+        } else {
+            r--;
+        }
+    }
+
+    cout << ansl << ' ' << ansr;
 }
