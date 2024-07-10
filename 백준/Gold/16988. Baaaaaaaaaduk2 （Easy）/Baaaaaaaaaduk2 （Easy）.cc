0d9e35097e32ab82cdac309565575faa4ba14024
kariskan <73534426+kariskan@users.noreply.github.com>
2024-07-10 18:37:33 +0900

    [Gold III] Title: Baaaaaaaaaduk2 (Easy), Time: 88 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/16988. Baaaaaaaaaduk2 （Easy）/Baaaaaaaaaduk2 （Easy）.cc
+++ 백준/Gold/16988. Baaaaaaaaaduk2 （Easy）/Baaaaaaaaaduk2 （Easy）.cc
@@ -1,73 +1,83 @@
 #include <iostream>
+#include <vector>
 #include <queue>
+#include <climits>
+#include <algorithm>
+#include <map>
+#include <cmath>
 using namespace std;
-int board[21][21];
-int n, m;
-int x[4] = { 0,0,1,-1 }, y[4] = { 1,-1,0,0 };
-int ans;
-void bfs() {
-	queue<pair<int, int>>q;
-	int visit[21][21] = { 0, };
-	int sum = 0;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= m; j++) {
-			if (board[i][j] == 2 && !visit[i][j]) {
-				q.push({ i,j });
-				visit[i][j] = 1;
-				int brk = 0;
-				int cnt = 1;
-				while (!q.empty()) {
-					int a = q.front().first;
-					int b = q.front().second;
-					q.pop();
-					for (int k = 0; k < 4; k++) {
-						int nx = a + x[k];
-						int ny = b + y[k];
-						if (nx >= 1 && nx <= n && ny >= 1 && ny <= m && !visit[nx][ny]) {
-							if (board[nx][ny] == 0) {
-								brk = 1;
-							}
-							else if (board[nx][ny] == 1)continue;
-							else {
-								q.push({ nx,ny });
-								visit[nx][ny] = 1;
-								cnt++;
-							}
-						}
-					}
-				}
-				if (!brk) {
-					sum += cnt;
-				}
-			}
-		}
-	}
-	if (sum == 0)return;
-	ans = max(ans, sum);
-}
+
 int main() {
-	cin >> n >> m;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= m; j++) {
-			cin >> board[i][j];
-		}
-	}
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= m; j++) {
-			if (!board[i][j]) {
-				board[i][j] = 1;
-				for (int i1 = 1; i1 <= n; i1++) {
-					for (int j1 = 1; j1 <= m; j1++) {
-						if (!board[i1][j1]) {
-							board[i1][j1] = 1;
-							bfs();
-							board[i1][j1] = 0;
-						}
-					}
-				}
-				board[i][j] = 0;
-			}
-		}
-	}
-	cout << ans;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int> > board(n, vector<int>(m));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+    int ans = 0;
+    for (int x1 = 0; x1 < n; x1++) {
+        for (int y1 = 0; y1 < m; y1++) {
+            for (int x2 = x1; x2 < n; x2++) {
+                for (int y2 = 0; y2 < m; y2++) {
+                    if (x1 == x2 && y1 == y2) {
+                        continue;
+                    }
+                    if (board[x1][y1] == 0 && board[x2][y2] == 0) {
+                        board[x1][y1] = board[x2][y2] = 1;
+                        int killCount = 0;
+                        int vis[20][20] = {0,};
+                        queue<pair<int, int> > q;
+                        for (int i = 0; i < n; i++) {
+                            for (int j = 0; j < m; j++) {
+                                if (!vis[i][j] && board[i][j] == 2) {
+                                    bool flag = true;
+                                    vis[i][j] = 1;
+                                    q.push({i, j});
+                                    int enemyCount = 0;
+                                    while (!q.empty()) {
+                                        int x = q.front().first;
+                                        int y = q.front().second;
+                                        q.pop();
+
+                                        enemyCount++;
+
+                                        for (int k = 0; k < 4; k++) {
+                                            int nx = x + dx[k];
+                                            int ny = y + dy[k];
+                                            if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+                                                if (board[nx][ny] == 0) {
+                                                    flag = false;
+                                                }
+                                                if (!vis[nx][ny] && board[nx][ny] == 2) {
+                                                    vis[nx][ny] = 1;
+                                                    q.push({nx, ny});
+                                                }
+                                            }
+                                        }
+                                    }
+                                    if (flag) {
+                                        killCount += enemyCount;
+                                    }
+                                }
+                            }
+                        }
+                        ans = max(ans, killCount);
+                        board[x1][y1] = board[x2][y2] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << ans;
 }
