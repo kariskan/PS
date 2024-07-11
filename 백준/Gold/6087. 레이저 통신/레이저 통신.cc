1642da6c8759d63fc908e0bfa29e99d57bee1471
kariskan <73534426+kariskan@users.noreply.github.com>
2024-07-11 18:34:24 +0900

    [Gold III] Title: 레이저 통신, Time: 0 ms, Memory: 2072 KB -BaekjoonHub

--- 백준/Gold/6087. 레이저 통신/레이저 통신.cc
+++ 백준/Gold/6087. 레이저 통신/레이저 통신.cc
@@ -1,54 +1,85 @@
 #include <iostream>
+#include <vector>
 #include <queue>
+#include <climits>
+#include <algorithm>
+#include <map>
+#include <cmath>
+#include <mutex>
 using namespace std;
+
+char board[100][100];
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
 int n, m;
-char board[101][101];
-int visit[101][101];
-int x[4] = { 0,0,1,-1 };
-int y[4] = { 1,-1,0,0 };
-pair<int, int> c1 = { -1,0 }, c2;
+
+bool range(int x, int y) {
+    return x >= 0 && x < n && y >= 0 && y < m;
+}
+
+bool canNotRotate(int d, int nd) {
+    return nd == (d + 2) % 4;
+}
+
 int main() {
-	cin >> m >> n;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> board[i][j];
-			if (board[i][j] == 'C') {
-				if (c1.first == -1) {
-					c1.first = i;
-					c1.second = j;
-				}
-				else {
-					c2.first = i;
-					c2.second = j;
-				}
-			}
-		}
-	}
-	queue<pair<int, int>> q; //i, j, dir
-	q.push({ c1.first,c1.second });
-	visit[c1.first][c1.second] = 1;
-	int ans = 100000000;
-	while (!q.empty()) {
-		int a, b;
-		a = q.front().first;
-		b = q.front().second;
-		q.pop();
-		if (a == c2.first && b == c2.second) {
-			ans = min(ans, visit[a][b]);
-			continue;
-		}
-		for (int i = 0; i < 4; i++) {
-			int nx = a + x[i];
-			int ny = b + y[i];
-			while (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] != '*') {
-				if (!visit[nx][ny]) {
-					visit[nx][ny] = visit[a][b] + 1;
-					q.push({ nx,ny });
-				}
-				nx += x[i];
-				ny += y[i];
-			}
-		}
-	}
-	cout << ans - 2;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> m >> n;
+
+    int sx = -1, sy, ex, ey;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> board[i][j];
+            if (board[i][j] == 'C') {
+                if (sx == -1) {
+                    sx = i;
+                    sy = j;
+                } else {
+                    ex = i;
+                    ey = j;
+                }
+                board[i][j] = '.';
+            }
+        }
+    }
+
+    int vis[100][100][4];
+    queue<pair<pair<int, int>, int> > q;
+
+    for (int i = 0; i < 4; i++) {
+        vis[sx][sy][i] = 1;
+        q.push({{sx, sy}, i});
+    }
+
+    while (!q.empty()) {
+        int x = q.front().first.first;
+        int y = q.front().first.second;
+        int d = q.front().second;
+        q.pop();
+
+        for (int k = 0; k < 4; k++) {
+            int nx = x + dx[k];
+            int ny = y + dy[k];
+            int nd = k;
+            if (canNotRotate(d, nd)) {
+                continue;
+            }
+            int nv = (d == nd) ? vis[x][y][d] : vis[x][y][d] + 1;
+            if (range(nx, ny) && (vis[nx][ny][nd] == 0 || vis[nx][ny][nd] > nv) && board[nx][ny] != '*') {
+                vis[nx][ny][nd] = nv;
+                q.push({{nx, ny}, nd});
+            }
+        }
+    }
+
+    int ans = INT_MAX;
+    for (int i = 0; i < 4; i++) {
+        if (vis[ex][ey][i] == 0) {
+            continue;
+        }
+        ans = min(ans, vis[ex][ey][i]);
+    }
+    cout << ans - 1;
 }
