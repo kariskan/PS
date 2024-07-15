71fa8756889fabfdf7e6f803e12047b32ba65b1c
kariskan <73534426+kariskan@users.noreply.github.com>
2024-07-15 17:23:59 +0900

    [Gold III] Title: 소수의 연속합, Time: 24 ms, Memory: 9128 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -1,42 +1,55 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+#include <algorithm>
+#include <map>
+#include <cmath>
+#include <mutex>
+#include <sstream>
 using namespace std;
-int n;
-int prime[4000001];
-int idx;
-int a[4000001];
-void p() {
-    for (int i = 2; i <= n; i++) {
-        if (prime[i]) continue;
-        prime[idx++] = i;
-        for (int j = i * 2; j <= n; j += i) {
-            prime[j] = 1;
-        }
-    }
-}
+
+bool isPrime[4000001];
+vector<int> prime;
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
+
+    int n;
     cin >> n;
-    p();
-    int p1 = 0, p2 = 0;
-    int ans = 0, sum = prime[0];
-    while (p1 <= p2 && p2 < idx) {
-        if (sum == n) {
-            ans++;
-            sum += prime[++p2];
-        }
-        else if (sum > n) {
-            sum -= prime[p1++];
-            if (p1 > p2) {
-                p2 = p1;
-                sum = prime[p1];
+    if (n == 1) {
+        cout << 0;
+        return 0;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (!isPrime[i]) {
+            for (int j = i * 2; j <= n; j += i) {
+                isPrime[j] = true;
             }
         }
-        else {
-            sum += prime[++p2];
+    }
+    for (int i = 2; i <= n; i++) {
+        if (!isPrime[i]) {
+            prime.push_back(i);
         }
     }
+
+    int l = 0, r = 0, sum = 2;
+
+    int ans = 0;
+    while (l <= r && r < prime.size()) {
+        if (sum == n) {
+            ans++;
+        }
+
+        if (sum <= n) {
+            sum += prime[++r];
+        } else {
+            sum -= prime[l++];
+        }
+    }
+
     cout << ans;
 }
