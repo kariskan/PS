38596a668636e42e5c1e1c6bf266a7100486c290
kariskan <73534426+kariskan@users.noreply.github.com>
2024-07-16 18:13:18 +0900

    [Gold V] Title: 집합의 표현, Time: 40 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.cc
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -1,36 +1,50 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+#include <algorithm>
+#include <map>
+#include <cmath>
+#include <mutex>
+#include <sstream>
 using namespace std;
-int set[1000001];
-int find(int x) {
-	if (x == set[x])return x;
-	else return set[x] = find(set[x]);
+
+int p[1000001];
+
+int Find(int x) {
+    if (p[x] == x) {
+        return x;
+    }
+    return p[x] = Find(p[x]);
 }
-void uni(int x, int y) {
-	x = find(x);
-	y = find(y);
-	if (x > y) set[x] = y;
-	else set[y] = x;
+
+void Union(int a, int b) {
+    a = Find(a);
+    b = Find(b);
+    p[b] = a;
 }
+
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	int n, m;
-	cin >> n >> m;
-	for (int i = 0; i <= n; i++) {
-		set[i] = i;
-	}
-	for (int i = 0; i < m; i++) {
-		int a, b, c;
-		cin >> a >> b >> c;
-		if (a) {
-			if (find(b) == find(c)) {
-				cout << "YES\n";
-			}
-			else cout << "NO\n";
-		}
-		else {
-			uni(b, c);
-		}
-	}
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        p[i] = i;
+    }
+    for (int i = 0; i < m; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a == 0) {
+            Union(b, c);
+        } else {
+            if (Find(b) != Find(c)) {
+                cout << "NO\n";
+            } else {
+                cout << "YES\n";
+            }
+        }
+    }
 }
