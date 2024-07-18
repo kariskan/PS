de4611855925fadf2ab3fd523a23ed4233e99929
kariskan <73534426+kariskan@users.noreply.github.com>
2024-07-18 19:16:02 +0900

    [Gold V] Title: 중첩 집합 모델, Time: 92 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Gold/19641. 중첩 집합 모델/중첩 집합 모델.cc
+++ 백준/Gold/19641. 중첩 집합 모델/중첩 집합 모델.cc
@@ -1,52 +1,64 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <climits>
 #include <algorithm>
+#include <map>
+#include <cmath>
+#include <sstream>
+#include <stack>
 using namespace std;
 
-int n, r;
+vector<vector<int> > v;
 pair<int, int> ans[100001];
-vector<vector<int>> v;
+int seq = 1, n, s;
 
-int go(int node, int pre, int last) {
-
-	int subSum = 0;
-
-	for (auto& i : v[node]) {
-		if (i != pre) {
-			subSum += go(i, node, last + subSum * 2 + 1);
-		}
-	}
-	ans[node] = { last + 1, last + 1 + subSum * 2 + 1 };
-	return subSum + 1;
+void go(int node, int pre) {
+    if (node == 100001) {
+        return;
+    }
+    int cnt = 0, last = 0;
+    for (auto &i: v[node]) {
+        if (i == pre) {
+            continue;
+        }
+        cnt++;
+        last = i;
+        if (cnt == 1) {
+            ans[node].first = seq++;
+        }
+        go(i, node);
+    }
+    ans[node].second = seq++;
 }
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-
-	cin >> n;
-	v.resize(n + 1);
-
-	for (int i = 0; i < n; i++) {
-		int t; cin >> t;
-		while (1) {
-			int k; cin >> k;
-			if (k == -1)break;
-			v[t].push_back(k);
-		}
-	}
-
-	for (int i = 1; i <= n; i++) {
-		sort(v[i].begin(), v[i].end());
-	}
-
-	cin >> r;
-
-	go(r, 0, 0);
-
-	for (int i = 1; i <= n; i++) {
-		cout << i << ' ' << ans[i].first << ' ' << ans[i].second << '\n';
-	}
+    cin >> n;
+    v.resize(n + 1);
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        while (1) {
+            int b;
+            cin >> b;
+            if (b == -1) {
+                break;
+            }
+            v[a].push_back(b);
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        sort(v[i].begin(), v[i].end());
+        v[i].push_back(100001);
+        v[i].push_back(100001);
+    }
+    cin >> s;
+    go(s, 0);
+    for (int i = 1; i <= n; i++) {
+        cout << i << ' ' << ans[i].first << ' ' << ans[i].second << '\n';
+    }
 }
