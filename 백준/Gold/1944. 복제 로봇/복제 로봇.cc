590eb49c666146e819185dc8bc7af8938b3bad77
kariskan <73534426+kariskan@users.noreply.github.com>
2024-07-24 18:55:01 +0900

    [Gold I] Title: 복제 로봇, Time: 16 ms, Memory: 3316 KB -BaekjoonHub

--- 백준/Gold/1944. 복제 로봇/복제 로봇.cc
+++ 백준/Gold/1944. 복제 로봇/복제 로봇.cc
@@ -1,17 +1,15 @@
-#include <algorithm>
 #include <iostream>
+#include <vector>
 #include <queue>
+#include <climits>
+#include <algorithm>
+#include <map>
+#include <cmath>
+#include <sstream>
+#include <stack>
 using namespace std;
 
-int n, m, p[251], seq[51][51];
-char map[51][51];
-int dx[4] = {0, 0, 1, -1};
-int dy[4] = {1, -1, 0, 0};
-vector<pair<pair<int, int>, int>> v;
-
-bool compare(pair<pair<int, int>, int> &p1, pair<pair<int, int>, int> &p2) {
-    return p1.second < p2.second;
-}
+int p[2501];
 
 int Find(int x) {
     if (p[x] == x) {
@@ -23,30 +21,38 @@ int Find(int x) {
 void Union(int a, int b) {
     a = Find(a);
     b = Find(b);
-    p[a] = b;
+
+    p[b] = a;
 }
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m;
     cin >> n >> m;
-    int idx = 0;
-    for (int i = 1; i <= m; i++) {
-        p[i] = i;
-    }
+    char board[50][50];
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            cin >> map[i][j];
-            if (map[i][j] == 'S' || map[i][j] == 'K') {
-                seq[i][j] = idx++;
+            cin >> board[i][j];
+            if (board[i][j] == 'S') {
+                board[i][j] = 'K';
             }
+            p[i * n + j] = i * n + j;
         }
     }
-    
+
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    vector<pair<int, pair<int, int> > > v;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (map[i][j] == 'S' || map[i][j] == 'K') {
-                queue<pair<int, int>> q;
-                int vis[51][51] = {0};
-                int temp = 0;
+            if (board[i][j] == 'K') {
+
+                queue<pair<int, int> > q;
+                int vis[50][50] = {0,};
+
                 q.push({i, j});
                 vis[i][j] = 1;
 
@@ -55,35 +61,39 @@ int main() {
                     int y = q.front().second;
                     q.pop();
 
+                    if ((i != x || j != y) && board[x][y] == 'K') {
+                        v.push_back({vis[x][y] - 1, {i * n + j, x * n + y}});
+                    }
+
                     for (int k = 0; k < 4; k++) {
                         int nx = x + dx[k];
                         int ny = y + dy[k];
-                        if (nx >= 0 && nx < n && ny >= 0 && ny < n &&
-                            !vis[nx][ny] && map[nx][ny] != '1') {
-                            if (map[nx][ny] == 'S' || map[nx][ny] == 'K') {
-                                v.push_back(
-                                    {{seq[i][j], seq[nx][ny]}, vis[x][y]});
-                                temp++;
-                            }
+                        if (nx >= 0 && nx < n && ny >= 0 && ny < n && !vis[nx][ny] && board[nx][ny] != '1') {
                             vis[nx][ny] = vis[x][y] + 1;
                             q.push({nx, ny});
                         }
                     }
                 }
-                if (temp != m) {
-                    cout << -1 << '\n';
-                    return 0;
-                }
             }
         }
     }
-    int ans = 0;
-    sort(v.begin(), v.end(), compare);
+
+    sort(v.begin(), v.end());
+    int cnt = 0, ans = 0;
     for (int i = 0; i < v.size(); i++) {
-        if (Find(v[i].first.first) != Find(v[i].first.second)) {
-            Union(v[i].first.first, v[i].first.second);
-            ans += v[i].second;
+        int n1 = v[i].second.first;
+        int n2 = v[i].second.second;
+        int cost = v[i].first;
+        if (Find(n1) != Find(n2)) {
+            Union(n1, n2);
+            cnt++;
+            ans += cost;
         }
     }
-    cout << ans;
+
+    if (cnt != m) {
+        cout << -1;
+    } else {
+        cout << ans << '\n';
+    }
 }
