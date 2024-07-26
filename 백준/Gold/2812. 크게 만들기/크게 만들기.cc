3d97fa7a886eb7fa88c1956100dd657f2b911b47
kariskan <73534426+kariskan@users.noreply.github.com>
2024-07-26 18:35:48 +0900

    [Gold III] Title: 크게 만들기, Time: 8 ms, Memory: 3888 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.cc
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.cc
@@ -1,33 +1,45 @@
 #include <iostream>
-#include <stack>
+#include <vector>
+#include <queue>
+#include <climits>
 #include <algorithm>
+#include <map>
+#include <cmath>
+#include <sstream>
+#include <stack>
 using namespace std;
-stack<char> st;
 
 int main() {
-	int n, m;
-	cin >> n >> m;
-	string s;
-	string ans = "";
-	cin >> s;
-	for (int i = 0; i < s.length(); i++) {
-		if (st.empty())st.push(s[i]);
-		else {
-			while (!st.empty() && m && st.top() < s[i]) {
-				st.pop();
-				m--;
-			}
-			st.push(s[i]);
-		}
-	}
-	while (m) {
-		st.pop();
-		m--;
-	}
-	while (!st.empty()) {
-		ans += st.top();
-		st.pop();
-	}
-	reverse(ans.begin(), ans.end());
-	cout << ans;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    string s;
+    cin >> s;
+
+    // 5 4 3 2 1 9
+    stack<char> st;
+    for (int i = 0; i < s.length(); i++) {
+        if (!st.empty()) {
+            while (!st.empty() && m && st.top() < s[i]) {
+                st.pop();
+                m--;
+            }
+        }
+        st.push(s[i]);
+    }
+
+    while (m--) {
+        st.pop();
+    }
+    string ans = "";
+    while (!st.empty()) {
+        ans += st.top();
+        st.pop();
+    }
+    reverse(ans.begin(), ans.end());
+
+    cout << ans;
 }
