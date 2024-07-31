dd7f7702a968cd5b47ce3d85be3a650c58ece7a1
kariskan <73534426+kariskan@users.noreply.github.com>
2024-07-31 18:19:17 +0900

    [Gold V] Title: 나무 위의 빗물, Time: 304 ms, Memory: 52760 KB -BaekjoonHub

--- 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.cc
+++ 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.cc
@@ -1,33 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <climits>
+#include <algorithm>
+#include <map>
+#include <cmath>
+#include <sstream>
+#include <stack>
 using namespace std;
 
-int n, w, cnt;
-vector<vector<int>> v;
+int n, w, leaf;
+vector<vector<int> > v;
 
 void go(int node, int pre) {
-	int ok = 0;
-	for (auto& i : v[node]) {
-		if (i != pre) {
-			ok = 1;
-			go(i, node);
-		}
-	}
-	if (!ok)cnt++;
+    bool isLeaf = true;
+    for (auto &i: v[node]) {
+        if (i != pre) {
+            go(i, node);
+            isLeaf = false;
+        }
+    }
+    if (isLeaf) {
+        leaf++;
+    }
 }
 
 int main() {
-	cin >> n >> w;
-	v.resize(n + 1);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	for (int i = 0; i < n - 1; i++) {
-		int a, b; cin >> a >> b;
-		v[a].push_back(b);
-		v[b].push_back(a);
-	}
+    cin >> n >> w;
+    v.resize(n + 1);
+    for (int i = 0; i < n - 1; i++) {
+        int a, b;
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+    go(1, 0);
 
-	go(1, 0);
+    cout.precision(6);
 
-	cout.precision(6);
-	cout << fixed << (double)w / (double)cnt;
+    cout << fixed << (double) w / leaf;
 }
