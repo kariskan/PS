07447587fbf0a08ea06034b1bffdd01c1581052f
kariskan <73534426+kariskan@users.noreply.github.com>
2024-07-31 18:47:32 +0900

    [Gold III] Title: 공주님의 정원, Time: 32 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.cc
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.cc
@@ -1,36 +1,60 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
 #include <algorithm>
+#include <map>
+#include <cmath>
+#include <sstream>
+#include <stack>
 using namespace std;
-int day[13] = { 0,31,59,90,120,151,181,212,243,273,304,334,365 };
-pair<int, int> inp[100000];
+
+int n;
+int monthSum[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+vector<pair<int, int> > v;
+
 int main() {
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		int m1, d1;
-		cin >> m1 >> d1;
-		inp[i].first = day[m1 - 1] + d1;
-		cin >> m1 >> d1;
-		inp[i].second = day[m1 - 1] + d1 - 1;
-	}
-	sort(inp, inp + n);
-	int now = day[2] + 1;
-	int idx = 0;
-	int ans = 0;
-	while (now <= day[11] && idx < n) {
-		int maxDay = -1;
-		for (; inp[idx].first <= now && idx < n; idx++) {
-			if (maxDay < inp[idx].second) {
-				maxDay = inp[idx].second;
-			}
-		}
-		if (maxDay == -1) {
-			cout << 0;
-			return 0;
-		}
-		ans++;
-		now = maxDay + 1;
-	}
-	if (now <= day[11])cout << 0;
-	else cout << ans;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    for (int i = 2; i <= 12; i++) {
+        monthSum[i] += monthSum[i - 1];
+    }
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        pair<int, int> p;
+        int m, d;
+        cin >> m >> d;
+        p.first = monthSum[m - 1] + d;
+        cin >> m >> d;
+        p.second = monthSum[m - 1] + d - 1;
+        v.push_back(p);
+    }
+
+    sort(v.begin(), v.end());
+
+    // 3월 1일
+    int now = monthSum[2] + 1;
+    int idx = 0;
+    int ans = 0;
+    while (idx < n && now <= monthSum[11]) {
+        int maxDay = -1;
+        while (idx < n && v[idx].first <= now) {
+            maxDay = max(maxDay, v[idx].second);
+            idx++;
+        }
+        if (maxDay != -1) {
+            now = maxDay + 1;
+            ans++;
+        } else {
+            cout << 0;
+            return 0;
+        }
+    }
+    if (now > monthSum[11]) {
+        cout << ans;
+    } else {
+        cout << 0;
+    }
 }
