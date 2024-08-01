ba1de6993cd6eccd9c1cca94650c15170e569f8e
kariskan <73534426+kariskan@users.noreply.github.com>
2024-08-01 18:47:15 +0900

    [Gold III] Title: 전기가 부족해, Time: 36 ms, Memory: 4460 KB -BaekjoonHub

--- 백준/Gold/10423. 전기가 부족해/전기가 부족해.cc
+++ 백준/Gold/10423. 전기가 부족해/전기가 부족해.cc
@@ -1,55 +1,61 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <queue>
+#include <climits>
+#include <algorithm>
+#include <map>
+#include <cmath>
+#include <sstream>
+#include <stack>
 using namespace std;
 
-int n, m, k, parent[1001], y[1001];
-vector<pair<int, pair<int, int>>> v;
+int p[1001];
 
 int Find(int x) {
-	if (parent[x] == x) return x;
-	else return parent[x] = Find(parent[x]);
+    if (p[x] == x) {
+        return x;
+    }
+    return p[x] = Find(p[x]);
 }
 
 void Union(int a, int b) {
-	a = Find(a);
-	b = Find(b);
-
-	if (a < b) parent[a] = b;
-	else parent[b] = a;
+    a = Find(a);
+    b = Find(b);
+    p[b] = a;
 }
 
 int main() {
-	cin >> n >> m >> k;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	for (int i = 1; i <= n; i++) parent[i] = i;
+    int n, m, k;
+    cin >> n >> m >> k;
+    for (int i = 1; i <= n; i++) {
+        p[i] = i;
+    }
+    for (int i = 0; i < k; i++) {
+        int a;
+        cin >> a;
+        p[a] = 0;
+    }
 
-	for (int i = 0; i < k; i++) {
-		int a; cin >> a;
-		parent[a] = 0;
-	}
+    vector<pair<int, pair<int, int> > > v;
+    for (int i = 0; i < m; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        v.push_back({c, {a, b}});
+    }
+    sort(v.begin(), v.end());
 
-	for (int i = 0; i < m; i++) {
-		int a, b, c; cin >> a >> b >> c;
-		v.push_back({ c,{a,b} });
-	}
-
-	sort(v.begin(), v.end());
-	int ans = 0;
-
-	for (auto& i : v) {
-		int n1 = i.second.first;
-		int n2 = i.second.second;
-		int cost = i.first;
-
-		int f1 = Find(n1);
-		int f2 = Find(n2);
-
-		if (f1 != f2) {
-			Union(n1, n2);
-			ans += cost;
-		}
-	}
-
-	cout << ans;
+    int ans = 0;
+    for (auto & i : v) {
+        int n1 = Find(i.second.first);
+        int n2 = Find(i.second.second);
+        if (n1 != n2) {
+            Union(n1, n2);
+            ans += i.first;
+        }
+    }
+    cout << ans;
 }
