a546f67be24bd1ab21897e6ef2fdbcb75f58f7ff
kariskan <73534426+kariskan@users.noreply.github.com>
2024-08-02 18:15:53 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 20 ms, Memory: 9848 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
@@ -1,29 +1,33 @@
-#include <stdio.h>
-int map[1000][1000] = { 0, };
-int dp[1000][1000] = { 0, };
-int min(int a, int b, int c) {
-	if (a <= b && a <= c)return a;
-	if (b <= a && b <= c)return b;
-	return c;
-}
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+#include <algorithm>
+#include <map>
+#include <cmath>
+#include <sstream>
+#include <stack>
+using namespace std;
+
+int board[1001][1001], dp[1001][1001];
+
 int main() {
-	int n, m;
-	scanf("%d %d", &n, &m);
-	int ans = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			scanf("%1d", &map[i][j]);
-			dp[i][j] = map[i][j];
-			if (dp[i][j] == 1)ans = 1;
-		}
-	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (map[i][j] != 0 && dp[i][j - 1] != 0 && dp[i - 1][j] != 0 && dp[i - 1][j - 1] != 0&&i>0&&j>0) {
-				dp[i][j] = min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) + 1;
-				if (ans < dp[i][j])ans = dp[i][j];
-			}
-		}
-	}
-	printf("%d", ans * ans);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m, ans = 0;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            char a;
+            cin >> a;
+            board[i][j] = a - '0';
+            if (board[i][j] == 1) {
+                dp[i][j] = min(dp[i - 1][j], min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+            }
+            ans = max(ans, dp[i][j]);
+        }
+    }
+    cout << ans * ans;
 }
