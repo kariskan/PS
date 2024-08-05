0661203d8b2862b190a1bdaa390401f132e19642
kariskan <73534426+kariskan@users.noreply.github.com>
2024-08-05 17:44:31 +0900

    [Gold III] Title: 미로 탈출하기, Time: 12 ms, Memory: 10928 KB -BaekjoonHub

--- 백준/Gold/17090. 미로 탈출하기/미로 탈출하기.cc
+++ 백준/Gold/17090. 미로 탈출하기/미로 탈출하기.cc
@@ -1,53 +1,79 @@
 #include <iostream>
-#include <cstring>
+#include <vector>
+#include <queue>
+#include <climits>
+#include <algorithm>
+#include <map>
+#include <cmath>
+#include <sstream>
+#include <stack>
 using namespace std;
 
-int n, m;
-char map[500][501];
-int dp[500][500];
-int visit[500][500];
-int x[4] = { 0,0,1,-1 }; //RLDU
-int y[4] = { 1,-1,0,0 };
-string s = "RLDU";
-int dfs(int i, int j) {
-	visit[i][j] = 1;
-	for (int k = 0; k < 4; k++) {
-		if (map[i][j] == s[k]) {
-			int nx = i + x[k];
-			int ny = j + y[k];
-			if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-				if (dp[nx][ny] == -1) {
-					if (!visit[nx][ny])dp[i][j] = dfs(nx, ny);
-					else dp[i][j] = 0;
-				}
-				else dp[i][j] = dp[nx][ny];
-			}
-			else dp[i][j] = 1;
-			break;
-		}
-	}
-	return dp[i][j];
+int n, m, vis[500][500], cur, ans;
+char board[500][500];
+
+pair<int, int> next(int x, int y) {
+    char dir = board[x][y];
+    switch (dir) {
+        case 'R': return {x, y + 1};
+        case 'L': return {x, y - 1};
+        case 'U': return {x - 1, y};
+        default: return {x + 1, y};
+    }
 }
+
+bool range(int x, int y) {
+    return x >= 0 && x < n && y >= 0 && y < m;
+}
+
+// vis[x][y] = 1이면 사이클
+// vis[x][y] = 2이면 탈출 가능
+void go(int x, int y) {
+    vis[x][y] = 1;
+    pair<int, int> nxt = next(x, y);
+    int nx = nxt.first;
+    int ny = nxt.second;
+    if (!range(nx, ny)) {
+        vis[x][y] = 2;
+        cur = 2;
+        return;
+    }
+    if (vis[nx][ny] != 0) {
+        vis[x][y] = vis[nx][ny];
+        cur = vis[nx][ny];
+        return;
+    }
+    go(nx, ny);
+    vis[x][y] = cur;
+}
+
 int main() {
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> map[i][j];
-		}
-	}
-	memset(dp, -1, sizeof(dp));
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (dp[i][j] == -1) {
-				dfs(i, j);
-			}
-		}
-	}
-	int ans = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (dp[i][j])ans++;
-		}
-	}
-	cout << ans;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (vis[i][j] == 0) {
+                cur = 0;
+                go(i, j);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (vis[i][j] == 2) {
+                ans++;
+            }
+        }
+    }
+    cout << ans;
 }
