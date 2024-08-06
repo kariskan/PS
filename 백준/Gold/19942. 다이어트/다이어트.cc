938b51244904ec79c8c572d2ee634001447eaf6a
kariskan <73534426+kariskan@users.noreply.github.com>
2024-08-06 18:26:09 +0900

    [Gold IV] Title: 다이어트, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/19942. 다이어트/다이어트.cc
+++ 백준/Gold/19942. 다이어트/다이어트.cc
@@ -1,60 +1,80 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <climits>
+#include <algorithm>
+#include <map>
+#include <cmath>
+#include <sstream>
+#include <stack>
 using namespace std;
 
-int n, mp, mf, ms, mv;
-int a[15][5];
-int ans = -1;
-vector<int> seq;
-bool check(vector<int> v)
-{
-	if (seq.empty())
-	{
-		return true;
-	}
-	for (int i = 0; i < min(seq.size(), v.size()); i++)
-	{
-		if (v[i] < seq[i])
-		{
-			return true;
-		}
-	}
-	return v.size() < seq.size();
-}
-void go(int idx, int nmp, int nmf, int nms, int nmv, int val, vector<int> v)
-{
-	if (nmp >= mp && nmf >= mf && nms >= ms && nmv >= mv)
-	{
-		if (ans == -1 || ans >= val)
-		{
-			ans = val;
-			if (check(v))
-			{
-				seq.clear();
-				seq = v;
-			}
-		}
-	}
-	if (idx >= n)
-	{
-		return;
-	}
-	go(idx + 1, nmp, nmf, nms, nmv, val, v);
-	v.push_back(idx + 1);
-	go(idx + 1, nmp + a[idx][0], nmf + a[idx][1], nms + a[idx][2], nmv + a[idx][3], val + a[idx][4], v);
-}
-int main()
-{
-	cin >> n >> mp >> mf >> ms >> mv;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> a[i][0] >> a[i][1] >> a[i][2] >> a[i][3] >> a[i][4];
-	}
-	go(0, 0, 0, 0, 0, 0, {});
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	cout << ans << '\n';
-	for (int i = 0; i < seq.size(); i++)
-	{
-		cout << seq[i] << ' ';
-	}
+    int n;
+    cin >> n;
+    int mp, mf, ms, mv;
+    cin >> mp >> mf >> ms >> mv;
+    int arr[15][5];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 5; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    int ans = -1;
+    vector<int> v;
+
+    for (int i = 0; i < (1 << n); i++) {
+        int tp = 0, tf = 0, ts = 0, tv = 0;
+        int cost = 0;
+        vector<int> t;
+        for (int j = 0; j < n; j++) {
+            if (i & (1 << j)) {
+                tp += arr[j][0];
+                tf += arr[j][1];
+                ts += arr[j][2];
+                tv += arr[j][3];
+                cost += arr[j][4];
+                t.push_back(j + 1);
+            }
+        }
+        if (tp >= mp && tf >= mf && ts >= ms && tv >= mv) {
+            if (ans == -1 || ans >= cost) {
+                if (ans == -1 || ans > cost) {
+                    ans = cost;
+                    v = t;
+                } else {
+                    bool flag = false;
+                    for (int j = 0; j < min(v.size(), t.size()); j++) {
+                        if (v[j] < t[j]) {
+                            flag = true;
+                            break;
+                        } else if (v[j] > t[j]) {
+                            v = t;
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (!flag) {
+                        if (v.size() > t.size()) {
+                            v = t;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if (ans == -1) {
+        cout << -1;
+    } else {
+        cout << ans << '\n';
+        for (int i = 0; i < v.size(); i++) {
+            cout << v[i] << ' ';
+        }
+    }
 }
