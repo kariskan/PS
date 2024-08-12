6cf5c17002864872a95c11f446a0565c3755ddb4
Yoon JungJin <73534426+kariskan@users.noreply.github.com>
2024-08-12 17:40:23 +0900

    [Gold IV] Title: LCS 3, Time: 204 ms, Memory: 30080 KB -BaekjoonHub

--- 백준/Gold/1958. LCS 3/LCS 3.java
+++ 백준/Gold/1958. LCS 3/LCS 3.java
@@ -19,8 +19,7 @@ public class Main {
 		for (int i = 1; i <= s1.length(); i++) {
 			for (int j = 1; j <= s2.length(); j++) {
 				for (int k = 1; k <= s3.length(); k++) {
-					dp[i][j][k] = maxV(dp[i - 1][j][k], dp[i][j - 1][k], dp[i][j][k - 1], dp[i - 1][j - 1][k],
-						dp[i - 1][j][k - 1], dp[i][j - 1][k - 1]);
+					dp[i][j][k] = maxV(dp[i - 1][j][k], dp[i][j - 1][k], dp[i][j][k - 1]);
 					if (s1.charAt(i - 1) == s2.charAt(j - 1) && s2.charAt(j - 1) == s3.charAt(k - 1)) {
 						dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j - 1][k - 1] + 1);
 					}
