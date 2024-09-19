d89bad846813a36197d2bae97033c1765dcdd7f6
Yoon JungJin <73534426+kariskan@users.noreply.github.com>
2024-09-19 10:17:12 +0900

    [Gold IV] Title: 기차 여행, Time: 128 ms, Memory: 14688 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Gold/10713. 기차 여행/기차 여행.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		var br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n, m;
+		var st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		int[] path = new int[m + 1];
+		int[][] cost = new int[n + 1][3];
+		int[] sum = new int[n + 1];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= m; i++) {
+			path[i] = Integer.parseInt(st.nextToken());
+			if (i > 1) {
+				int min = Math.min(path[i - 1], path[i]);
+				int max = Math.max(path[i - 1], path[i]);
+				sum[min]++;
+				sum[max]--;
+			}
+		}
+		for (int i = 1; i <= n; i++) {
+			sum[i] += sum[i - 1];
+		}
+		for (int i = 1; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			cost[i][0] = Integer.parseInt(st.nextToken());
+			cost[i][1] = Integer.parseInt(st.nextToken());
+			cost[i][2] = Integer.parseInt(st.nextToken());
+		}
+		long ans = 0;
+		for (int i = 1; i <= n; i++) {
+			if (sum[i] == 0) {
+				continue;
+			}
+			ans += Math.min(cost[i][0] * sum[i], cost[i][2] + cost[i][1] * sum[i]);
+		}
+		System.out.println(ans);
+	}
+}
