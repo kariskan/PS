4de49d2b68abc3848064a4b90e840585d462b8e2
Yoon JungJin <73534426+kariskan@users.noreply.github.com>
2024-09-23 15:19:47 +0900

    [Gold III] Title: 짝수 팰린드롬, Time: 120 ms, Memory: 14764 KB -BaekjoonHub

--- 백준/Gold/21925. 짝수 팰린드롬/짝수 팰린드롬.java
+++ 백준/Gold/21925. 짝수 팰린드롬/짝수 팰린드롬.java
@@ -11,18 +11,27 @@ class Main {
 
 		st = new StringTokenizer(br.readLine());
 		int answer = 0;
+		boolean[] check = new boolean[N];
 		int head = 0;
+		int tail = 0;
 		for(int i = 0; i < N; i++) {
+			tail = i;
 			arr.add(Integer.parseInt(st.nextToken()));
 			if (isPalindrome(arr)) {
-				head = i + 1;
+				Arrays.fill(check, head, tail + 1, true);
+				head = tail + 1;
 				answer++;
 				arr.clear();
 			}
 		}
 		if (answer == 0) System.out.println("-1");
 		else {
-			if (head == N)
+			boolean checker = true;
+			for (int i = 0; i < N; i++) {
+				// System.out.println(check[i]);
+				checker &= check[i];
+			}
+			if (checker)
 				System.out.println(answer);
 			else
 				System.out.println("-1");
@@ -32,7 +41,7 @@ class Main {
 	private boolean isPalindrome(List<Integer> arr) {
 		int N = arr.size();
 		if (N != 0 && N % 2 == 0) {
-			for(int i = 0; i < N / 2; i++) {
+			for(int i = 0; i < (N / 2); i++) {
 				if (!Objects.equals(arr.get(i), arr.get(N - i - 1))) return false;
 			}
 			return true;
